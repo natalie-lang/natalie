11ee63da874da39e1a4818ecc8a61ed4016dffc8
Tim Morgan <tim@timmorgan.org>
2019-11-16 19:11:08 -0600

    Start work on classes and objects

--- lib/natalie/boilerplate.c
+++ lib/natalie/boilerplate.c
@@ -2,94 +2,178 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hashmap.h"
 
-enum NatTypeType {
-    NAT_NIL_TYPE,
-    NAT_TRUE_TYPE,
-    NAT_FALSE_TYPE,
-    NAT_KEYWORD_TYPE,
-    NAT_NUMBER_TYPE,
-    NAT_SYMBOL_TYPE,
-    NAT_ARRAY_TYPE,
-    NAT_HASHMAP_TYPE,
-    NAT_STRING_TYPE,
-    NAT_REGEX_TYPE,
-    NAT_LAMBDA_TYPE,
-    NAT_CONTINUATION_TYPE,
-    NAT_ATOM_TYPE,
-    NAT_BLANK_LINE_TYPE,
-    NAT_ERROR_TYPE
+typedef struct NatObject NatObject;
+
+enum NatValueType {
+    NAT_VALUE_STRING,
+    NAT_VALUE_NUMBER,
+    NAT_VALUE_OTHER
 };
 
-typedef struct NatType NatType;
+struct NatObject {
+    enum NatValueType type;
+    NatObject *class;
+    int is_class;
+    char *name;
+    NatObject *superclass;
 
-struct NatType {
-enum NatTypeType type;
+    union {
+        long long number;
+        //struct hashmap hashmap;
 
-union {
-    long long number;
-    char *symbol;
-    char *keyword;
-    //struct hashmap hashmap;
-    NatType *atom_val;
-    NatType *error_val;
+        // NAT_ARRAY_TYPE
+        struct {
+            size_t ary_len;
+            size_t ary_cap;
+            NatObject **ary;
+        };
 
-    // NAT_ARRAY_TYPE
-    struct {
-    size_t ary_len;
-    size_t ary_cap;
-    NatType **ary;
+        // NAT_STRING_TYPE
+        struct {
+            size_t str_len;
+            size_t str_cap;
+            char *str;
+        };
+
+        // NAT_REGEX_TYPE
+        struct {
+            size_t regex_len;
+            char *regex;
+        };
+
+        // NAT_LAMBDA_TYPE, NAT_CONTINUATION_TYPE
+        //struct {
+        //  NatObject* (*fn)(NatEnv *env, size_t argc, NatObject **args);
+        //  char *function_name;
+        //  NatEnv *env;
+        //  size_t argc;
+        //  NatObject **args;
+        //};
     };
-
-    // NAT_STRING_TYPE
-    struct {
-    size_t str_len;
-    size_t str_cap;
-    char *str;
-    };
-
-    // NAT_REGEX_TYPE
-    struct {
-    size_t regex_len;
-    char *regex;
-    };
-
-    // NAT_LAMBDA_TYPE, NAT_CONTINUATION_TYPE
-    //struct {
-    //  NatType* (*fn)(NatEnv *env, size_t argc, NatType **args);
-    //  char *function_name;
-    //  NatEnv *env;
-    //  size_t argc;
-    //  NatType **args;
-    //};
-};
 };
 
-NatType* nat_alloc() {
-    NatType *val = malloc(sizeof(NatType));
+typedef struct NatEnv NatEnv;
+
+struct NatEnv {
+  struct hashmap data;
+  NatEnv *outer;
+};
+
+NatEnv* build_env(NatEnv *outer) {
+  NatEnv *env = malloc(sizeof(NatEnv));
+  env->outer = outer;
+  hashmap_init(&env->data, hashmap_hash_string, hashmap_compare_string, 100);
+  hashmap_set_key_alloc_funcs(&env->data, hashmap_alloc_key_string, NULL);
+  return env;
+}
+
+NatEnv* build_top_env() {
+  NatEnv *top_env = build_env(NULL);
+  return top_env;
+}
+
+NatEnv* env_find(NatEnv *env, char *key) {
+  if (hashmap_get(&env->data, key)) {
+    return env;
+  } else if (env->outer) {
+    return env_find(env->outer, key);
+  } else {
+    return NULL;
+  }
+}
+
+NatObject* env_get(NatEnv *env, char *key) {
+  env = env_find(env, key);
+  /*
+  if (!env) {
+    return mal_error(mal_sprintf("'%s' not found", key));
+  }
+  */
+  NatObject *val = hashmap_get(&env->data, key);
+  if (val) {
+    return val;
+  } else {
+    //return mal_error(mal_sprintf("'%s' not found", key));
+  }
+}
+
+NatObject* env_set(NatEnv *env, char *key, NatObject *val) {
+  //if (is_blank_line(val)) return val;
+  hashmap_remove(&env->data, key);
+  hashmap_put(&env->data, key, val);
+  return val;
+}
+
+void env_delete(NatEnv *env, char *key) {
+  hashmap_remove(&env->data, key);
+}
+
+#define TRUE 1
+#define FALSE 0
+
+char *heap_string(char *str) {
+    size_t len = strlen(str);
+    char *copy = malloc(len + 1);
+    memcpy(copy, str, len + 1);
+    return copy;
+}
+
+NatObject* nat_alloc() {
+    NatObject *val = malloc(sizeof(NatObject));
+    val->type = NAT_VALUE_OTHER;
     return val;
 }
 
-NatType* nat_number(long long num) {
-    NatType *val = nat_alloc();
-    val->type = NAT_NUMBER_TYPE;
+NatObject* nat_subclass(NatObject *superclass, char *name) {
+    NatObject *val = nat_alloc();
+    val->is_class = TRUE;
+    val->name = heap_string(name);
+    val->superclass = superclass;
+    return val;
+}
+
+NatObject* nat_new(NatObject *class) {
+    NatObject *val = nat_alloc();
+    val->is_class = FALSE;
+    val->class = class;
+    return val;
+}
+
+NatObject* nat_number(NatEnv *env, long long num) {
+    NatObject *val = nat_new(env_get(env, "Numeric"));
+    val->type = NAT_VALUE_NUMBER;
     val->number = num;
     return val;
 }
 
-NatType* nat_string(char *str) {
-    NatType *val = nat_alloc();
-    val->type = NAT_STRING_TYPE;
+NatObject* nat_string(NatEnv *env, char *str) {
+    NatObject *val = nat_new(env_get(env, "String"));
+    val->type = NAT_VALUE_STRING;
     size_t len = strlen(str);
-    char *copy = malloc(len + 1);
-    memcpy(copy, str, len + 1);
-    val->str = copy;
+    val->str = heap_string(str);
     val->str_len = len;
     val->str_cap = len;
     return val;
 }
 
 int main() {
-    __MAIN__
+    NatEnv *env = build_top_env();
+    NatObject *Class = nat_alloc();
+    Class->is_class = TRUE;
+    Class->name = heap_string("Class");
+    Class->superclass = Class;
+    env_set(env, "Class", Class);
+    NatObject *Object = nat_subclass(Class, "Object");
+    env_set(env, "Object", Object);
+    NatObject *main = nat_new(Object);
+    NatObject *Numeric = nat_subclass(Object, "Numeric");
+    Numeric->type = NAT_VALUE_NUMBER;
+    env_set(env, "Numeric", Numeric);
+    NatObject *String = nat_subclass(Object, "String");
+    String->type = NAT_VALUE_STRING;
+    env_set(env, "String", String);
+    /*MAIN*/
     return 0;
 }
--- lib/natalie/compiler.rb
+++ lib/natalie/compiler.rb
@@ -13,39 +13,43 @@ module Natalie
 
     def compile(out_path)
       write_file
-      puts @c_path if ENV['DEBUG']
-      out = `gcc -g -x c -o #{out_path} #{@c_path} 2>&1`
-      if $? == 0
-        File.unlink(@c_path) unless ENV['DEBUG']
-      else
+      out = `gcc -g -x c -I #{lib_path} -o #{out_path} #{@c_path} #{lib_path}/hashmap.c 2>&1`
+      File.unlink(@c_path) unless ENV['DEBUG']
+      if $? != 0
         puts "There was an error compiling:\n#{out}"
       end
     end
 
     def write_file
+      c = to_c
+      puts c if ENV['DEBUG']
       temp_c = Tempfile.create('natalie.c')
-      temp_c.write(to_c)
+      temp_c.write(c)
       temp_c.close
       @c_path = temp_c.path
     end
 
     def to_c
       exprs = @ast.map { |e| compile_expr(e) }.join("\n")
-      BOILERPLATE.sub(/__MAIN__/, exprs)
+      BOILERPLATE.sub('/*MAIN*/', exprs)
     end
 
     def compile_expr(expr)
       case expr.first
       when :number
-        "NatType *#{next_var_name} = nat_number(#{expr.last});"
+        "NatObject *#{next_var_name} = nat_number(env, #{expr.last});"
       when :string
-        "NatType *#{next_var_name} = nat_string(#{expr.last.inspect});"
+        "NatObject *#{next_var_name} = nat_string(env, #{expr.last.inspect});"
       else
         raise "unknown AST node: #{expr.inspect}"
       end
     end
 
     private
+
+    def lib_path
+      File.expand_path('.', __dir__)
+    end
 
     def next_var_name
       @var_num += 1
