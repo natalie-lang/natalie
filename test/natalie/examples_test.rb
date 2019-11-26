63e92ba95c9d6b5c636ffb70629d0b6acf6ce8fe
Tim Morgan <tim@timmorgan.org>
2019-11-25 18:53:30 -0600

    Print result if there is an error

--- test/natalie/examples_test.rb
+++ test/natalie/examples_test.rb
@@ -7,8 +7,10 @@ describe 'Examples' do
     describe path do
       it 'has the same output in ruby and natalie' do
         out_nat = `bin/natalie #{path} 2>&1`
+        puts out_nat unless $?.to_i == 0
         $?.to_i.must_equal 0
         out_ruby = `ruby #{path} 2>&1`
+        puts out_ruby unless $?.to_i == 0
         $?.to_i.must_equal 0
         out_nat.must_equal(out_ruby)
       end
