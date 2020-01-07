74f0ff510e7878eb956586a0f765c4ad91edad25
Tim Morgan <tim@timmorgan.org>
2020-01-06 20:24:09 -0600

    Add Comparable#< and Comparable#> methods

--- src/nat_comparable.c
+++ src/nat_comparable.c
@@ -23,3 +23,23 @@ NatObject *Comparable_neq(NatEnv *env, NatObject *self, size_t argc, NatObject *
         return env_get(env, "true");
     }
 }
+
+NatObject *Comparable_lt(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block) {
+    NAT_ASSERT_ARGC(1);
+    NatObject *result = nat_send(env, self, "<=>", argc, args, NULL);
+    if (result->type == NAT_VALUE_INTEGER && result->integer < 0) {
+        return env_get(env, "true");
+    } else {
+        return env_get(env, "false");
+    }
+}
+
+NatObject *Comparable_gt(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block) {
+    NAT_ASSERT_ARGC(1);
+    NatObject *result = nat_send(env, self, "<=>", argc, args, NULL);
+    if (result->type == NAT_VALUE_INTEGER && result->integer > 0) {
+        return env_get(env, "true");
+    } else {
+        return env_get(env, "false");
+    }
+}
--- src/nat_comparable.h
+++ src/nat_comparable.h
@@ -6,8 +6,12 @@
 #define COMPARABLE_INIT() \
     nat_define_method(Comparable, "==", Comparable_eqeq); \
     nat_define_method(Comparable, "!=", Comparable_neq); \
+    nat_define_method(Comparable, "<", Comparable_lt); \
+    nat_define_method(Comparable, ">", Comparable_gt);
 
 NatObject *Comparable_eqeq(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block);
 NatObject *Comparable_neq(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block);
+NatObject *Comparable_lt(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block);
+NatObject *Comparable_gt(NatEnv *env, NatObject *self, size_t argc, NatObject **args, struct hashmap *kwargs, NatBlock *block);
 
 #endif
