33126f6922bb3cd37d4bf5839ea0beb12a96dece
Tim Morgan <tim@timmorgan.org>
2020-02-08 14:22:19 -0800

    Unify the two spec libraries
    We're mimicing mspec until we can actually compile mspec...

--- test/support/ruby_require_patch.rb
+++ test/support/ruby_require_patch.rb
@@ -2,4 +2,9 @@ module Kernel
   define_method(:require) do |filename|
     load filename + '.nat'
   end
+
+  define_method(:require_relative) do |filename|
+    path = File.split(caller.first.split(':').first).first
+    load File.join(path, filename + '.nat')
+  end
 end
