8a23820f57204167de9a3b9b6a929d5581ea6d5d
Tim Morgan <tim@timmorgan.org>
2020-04-15 19:50:00 -0500

    Properly inspect symbols with spaces and other non-alpha chars

--- src/symbol.c
+++ src/symbol.c
@@ -11,6 +11,14 @@ NatObject *Symbol_inspect(NatEnv *env, NatObject *self, size_t argc, NatObject *
     assert(NAT_TYPE(self) == NAT_VALUE_SYMBOL);
     NAT_ASSERT_ARGC(0);
     NatObject *str = nat_string(env, ":");
+    size_t len = strlen(self->symbol);
+    for (size_t i = 0; i < len; i++) {
+        char c = self->symbol[i];
+        if (!((c >= 65 && c <= 90) || (c >= 97 && c <= 122))) {
+            nat_string_append_nat_string(env, str, nat_send(env, nat_string(env, self->symbol), "inspect", 0, NULL, NULL));
+            return str;
+        }
+    };
     nat_string_append(env, str, self->symbol);
     return str;
 }
