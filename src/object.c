db4d550a22ac399e43d1c6d54889d4ad2295f2a5
Tim Morgan <tim@timmorgan.org>
2020-05-09 15:22:22 -0500

    I don't think this comment is accurate

--- src/object.c
+++ src/object.c
@@ -2,7 +2,6 @@
 #include "gc.h"
 #include "natalie.h"
 
-// TODO: move to BasicObject
 NatObject *Object_new(NatEnv *env, NatObject *self, ssize_t argc, NatObject **args, NatBlock *block) {
     NatObject *obj = nat_alloc(env, self, NAT_VALUE_OTHER);
     return nat_initialize(env, obj, argc, args, block);
