0f0e1f2047883f46c17ff0d5405803125a714652
Tim Morgan <tim@timmorgan.org>
2020-07-07 22:18:43 -0500

    Start work on StringScanner
    We'll need this to get ruby_parser working...

---
+++ test/lib/strscan_test.rb
@@ -0,0 +1,24 @@
+require_relative '../spec_helper'
+require 'strscan'
+
+describe 'StringScanner' do
+  it 'works' do
+    s = StringScanner.new('This is an example string')
+    s.eos?.should == false
+    s.scan(/\w+/).should == "This"
+    s.scan(/\w+/).should == nil
+    s.scan(/\s+/).should == " "
+    s.scan(/\s+/).should == nil
+    s.scan(/\w+/).should == "is"
+    s.eos?.should == false
+    s.scan(/\s+/).should == " "
+    s.scan(/\w+/).should == "an"
+    s.scan(/\s+/).should == " "
+    s.scan(/\w+/).should == "example"
+    s.scan(/\s+/).should == " "
+    s.scan(/\w+/).should == "string"
+    s.eos?.should == true
+    s.scan(/\s+/).should == nil
+    s.scan(/\w+/).should == nil
+  end
+end
