3f277917328b2cf710a7412760400b529868f0f7
Tim Morgan <tim@timmorgan.org>
2020-08-09 09:10:39 -0500

    Fix building of external libs

--- ext/bdwgc.cmake
+++ ext/bdwgc.cmake
@@ -3,14 +3,8 @@ set(BDWGC_SRC "${PROJECT_SOURCE_DIR}/ext/bdwgc")
 include(ExternalProject)
 
 ExternalProject_Add(bdwgc
-  EXCLUDE_FROM_ALL true
   BUILD_IN_SOURCE TRUE
   SOURCE_DIR ${BDWGC_SRC}
   CONFIGURE_COMMAND ./autogen.sh
             COMMAND ./configure --enable-cplusplus --enable-redirect-malloc --disable-threads --enable-static --with-pic
   BUILD_COMMAND $(MAKE))
-
-set(BDWGC_STATIC_LIB "${BDWGC_SRC}/.libs/libgccpp.a")
-add_library(libbdwgc STATIC IMPORTED GLOBAL)
-add_dependencies(libbdwgc bdwgc)
-set_target_properties(libbdwgc PROPERTIES IMPORTED_LOCATION ${BDWGC_STATIC_LIB})
