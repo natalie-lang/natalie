744b2fabaf1efda366a16d3cccae547f66bd797a
AnotherTest <ali.mpfard@gmail.com>
2020-08-12 05:51:39 +0430

    Build: General CMake-y improvements
    - Move all (needed) built external libraries to build dir
    - Make root CMakeFiles less verbose ('fancy')
    - 'Fix' `compiler_command` to be explicit about library dependencies
    - Swap out LIB_PATHS to LIB_NAMES for the compiler invocation

--- ext/onigmo.cmake
+++ ext/onigmo.cmake
@@ -1,9 +1,26 @@
 set(ONIGMO_SRC "${PROJECT_SOURCE_DIR}/ext/onigmo")
 
-include(ExternalProject)
+set(ONIGMO_LIB "${CMAKE_BINARY_DIR}/libonigmo.a")
+set(ONIGMO_BUILD_DIR "${ONIGMO_SRC}/build")
 
-ExternalProject_Add(onigmo
-  BUILD_IN_SOURCE TRUE
-  SOURCE_DIR ${ONIGMO_SRC}
-  CONFIGURE_COMMAND ./configure --with-pic --disable-shared --enable-static
-  BUILD_COMMAND $(MAKE))
+add_custom_target(make_onigmo_build_dir
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${ONIGMO_BUILD_DIR})
+
+add_custom_command(
+    OUTPUT "${ONIGMO_LIB}"
+    BYPRODUCTS "${ONIGMO_BUILD_DIR}/libonigmo.a"
+    DEPENDS make_onigmo_build_dir
+    WORKING_DIRECTORY ${ONIGMO_SRC}
+    COMMAND sh autogen.sh
+    COMMAND mkdir -p build
+    COMMAND ./configure --with-pic --prefix "${ONIGMO_BUILD_DIR}"
+    COMMAND make
+    COMMAND make install
+    COMMAND make clean
+    COMMAND ${CMAKE_COMMAND} -E copy_directory "${ONIGMO_BUILD_DIR}/include" "${CMAKE_BINARY_DIR}/include"
+    COMMAND ${CMAKE_COMMAND} -E copy "${ONIGMO_BUILD_DIR}/lib/libonigmo.a" "${ONIGMO_LIB}")
+
+add_custom_target(onigmo
+    DEPENDS "${ONIGMO_LIB}")
+
+set(ONIGMO_TARGET onigmo)
