fddc24f8fe6221e68369a298098e8c68ec6e2e5a
AnotherTest <ali.mpfard@gmail.com>
2020-08-12 05:51:39 +0430

    Build: General CMake-y improvements
    - Move all (needed) built external libraries to build dir
    - Make root CMakeFiles less verbose ('fancy')
    - 'Fix' `compiler_command` to be explicit about library dependencies
    - Swap out LIB_PATHS to LIB_NAMES for the compiler invocation

--- ext/hashmap.cmake
+++ ext/hashmap.cmake
@@ -1,10 +1,12 @@
 set(HASHMAP_SRC "${PROJECT_SOURCE_DIR}/ext/hashmap")
 
-include(ExternalProject)
+add_subdirectory(${HASHMAP_SRC})
 
-ExternalProject_Add(hashmap
-  BINARY_DIR ${HASHMAP_SRC}/build
-  SOURCE_DIR ${HASHMAP_SRC}
-  BUILD_COMMAND CFLAGS="-fPIC" make
-  BUILD_BYPRODUCTS ${HASHMAP_SRC}/build/libhashmap.a
-  INSTALL_COMMAND "")
+target_compile_options(hashmap PRIVATE "-fPIC")
+
+add_custom_target(hashmap_dep
+    COMMAND ${CMAKE_COMMAND} -E copy "ext/hashmap/libhashmap.a" "libhashmap.a"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory "${HASHMAP_SRC}/include" "include"
+    DEPENDS hashmap)
+
+set(HASHMAP_TARGET hashmap_dep)
