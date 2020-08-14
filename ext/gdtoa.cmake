1c90b90f6d917a1d92226a0e691e705ad7c59b5e
Tim Morgan <tim@timmorgan.org>
2020-08-13 20:09:38 -0500

    Build gdtoa outside of source tree
    We like that clean git index...

--- ext/gdtoa.cmake
+++ ext/gdtoa.cmake
@@ -1,7 +1,7 @@
 set(GDTOA_SRC "${PROJECT_SOURCE_DIR}/ext/gdtoa")
 
 set(GDTOA_LIB "${CMAKE_BINARY_DIR}/libgdtoa.a")
-set(GDTOA_BUILD_DIR "${GDTOA_SRC}/build")
+set(GDTOA_BUILD_DIR "${CMAKE_BINARY_DIR}/gdtoa-build")
 
 add_custom_target(make_gdtoa_build_dir
     COMMAND ${CMAKE_COMMAND} -E make_directory ${GDTOA_BUILD_DIR})
@@ -12,7 +12,7 @@ add_custom_command(
     DEPENDS make_gdtoa_build_dir
     WORKING_DIRECTORY ${GDTOA_SRC}
     COMMAND sh autogen.sh
-    COMMAND mkdir -p build
+    COMMAND mkdir -p ${GDTOA_BUILD_DIR}
     COMMAND ./configure --with-pic --prefix "${GDTOA_BUILD_DIR}"
     COMMAND make
     COMMAND make install
@@ -20,7 +20,7 @@ add_custom_command(
     COMMAND ${CMAKE_COMMAND} -E copy_directory "${GDTOA_BUILD_DIR}/include" "${CMAKE_BINARY_DIR}/include"
     COMMAND ${CMAKE_COMMAND} -E copy "${GDTOA_SRC}/*.h" "${CMAKE_BINARY_DIR}/include/gdtoa" # gdtoa does not install all its headers :(
     COMMAND make clean
-    )
+    COMMAND ${CMAKE_COMMAND} -E remove ${GDTOA_SRC}/compile)
 
 add_custom_target(gdtoa
     DEPENDS "${GDTOA_LIB}" "${GDTOA_CPPLIB}")
