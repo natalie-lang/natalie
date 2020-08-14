a3032481f5e636149aba13fbe5dab4fd2f2beffa
Tim Morgan <tim@timmorgan.org>
2020-08-13 22:12:17 -0500

    Fix hashmap dependency so it doesn't cause a rebuild every time

--- ext/hashmap.cmake
+++ ext/hashmap.cmake
@@ -4,9 +4,18 @@ add_subdirectory(${HASHMAP_SRC})
 
 target_compile_options(hashmap PRIVATE "-fPIC")
 
-add_custom_target(hashmap_dep
-    COMMAND ${CMAKE_COMMAND} -E copy "ext/hashmap/libhashmap.a" "libhashmap.a"
-    COMMAND ${CMAKE_COMMAND} -E copy_directory "${HASHMAP_SRC}/include" "include"
-    DEPENDS hashmap)
+add_custom_command(
+  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/libhashmap.a
+  COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_BINARY_DIR}/ext/hashmap/libhashmap.a" "libhashmap.a"
+  DEPENDS hashmap)
+
+add_custom_command(
+  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/include/hashmap.h
+  COMMAND ${CMAKE_COMMAND} -E copy "${HASHMAP_SRC}/include/hashmap.h" "include/hashmap.h"
+  DEPENDS hashmap)
+
+add_custom_target(hashmap_dep DEPENDS
+  ${CMAKE_CURRENT_BINARY_DIR}/include/hashmap.h
+  ${CMAKE_CURRENT_BINARY_DIR}/libhashmap.a)
 
 set(HASHMAP_TARGET hashmap_dep)
