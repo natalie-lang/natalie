natalie-lang/natalie

commit fef995e52e0d710d57a1686ff55cbcae55acfbde

Fix post-conditional while and until
I didn't know what the fourth value in the sexp represented until now!

lib/natalie/compiler/pass1.rb
@@ -809,33 +809,45 @@ module Natalie
       end
 
       def process_until(exp)
-        (_, condition, body, unknown) = exp
-        raise 'check this out' if unknown != true # NOTE: I don't know what this is; it always seems to be true
+        (_, condition, body, pre) = exp
         body ||= s(:nil)
         result_name = temp('while_result')
+        condition_and_body = if pre
+                               s(:block,
+                                 s(:c_if, s(:is_truthy, process(condition)), s(:c_break)),
+                                 process(body))
+                             else
+                               s(:block,
+                                 process(body),
+                                 s(:c_if, s(:is_truthy, process(condition)), s(:c_break)))
+                             end
         loop_context(result_name) do
           exp.new(:block,
                   s(:declare, result_name, s(:nil)),
                   s(:c_while, 'true',
-                    s(:block,
-                      s(:c_if, s(:is_truthy, process(condition)), s(:c_break)),
-                      process(body))),
-          result_name)
+                    condition_and_body),
+            result_name)
         end
       end
 
       def process_while(exp)
-        (_, condition, body, unknown) = exp
-        raise 'check this out' if unknown != true # NOTE: I don't know what this is; it always seems to be true
+        (_, condition, body, pre) = exp
         body ||= s(:nil)
         result_name = temp('while_result')
+        condition_and_body = if pre
+                               s(:block,
+                                 s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break)),
+                                 process(body))
+                             else
+                               s(:block,
+                                 process(body),
+                                 s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break)))
+                             end
         loop_context(result_name) do
           exp.new(:block,
                   s(:declare, result_name, s(:nil)),
                   s(:c_while, 'true',
-                    s(:block,
-                      s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break)),
-                      process(body))),
+                    condition_and_body),
           result_name)
         end
       end

test/natalie/loop_test.rb
@@ -1,7 +1,7 @@
 require_relative '../spec_helper'
 
 describe 'while' do
-  it 'works' do
+  it 'works as a pre-condition' do
     r = []
     x = 10
     while x != 0
@@ -10,16 +10,36 @@ describe 'while' do
     end
     r.should == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   end
+
+  it 'works as a post-condition' do
+    r = []
+    x = 10
+    begin
+      r << x
+      x = x - 1
+    end while x != 0
+    r.should == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+  end
 end
 
 describe 'until' do
-  it 'works' do
+  it 'works as a pre-condition' do
     r = []
     x = 10
     until x == 0
       r << x
       x = x - 1
     end
+    r.should == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+  end
+
+  it 'works as a post-condition' do
+    r = []
+    x = 10
+    begin
+      r << x
+      x = x - 1
+    end until x == 0
     r.should == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   end
 end

commit 937480b667dd3aa952da9c6367f66d133693f795

Fix break in while/until loop
I broke this in fef995e52e0d710d57a1686ff55cbcae55acfbde, whoops!

lib/natalie/compiler/pass1.rb
@@ -812,20 +812,16 @@ module Natalie
         (_, condition, body, pre) = exp
         body ||= s(:nil)
         result_name = temp('while_result')
-        condition_and_body = if pre
-                               s(:block,
-                                 s(:c_if, s(:is_truthy, process(condition)), s(:c_break)),
-                                 process(body))
-                             else
-                               s(:block,
-                                 process(body),
-                                 s(:c_if, s(:is_truthy, process(condition)), s(:c_break)))
-                             end
         loop_context(result_name) do
+          condition = s(:c_if, s(:is_truthy, process(condition)), s(:c_break)),
+          condition_and_body = if pre
+                                 s(:block, condition, process(body))
+                               else
+                                 s(:block, process(body), condition)
+                               end
           exp.new(:block,
                   s(:declare, result_name, s(:nil)),
-                  s(:c_while, 'true',
-                    condition_and_body),
+                  s(:c_while, 'true', condition_and_body),
             result_name)
         end
       end
@@ -834,20 +830,16 @@ module Natalie
         (_, condition, body, pre) = exp
         body ||= s(:nil)
         result_name = temp('while_result')
-        condition_and_body = if pre
-                               s(:block,
-                                 s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break)),
-                                 process(body))
-                             else
-                               s(:block,
-                                 process(body),
-                                 s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break)))
-                             end
         loop_context(result_name) do
+          condition = s(:c_if, s(:not, s(:is_truthy, process(condition))), s(:c_break))
+          condition_and_body = if pre
+                                 s(:block, condition, process(body))
+                               else
+                                 s(:block, process(body), condition)
+                               end
           exp.new(:block,
                   s(:declare, result_name, s(:nil)),
-                  s(:c_while, 'true',
-                    condition_and_body),
+                  s(:c_while, 'true', condition_and_body),
           result_name)
         end
       end