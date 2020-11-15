385d0f8808f420c26969a19065332aee3027dd83
Tim Morgan <tim@timmorgan.org>
2020-11-15 11:43:31 -0600

    Build onigmo outside the source tree, in the build directory

--- ext/onigmo.cmake
+++ ext/onigmo.cmake
@@ -1,7 +1,7 @@
 set(ONIGMO_SRC "${PROJECT_SOURCE_DIR}/ext/onigmo")
 
 set(ONIGMO_LIB "${CMAKE_BINARY_DIR}/libonigmo.a")
-set(ONIGMO_BUILD_DIR "${ONIGMO_SRC}/build")
+set(ONIGMO_BUILD_DIR "${CMAKE_BINARY_DIR}/onigmo-build")
 
 add_custom_target(make_onigmo_build_dir
     COMMAND ${CMAKE_COMMAND} -E make_directory ${ONIGMO_BUILD_DIR})
@@ -10,13 +10,12 @@ add_custom_command(
     OUTPUT "${ONIGMO_LIB}"
     BYPRODUCTS "${ONIGMO_BUILD_DIR}/libonigmo.a"
     DEPENDS make_onigmo_build_dir
-    WORKING_DIRECTORY ${ONIGMO_SRC}
+    COMMAND ${CMAKE_COMMAND} -E copy_directory "${ONIGMO_SRC}" "${ONIGMO_BUILD_DIR}"
+    WORKING_DIRECTORY ${ONIGMO_BUILD_DIR}
     COMMAND sh autogen.sh
-    COMMAND mkdir -p build
     COMMAND ./configure --with-pic --prefix "${ONIGMO_BUILD_DIR}"
     COMMAND make
     COMMAND make install
-    COMMAND make clean
     COMMAND ${CMAKE_COMMAND} -E copy_directory "${ONIGMO_BUILD_DIR}/include" "${CMAKE_BINARY_DIR}/include"
     COMMAND ${CMAKE_COMMAND} -E copy "${ONIGMO_BUILD_DIR}/lib/libonigmo.a" "${ONIGMO_LIB}")
 
