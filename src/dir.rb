b84af30704959591f6c2bca22845c41da6ca3375
Tim Morgan <tim@timmorgan.org>
2020-12-20 10:08:29 -0600

    Add Dir.each_child method

--- src/dir.rb
+++ src/dir.rb
@@ -1,5 +1,44 @@
+require 'natalie/inline'
+
+__inline__ "#include <sys/types.h>"
+__inline__ "#include <dirent.h>"
+
 class Dir
   def self.tmpdir
     '/tmp'
+  end
+
+  def self.each_child(dirname)
+    __inline__ <<-END
+        auto dirname = args[0];
+        dirname->assert_type(env, Value::Type::String, "String");
+        if (!block) {
+            Value *enum_for_args[] = { SymbolValue::intern(env, "each_child"), args[0] };
+            return self->send(env, "enum_for", 2, enum_for_args, nullptr);
+        }
+        auto dir = opendir(dirname->as_string()->c_str());
+        if (!dir) {
+            Value *args[] = { new IntegerValue { env, errno } };
+            auto exception = env->Object()->const_fetch("SystemCallError")->send(env, "exception", 1, args)->as_exception();
+            env->raise_exception(exception);
+        }
+        errno = 0;
+        dirent *entry;
+        for (;;) {
+            entry = readdir(dir);
+            if (!entry) break;
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+            Value *args[] = { new StringValue { env, entry->d_name } };
+            NAT_RUN_BLOCK_AND_POSSIBLY_BREAK(env, block, 1, args, nullptr);
+        }
+        if (errno) {
+            closedir(dir);
+            Value *args[] = { new IntegerValue { env, errno } };
+            auto exception = env->Object()->const_fetch("SystemCallError")->send(env, "exception", 1, args)->as_exception();
+            env->raise_exception(exception);
+        }
+        closedir(dir);
+        return env->nil_obj();
+    END
   end
 end
---
+++ test/natalie/dir_test.rb
@@ -0,0 +1,39 @@
+require_relative '../spec_helper'
+
+describe 'Dir' do
+  describe '#each_child' do
+    it 'raises an error if given a bad path' do
+      -> { Dir.each_child('this_directory_does_not_exist') {  } }.should raise_error(SystemCallError)
+    end
+
+    it 'yields each entry in the named directory except . and ..' do
+      entries = []
+      Dir.each_child(File.expand_path('../support', __dir__)) do |entry|
+        entries << entry
+      end
+      entries.sort.should == [
+        'file.txt',
+        'require_sub1.rb',
+        'require_sub2.rb',
+        'require_sub3.rb',
+        'require_sub4.foo',
+        'require_sub4.foo.rb',
+        'spec.rb',
+      ]
+    end
+
+    it 'returns an Enumerator when no block is given' do
+      dir = Dir.each_child(File.expand_path('../support', __dir__))
+      dir.should be_an_instance_of(Enumerator)
+      dir.to_a.sort.should == [
+        'file.txt',
+        'require_sub1.rb',
+        'require_sub2.rb',
+        'require_sub3.rb',
+        'require_sub4.foo',
+        'require_sub4.foo.rb',
+        'spec.rb',
+      ]
+    end
+  end
+end
