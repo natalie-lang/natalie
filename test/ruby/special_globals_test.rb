4504d04f837d05a17466d0c9e5fb00290252aa77
Tim Morgan <tim@timmorgan.org>
2021-01-19 20:56:52 -0600

    Parallelize both tests here :^)

--- test/ruby/special_globals_test.rb
+++ test/ruby/special_globals_test.rb
@@ -1,20 +1,22 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
-describe '$0' do
+describe 'special globals' do
   parallelize_me!
 
-  it 'returns the path to the initial ruby script executed/compiled' do
-    expect(`bin/natalie test/support/dollar_zero.rb`.strip).must_equal '"test/support/dollar_zero.rb"'
-    `bin/natalie -c test/tmp/dollar_zero test/support/dollar_zero.rb`
-    expect(`test/tmp/dollar_zero`.strip).must_equal '"test/support/dollar_zero.rb"'
+  describe '$0' do
+    it 'returns the path to the initial ruby script executed/compiled' do
+      expect(`bin/natalie test/support/dollar_zero.rb`.strip).must_equal '"test/support/dollar_zero.rb"'
+      `bin/natalie -c test/tmp/dollar_zero test/support/dollar_zero.rb`
+      expect(`test/tmp/dollar_zero`.strip).must_equal '"test/support/dollar_zero.rb"'
+    end
   end
-end
 
-describe '$exe' do
-  it 'returns the path to the compiled binary exe' do
-    expect(`bin/natalie test/support/dollar_exe.rb`.strip).must_match /natalie/ # usually something like: /tmp/natalie20210119-2242842-eystnh
-    `bin/natalie -c test/tmp/dollar_exe test/support/dollar_exe.rb`
-    expect(`test/tmp/dollar_exe`.strip).must_equal '"test/tmp/dollar_exe"'
+  describe '$exe' do
+    it 'returns the path to the compiled binary exe' do
+      expect(`bin/natalie test/support/dollar_exe.rb`.strip).must_match /natalie/ # usually something like: /tmp/natalie20210119-2242842-eystnh
+      `bin/natalie -c test/tmp/dollar_exe test/support/dollar_exe.rb`
+      expect(`test/tmp/dollar_exe`.strip).must_equal '"test/tmp/dollar_exe"'
+    end
   end
 end
