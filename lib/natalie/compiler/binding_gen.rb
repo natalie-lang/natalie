ce48f49d17c9c191895d192d2d8ac74dd3f8b839
Richard Böhme <richard.boehme1999@gmail.com>
2021-09-23 23:52:26 +0200

    Make Hash#initialize spec compliant
    [#96]

--- lib/natalie/compiler/binding_gen.rb
+++ lib/natalie/compiler/binding_gen.rb
@@ -499,7 +499,7 @@ gen.binding('Hash', 'fetch', 'HashValue', 'fetch', argc: 1..2, pass_env: true, p
 gen.binding('Hash', 'fetch_values', 'HashValue', 'fetch_values', argc: :any, pass_env: true, pass_block: true, return_type: :Value)
 gen.binding('Hash', 'has_key?', 'HashValue', 'has_key', argc: 1, pass_env: true, pass_block: false, return_type: :Value)
 gen.binding('Hash', 'has_value?', 'HashValue', 'has_value', argc: 1, pass_env: true, pass_block: false, return_type: :Value)
-gen.binding('Hash', 'initialize', 'HashValue', 'initialize', argc: 0..1, pass_env: true, pass_block: true, return_type: :Value)
+gen.binding('Hash', 'initialize', 'HashValue', 'initialize', argc: 0..1, pass_env: true, pass_block: true, return_type: :Value, visibility: :private)
 gen.binding('Hash', 'initialize_copy', 'HashValue', 'replace', argc: 1, pass_env: true, pass_block: false, return_type: :Value)
 gen.binding('Hash', 'inspect', 'HashValue', 'inspect', argc: 0, pass_env: true, pass_block: false, return_type: :Value)
 gen.binding('Hash', 'include?', 'HashValue', 'has_key', argc: 1, pass_env: true, pass_block: false, return_type: :Value)
