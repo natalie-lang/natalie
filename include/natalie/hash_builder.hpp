abb417a60d8cfb2e197b859bd5bef4cf59158e19
TheGrizzlyDev <tonio9681@gmail.com>
2021-09-26 12:21:28 +0100

    Add option in HashBuilder to keep digesting without shifting so ordering of the elements does not matter.

--- include/natalie/hash_builder.hpp
+++ include/natalie/hash_builder.hpp
@@ -6,19 +6,24 @@ namespace Natalie {
 class HashBuilder {
 public:
     HashBuilder()
-        : HashBuilder { 100019 } { }
+        : HashBuilder { 100019, true } { }
 
-    HashBuilder(nat_int_t starting_digest)
-        : m_digest(starting_digest) { }
+    HashBuilder(nat_int_t starting_digest, bool does_order_matter)
+        : m_digest(starting_digest)
+        , m_does_order_matter(does_order_matter) { }
 
     void append(nat_int_t hash) {
-        m_digest += (m_digest << 5) ^ hash;
+        if (m_does_order_matter)
+            m_digest += (m_digest << 5) ^ hash;
+        else 
+            m_digest ^= hash;
     }
 
     nat_int_t digest() { return m_digest; }
 
 private:
     nat_int_t m_digest;
+    bool m_does_order_matter;
 };
 
 }
