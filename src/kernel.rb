3c09b2c51ec824c119d05200cc5bf33c364e0f18
Richard Böhme <richard.boehme1999@gmail.com>
2021-09-30 22:11:55 +0200

    Move Kernel#<=> to Kernel module (+ spec compliant)

--- src/kernel.rb
+++ src/kernel.rb
@@ -1,4 +1,10 @@
 module Kernel
+  def <=>(other)
+    if other.object_id == self.object_id || (!is_a?(Comparable) && self == other)
+      0
+    end
+  end
+
   def enum_for(method = :each, *args)
     size = block_given? ? yield : nil
     Enumerator.new(size) do |yielder|
