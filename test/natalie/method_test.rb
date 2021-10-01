c1e02472db3222f60defca7752308c52961c7709
Richard Böhme <richard.boehme1999@gmail.com>
2021-10-01 17:21:43 +0200

    Sort keyword argument test method calls

--- test/natalie/method_test.rb
+++ test/natalie/method_test.rb
@@ -439,10 +439,9 @@ describe 'method with keyword args' do
     method_with_kwargs8(a: 1).should == [1, nil]
     method_with_kwargs9.should == [1, 2]
     method_with_kwargs9('a').should == ['a', 2]
+    method_with_kwargs9(b: 'b').should == [1, 'b']
     method_with_kwargs9('a', b: 'b').should == ['a', 'b']
     method_with_kwargs10(b: 'b').should == [1]
-
-    method_with_kwargs9(b: 'b').should == [1, 'b']
     method_with_kwargs11(a: 'a', b: 'b').should == ['a', { b: 'b' }]
   end
 
