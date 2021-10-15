8ea4a8fdede45f08863cb2a3c171280b125bb65e
Richard Böhme <richard.boehme1999@gmail.com>
2021-10-15 22:53:40 +0200

    Make Array#values_at spec compliant
    [#39]

--- spec/core/array/values_at_spec.rb
+++ spec/core/array/values_at_spec.rb
@@ -22,12 +22,12 @@ describe "Array#values_at" do
   end
 
   describe "when passed ranges" do
-    xit "returns an array of elements in the ranges" do
+    it "returns an array of elements in the ranges" do
       [1, 2, 3, 4, 5].values_at(0..2, 1...3, 2..-2).should == [1, 2, 3, 2, 3, 3, 4]
       [1, 2, 3, 4, 5].values_at(6..4).should == []
     end
 
-    xit "calls to_int on arguments of ranges" do
+    it "calls to_int on arguments of ranges" do
       from = mock('from')
       to = mock('to')
 
@@ -44,23 +44,24 @@ describe "Array#values_at" do
   end
 
   describe "when passed a range" do
-    xit "fills with nil if the index is out of the range" do
+    it "fills with nil if the index is out of the range" do
       [0, 1].values_at(0..3).should == [0, 1, nil, nil]
       [0, 1].values_at(2..4).should == [nil, nil, nil]
     end
 
     describe "on an empty array" do
-      xit "fills with nils if the index is out of the range" do
+      it "fills with nils if the index is out of the range" do
         [].values_at(0..2).should == [nil, nil, nil]
         [].values_at(1..3).should == [nil, nil, nil]
       end
     end
   end
 
-  xit "does not return subclass instance on Array subclasses" do
+  it "does not return subclass instance on Array subclasses" do
     ArraySpecs::MyArray[1, 2, 3].values_at(0, 1..2, 1).should be_an_instance_of(Array)
   end
 
+  # NATFIXME: Implement endless ranges and eval
   xit "works when given endless ranges" do
     [1, 2, 3, 4].values_at(eval("(1..)")).should == [2, 3, 4]
     [1, 2, 3, 4].values_at(eval("(3...)")).should == [4]
