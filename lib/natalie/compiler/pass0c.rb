677171985fea2b510eedb0cdf95488d47ab8f1ea
Hendiadyoin1 <leon2002.la@gmail.com>
2022-01-02 15:19:38 +0100

    Collapse if-statements when we know the condition at compile-time
    This also adds constant evaluation for conditionals.
    
    We also do not collapse some conditional assignments, due to us
    not allocating variables in some cases.

--- lib/natalie/compiler/pass0c.rb
+++ lib/natalie/compiler/pass0c.rb
@@ -33,6 +33,53 @@ module Natalie
         return exp
       end
 
+      def type_is_always_truthy(arg)
+        # FIXME: Are there any other types that are always truthy?
+        return [:true, :str, :lit, :array].include?(arg)
+      end
+      def type_is_always_falsey(arg)
+        return [:false, :nil].include?(arg)
+      end
+      def type_is_const_evaluable(arg)
+        return [:true, :str, :lit, :array, :false, :nil].include?(arg)
+      end
+
+      def process_if(exp, block_with_args=nil)
+        exp = exp.new(*exp.map { |e| process_atom(e) })
+        if exp.flatten.include?(:lasgn)
+          # This is required for now because
+          # ```rb
+          # x = 1 unless true # s(:if, s(:true), nil, s(:lasgn, :x, s(:lit, 1)))
+          # ```
+          # gets converted to an if-statement, that does not allocate x in the
+          # taken branch, which makes x undefined
+          return exp
+        end
+        (_, (boolean, *), true_case, false_case) = exp
+        return true_case  if type_is_always_truthy(boolean)
+        return false_case if type_is_always_falsey(boolean)
+
+        return exp
+      end
+
+      def process_or(exp, block_with_args=nil)
+        exp = exp.new(*exp.map { |e| process_atom(e) })
+        _, (t1, *arg1), (t2, *arg2) = exp
+        return exp.new(t2, *arg2) if type_is_always_falsey(t1)
+        return exp.new(t1, *arg1) if type_is_always_truthy(t1)
+
+        return exp
+      end
+
+      def process_and(exp, block_with_args=nil)
+        exp = exp.new(*exp.map { |e| process_atom(e) })
+        _, (t1, *arg1), (t2, *arg2) = exp
+        return s(t1, *arg1) if type_is_always_falsey(t1)
+        return exp          if !type_is_always_truthy(t1) # t1 is not const-evaluable
+        # t2 will be returned even if it is falsey, so we don't have to check it
+        return exp.new(t2, *arg2)
+      end
+
       def process_call(exp, block_with_args=nil)
         exp = exp.new(*exp.map { |e| process_atom(e) })
         (_, receiver, method, *args) = exp
@@ -75,7 +122,28 @@ module Natalie
                 return canary_const_shl(exp, values) if [class_arg1, class_arg2].all? {|x| x == Integer}
               when :>>
                 return canary_const_shr(exp, values) if [class_arg1, class_arg2].all? {|x| x == Integer}
+              when :==
+                return canary_const_eq(exp, values)
+              when :!=
+                return canary_const_neq(exp,values)
+              when :>
+                return canary_const_gt(exp, values)
+              when :>=
+                return canary_const_geq(exp, values)
+              when :<
+                return canary_const_lt(exp, values)
+              when :<=
+                return canary_const_leq(exp, values)
+              when :===
+                return canary_const_eqeq(exp, values)
+              when :<=>
+                return canary_const_cmp(exp, values)
               end
+            end
+          elsif args.length == 0
+            if method == :!
+              # This can fail and fallback, so we should not try to process it again
+              return canary_const_not(exp, values)
             end
           end
         end
@@ -96,6 +164,7 @@ module Natalie
         return exp.new(:if, exp.new(:call, first, :>, second), canary_array_max(exp, [first, *rest]), canary_array_max(exp, [second, *rest]))
       end
 
+      # FIXME: These are pretty repetitive, maybe do these with meta programming
       def canary_const_addition(exp, values)
         _, (_, summand1), op, (_, summand2) = exp
         return s(:lit, summand1 + summand2)
@@ -139,6 +208,52 @@ module Natalie
       def canary_const_shr(exp, values)
         _, (_, arg1), op, (_, arg2) = exp
         return s(:lit, arg1 >> arg2)
+      end
+      def canary_const_eq(exp,values)
+        _, (_, arg1), leq, (_, arg2) = exp
+        return s(:true) if arg1 == arg2
+        return s(:false)
+      end
+      def canary_const_neq(exp,values)
+        _, (_, arg1), leq, (_, arg2) = exp
+        return s(:true) if arg1 != arg2
+        return s(:false)
+      end
+      def canary_const_gt(exp,values)
+        _, (_, arg1), leq, (_, arg2) = exp
+        return s(:true) if arg1 > arg2
+        return s(:false)
+      end
+      def canary_const_geq(exp,values)
+        _, (_, arg1), leq, (_, arg2) = exp
+        return s(:true) if arg1 >= arg2
+        return s(:false)
+      end
+      def canary_const_lt(exp,values)
+        _, (_,arg1), leq, (_,arg2) = exp
+        return s(:true) if arg1 < arg2
+        return s(:false)
+      end
+      def canary_const_leq(exp,values)
+        _, (_, arg1), leq, (_, arg2) = exp
+        return s(:true) if arg1 <= arg2
+        return s(:false)
+      end
+      def canary_const_eqeq(exp,values)
+        _, (_, arg1), eqeq, (_, arg2) = exp
+        return s(:true) if arg1 === arg2
+        return s(:false)
+      end
+      def canary_const_cmp(exp,values)
+        _, (_, arg1), cmp, (_, arg2) = exp
+        return s(:lit, arg1 <=> arg2)
+      end
+
+      def canary_const_not(exp, values)
+        _, (type,*), _not = exp
+        return exp.new(:false) if type_is_always_truthy(type)
+        return exp.new(:true) if type_is_always_falsey(type)
+        return exp
       end
     end
   end
