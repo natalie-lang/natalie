190c6db9b9b514395fd790fe4ffb44f7c8acf5cb
Tim Morgan <tim@timmorgan.org>
2022-01-05 02:03:03 +0000

    Format code with clang-format (automated)

--- include/natalie/method_info.hpp
+++ include/natalie/method_info.hpp
@@ -9,12 +9,12 @@ class MethodInfo : public Cell {
 public:
     MethodInfo(const char *name, MethodVisibility visibility)
         : m_name { name }
-        , m_visibility { visibility } {}
+        , m_visibility { visibility } { }
 
     MethodVisibility visibility() { return m_visibility; }
     void set_visibility(MethodVisibility visibility) { m_visibility = visibility; }
 
-    virtual void visit_children(Visitor &visitor) override final {}
+    virtual void visit_children(Visitor &visitor) override final { }
 
     virtual void gc_inspect(char *buf, size_t len) const override {
         snprintf(buf, len, "<MethodInfo %p name='%s'>", this, m_name);
