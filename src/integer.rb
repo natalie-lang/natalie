7cfcff96c8e66c5bb95819660d9d68602702febd
G-flat <63449095+G-flat@users.noreply.github.com>
2022-01-06 16:35:18 +0800

    Implement Integer#anybits? and Integer#nobits?

--- src/integer.rb
+++ src/integer.rb
@@ -37,6 +37,34 @@ class Integer
     (self & int_mask) == int_mask
   end
 
+  def anybits?(mask)
+    unless mask.respond_to?(:to_int)
+      raise TypeError, "No implicit conversion of #{mask.class} into Integer"
+    end
+
+    int_mask = mask.to_int
+
+    unless int_mask.is_a?(Integer)
+      raise TypeError, "can't convert #{mask.class} to Integer (#{mask.class}#to_int gives #{int_mask.class})"
+    end
+
+    (self & int_mask) != 0
+  end
+
+  def nobits?(mask)
+    unless mask.respond_to?(:to_int)
+      raise TypeError, "No implicit conversion of #{mask.class} into Integer"
+    end
+
+    int_mask = mask.to_int
+
+    unless int_mask.is_a?(Integer)
+      raise TypeError, "can't convert #{mask.class} to Integer (#{mask.class}#to_int gives #{int_mask.class})"
+    end
+
+    (self & int_mask) == 0       
+  end
+
   def integer?
     true
   end
