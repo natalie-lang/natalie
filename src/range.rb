a7cb43ccb69d9c283dae64fecfe95c0c11c46f61
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-01-06 13:57:33 -0800

    Implement Range#size

---
+++ src/range.rb
@@ -0,0 +1,15 @@
+class Range
+  def size
+    return Float::INFINITY if self.begin.nil?
+    return unless self.begin.is_a?(Numeric)
+    return Float::INFINITY if self.end.nil?
+    return unless self.end.is_a?(Numeric)
+    return 0 if self.end < self.begin
+    return Float::INFINITY if self.begin.is_a?(Float) && self.begin.infinite?
+    return Float::INFINITY if self.end.is_a?(Float) && self.end.infinite?
+    size = self.end - self.begin
+    size += 1 unless exclude_end?
+    return size.floor if size.is_a?(Float)
+    size
+  end
+end
