d0bb0ed14b9dec66227c425bdd85e2a5590ac191
Tim Morgan <tim@timmorgan.org>
2022-01-25 08:31:34 -0600

    Send self-hosted stats to API

---
+++ test/support/post_minitest_stats_to_api.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'uri'
+require 'net/http'
+
+stats = ARGF.read.split(', ').map(&:split).each_with_object({}) { |(c, l), h| h[l] = c.to_i }
+uri = URI('https://natalie-lang.org/specs-api/self_hosted_stats')
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+puts https.post(
+  uri.path,
+  URI.encode_www_form(
+    'stats' => stats.to_json,
+    'secret' => ENV['STATS_API_SECRET']
+  )
+)
