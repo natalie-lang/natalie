577b5730232c49cc159f3025f8bdaafccd04258a
Ryan Chandler <ryangjchandler@gmail.com>
2022-02-24 15:43:52 +0000

    Implement poor Base64#urlsafe_encode64

--- lib/base64.rb
+++ lib/base64.rb
@@ -6,4 +6,9 @@ module Base64
   def self.strict_encode64(str)
     [str].pack("m0")
   end
+
+  # NATFIXME: Make this spec-compliant.
+  def self.urlsafe_encode64(bin, padding: true)
+    strict_encode64(bin)
+  end
 end
