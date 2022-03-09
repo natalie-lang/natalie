44fe35d106afa2cfc1e2417ef41c92df680d78ee
Tim Morgan <tim@timmorgan.org>
2022-03-09 08:00:53 -0600

    Run 10 specs at-a-time to speed up the test suite

--- test/ruby/ruby_specs_test.rb
+++ test/ruby/ruby_specs_test.rb
@@ -7,19 +7,29 @@ require_relative '../support/nat_binary'
 describe 'ruby/spec' do
   parallelize_me!
 
-  SPEC_TIMEOUT = (ENV['SPEC_TIMEOUT'] || 120).to_i
+  PER_RUN = 10
+  SPEC_TIMEOUT = (ENV['SPEC_TIMEOUT'] || 600).to_i
 
   Dir.chdir File.expand_path('../..', __dir__)
-  Dir['spec/**/*_spec.rb'].each do |path|
-    code = File.read(path, encoding: 'utf-8')
-    describe path do
+  paths = Dir['spec/**/*_spec.rb'].sort
+  skipped_tests, tests = paths.partition { |path| File.read(path, encoding: 'utf-8') =~ /# skip-test/ }
+
+  tests.each_slice(PER_RUN) do |paths|
+    describe paths.join(' ') do
       it 'passes all specs' do
-        skip if code =~ /# skip-test/
-        out_nat =
-          Timeout.timeout(SPEC_TIMEOUT, nil, "execution expired running: #{path}") { `#{NAT_BINARY} #{path} 2>&1` }
+        path_args = paths.map { |p| "-r #{File.expand_path(p)}" }
+        out_nat = Timeout.timeout(SPEC_TIMEOUT, nil, "execution expired running: #{paths.join(', ')}") { `#{NAT_BINARY} #{path_args.join(' ')} -e '' 2>&1` }
         puts out_nat unless $?.success?
         expect($?).must_be :success?
         expect(out_nat).wont_match(/traceback|error/i)
+      end
+    end
+  end
+
+  skipped_tests.each do |path|
+    describe path do
+      it 'passes all specs' do
+        skip
       end
     end
   end
