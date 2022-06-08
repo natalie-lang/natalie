89515bc960f1af4a873f7fda0e85c9b77939b598
Tim Morgan <tim@timmorgan.org>
2022-06-08 18:55:49 -0500

    Add inspect method for Compiler2::BasePass

--- lib/natalie/compiler2/base_pass.rb
+++ lib/natalie/compiler2/base_pass.rb
@@ -3,6 +3,9 @@ require_relative './instructions'
 module Natalie
   class Compiler2
     class BasePass
+      def inspect
+        "<#{self.class.name}:0x#{object_id.to_s(16)}>"
+      end
     end
   end
 end
