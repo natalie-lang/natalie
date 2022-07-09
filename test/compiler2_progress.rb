7cecc6f06e16878576ee21fc041bfa47b10d5e82
Tim Morgan <tim@timmorgan.org>
2022-07-09 12:44:33 -0500

    Add script for testing Compiler2 progress

---
+++ test/compiler2_progress.rb
@@ -0,0 +1,21 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'timeout'
+
+Dir.chdir File.expand_path('..', __dir__)
+
+def sh(command)
+  command = "systemd-run --scope -p MemoryMax=1G --user #{command}"
+  Timeout.timeout(10, nil, "execution expired running: #{command}") {
+    `#{command}`
+  }
+end
+
+Dir['test/natalie/*_test.rb', 'spec/**/*_spec.rb'].each do |path|
+  describe path do
+    it 'passes' do
+      sh("bin/natalie -c2 -I test/support #{path} 2>&1").strip
+      expect($?).must_be :success?
+    end
+  end
+end
