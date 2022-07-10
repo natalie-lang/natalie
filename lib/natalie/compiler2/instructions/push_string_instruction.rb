d92005764639c281112c10efb0726e806ee52d34
Tim Morgan <tim@timmorgan.org>
2022-07-10 18:47:00 -0500

    C2: fix writing of string literal for C++

--- lib/natalie/compiler2/instructions/push_string_instruction.rb
+++ lib/natalie/compiler2/instructions/push_string_instruction.rb
@@ -16,12 +16,38 @@ module Natalie
         if @string.empty?
           transform.exec_and_push('string', "Value(new StringObject)")
         else
-          transform.exec_and_push('string', "Value(new StringObject(#{@string.inspect}, (size_t)#{@length}))")
+          transform.exec_and_push('string', "Value(new StringObject(#{to_cpp}, (size_t)#{@length}))")
         end
       end
 
       def execute(vm)
         vm.push(@string.dup)
+      end
+
+      private
+
+      def to_cpp
+        chars = @string.to_s.bytes.map do |byte|
+            case byte
+            when 9
+              "\\t"
+            when 10
+              "\\n"
+            when 13
+              "\\r"
+            when 27
+              "\\e"
+            when 34
+              "\\\""
+            when 92
+              "\\134"
+            when 32..126
+              byte.chr(Encoding::ASCII_8BIT)
+            else
+              "\\%03o" % byte
+            end
+          end
+        '"' + chars.join + '"'
       end
     end
   end
