f22e7eae7372b0c6548a98f81c2f9ba37372e770
Tim Morgan <tim@timmorgan.org>
2022-08-04 07:31:54 -0500

    Allow passing flags to test/runner.rb

--- test/runner.rb
+++ test/runner.rb
@@ -7,12 +7,15 @@
 
 require_relative 'support/nat_binary'
 
+flags = []
+flags << ARGV.shift while ARGV.first.to_s.start_with?('-')
+
 ARGV.each do |path|
   if File.directory?(path)
     $stderr.puts "WARNING: skipping directory #{path}"
     next
   end
-  pid = spawn(NAT_BINARY, path)
+  pid = spawn(NAT_BINARY, *(flags + [path]))
   Process.wait(pid)
   exit $?.exitstatus unless $?.success?
 end
