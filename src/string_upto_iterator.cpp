90cb9612b6520da7a51e1b672ed226e19f423728
Tim Morgan <tim@timmorgan.org>
2022-08-21 08:27:51 -0500

    Add comment about special single-char upto handling

--- src/string_upto_iterator.cpp
+++ src/string_upto_iterator.cpp
@@ -10,6 +10,7 @@ const TM::String StringUptoIterator::peek() const {
     if (m_treat_like_integer)
         return TM::String((Integer(m_string) + 1).to_nat_int_t());
 
+    // special handling for single characters
     if (m_string == "9")
         return ":";
     else if (m_string == "Z")
