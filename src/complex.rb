c4248fb3f0558ac74e9df6340c062d1a479bcdaa
Ryan Chandler <ryangjchandler@gmail.com>
2022-09-05 19:10:14 +0100

    Make Complex#== almost spec compliant

--- src/complex.rb
+++ src/complex.rb
@@ -11,6 +11,32 @@ class Complex
     self.real.to_i
   end
 
+  def ==(other)
+    real = self.real
+    imaginary = self.imaginary
+
+    if ! other.is_a?(Complex) && imaginary == 0
+      return real == other
+    end
+
+    other_real = other.real
+    other_imaginary = other.imaginary
+
+    if real == 0 || other_imaginary == 0
+      return false
+    end
+
+    if real != other_real
+      return false
+    end
+
+    if imaginary != other_imaginary
+      return false
+    end
+
+    return true
+  end
+
   def real?
     false
   end
