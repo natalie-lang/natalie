9f077c0cbecfdd79caae8f07acd8e88562c1bb24
Tim Morgan <tim@timmorgan.org>
2022-09-07 07:03:44 -0500

    Move remaining C++ code for Socket into socket.cpp

--- lib/socket.cpp
+++ lib/socket.cpp
@@ -1,7 +1,11 @@
-#include "natalie.hpp"
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/un.h>
+
+#include "natalie.hpp"
 
 using namespace Natalie;
 
@@ -98,4 +102,118 @@ Value Addrinfo_socktype(Env *env, Value self, Args args, Block *block) {
     auto addrinfo_void_ptr = self->ivar_get(env, "@_addrinfo"_s)->as_void_p();
     auto info_struct = (struct addrinfo *)addrinfo_void_ptr->void_ptr();
     return Value::integer(info_struct->ai_socktype);
+}
+
+Value Socket_pack_sockaddr_in(Env *env, Value self, Args args, Block *block) {
+    args.ensure_argc_between(env, 0, 2);
+    auto port = args.at(0, NilObject::the());
+    auto host = args.at(1, NilObject::the());
+    if (host->is_nil())
+        host = new StringObject { "127.0.0.1" };
+    if (host->is_string() && host->as_string()->is_empty())
+        host = new StringObject { "0.0.0.0" };
+    host->assert_type(env, Object::Type::String, "String");
+    auto host_string = host->as_string();
+
+    if (!port->is_integer() && port->respond_to(env, "to_i"_s))
+        port = port->send(env, "to_i"_s);
+
+    port->assert_type(env, Object::Type::Integer, "Integer");
+    auto port_unsigned = (unsigned short)port->as_integer()->to_nat_int_t();
+
+    if (host_string->include(":")) {
+
+        // IPV6
+        struct sockaddr_in6 in { };
+#if defined(__OpenBSD__) or defined(__APPLE__)
+        in.sin6_len = sizeof(in);
+#endif
+        in.sin6_family = AF_INET6;
+        in.sin6_port = port_unsigned;
+        memcpy(in.sin6_addr.s6_addr, host_string->c_str(), std::min(host_string->length(), (size_t)16));
+        return new StringObject { (const char *)&in, sizeof(in) };
+
+    } else {
+
+        // IPV4
+        struct in_addr a;
+        auto result = inet_aton(host->as_string()->c_str(), &a);
+        if (!result)
+            env->raise("SocketError", "getaddrinfo: Name or service not known");
+        struct sockaddr_in in { };
+#if defined(__OpenBSD__) or defined(__APPLE__)
+        in.sin_len = sizeof(in);
+#endif
+        in.sin_family = AF_INET;
+        in.sin_port = port_unsigned;
+        in.sin_addr = a;
+        return new StringObject { (const char *)&in, sizeof(in) };
+    }
+}
+
+Value Socket_pack_sockaddr_un(Env *env, Value self, Args args, Block *block) {
+    args.ensure_argc_between(env, 0, 1);
+    auto path = args.at(0, NilObject::the());
+    path->assert_type(env, Object::Type::String, "String");
+    auto path_string = path->as_string();
+
+    struct sockaddr_un un { };
+    constexpr auto unix_path_max = sizeof(un.sun_path);
+
+    if (path_string->length() >= unix_path_max)
+        env->raise("ArgumentError", "too long unix socket path ({} bytes given but {} bytes max))", path_string->length(), unix_path_max);
+
+    un.sun_family = AF_UNIX;
+    memcpy(un.sun_path, path_string->c_str(), path_string->length());
+    return new StringObject { (const char *)&un, sizeof(un) };
+}
+
+Value Socket_unpack_sockaddr_in(Env *env, Value self, Args args, Block *block) {
+    args.ensure_argc_between(env, 0, 1);
+    auto sockaddr = args.at(0, NilObject::the());
+      if (sockaddr->is_a(env, self->const_find(env, "Addrinfo"_s, Object::ConstLookupSearchMode::NotStrict)))
+          sockaddr = sockaddr->send(env, "sockaddr"_s);
+
+      sockaddr->assert_type(env, Object::Type::String, "String");
+
+      if (sockaddr->as_string()->length() == sizeof(struct sockaddr_in6)) {
+
+          // IPV6
+          const char *str = sockaddr->as_string()->c_str();
+          auto in = (struct sockaddr_in6 *)str;
+          auto ary = new ArrayObject;
+          ary->push(Value::integer(in->sin6_port));
+          ary->push(new StringObject((const char*)in->sin6_addr.s6_addr));
+          return ary;
+
+      } else if (sockaddr->as_string()->length() == sizeof(struct sockaddr_in)) {
+
+          // IPV4
+          const char *str = sockaddr->as_string()->c_str();
+          auto in = (struct sockaddr_in *)str;
+          auto addr = inet_ntoa(in->sin_addr);
+          auto ary = new ArrayObject;
+          ary->push(Value::integer(in->sin_port));
+          ary->push(new StringObject(addr));
+          return ary;
+
+      } else {
+          env->raise("ArgumentError", "not an AF_INET/AF_INET6 sockaddr");
+      }
+}
+
+Value Socket_unpack_sockaddr_un(Env *env, Value self, Args args, Block *block) {
+    args.ensure_argc_between(env, 0, 1);
+    auto sockaddr = args.at(0, NilObject::the());
+      if (sockaddr->is_a(env, self->const_find(env, "Addrinfo"_s, Object::ConstLookupSearchMode::NotStrict)))
+          sockaddr = sockaddr->send(env, "sockaddr"_s);
+
+      sockaddr->assert_type(env, Object::Type::String, "String");
+
+      if (sockaddr->as_string()->length() != sizeof(struct sockaddr_un))
+          env->raise("ArgumentError", "not an AF_UNIX sockaddr");
+
+      const char *str = sockaddr->as_string()->c_str();
+      auto un = (struct sockaddr_un *)str;
+      return new StringObject { un->sun_path };
 }
--- lib/socket.rb
+++ lib/socket.rb
@@ -1,15 +1,6 @@
 require 'natalie/inline'
 require 'socket.cpp'
 
-__inline__ <<-END
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <netdb.h>
-END
-
 class SocketError < StandardError
 end
 
@@ -60,131 +51,31 @@ class Socket < BasicSocket
   end
 
   class << self
-    __define_method__ :pack_sockaddr_in, %i[port host], <<-END
-      if (host->is_nil())
-          host = new StringObject { "127.0.0.1" };
-      if (host->is_string() && host->as_string()->is_empty())
-        host = new StringObject { "0.0.0.0" };
-      host->assert_type(env, Object::Type::String, "String");
-      auto host_string = host->as_string();
-
-      if (!port->is_integer() && port->respond_to(env, "to_i"_s))
-          port = port->send(env, "to_i"_s);
-
-      port->assert_type(env, Object::Type::Integer, "Integer");
-      auto port_unsigned = (unsigned short)port->as_integer()->to_nat_int_t();
-
-      if (host_string->include(":")) {
-
-        // IPV6
-        struct sockaddr_in6 in {};
-#if defined(__OpenBSD__) or defined(__APPLE__)
-        in.sin6_len = sizeof(in);
-#endif
-        in.sin6_family = AF_INET6;
-        in.sin6_port = port_unsigned;
-        memcpy(in.sin6_addr.s6_addr, host_string->c_str(), std::min(host_string->length(), (size_t)16));
-        return new StringObject { (const char*)&in, sizeof(in) };
-
-      } else {
-
-        // IPV4
-        struct in_addr a;
-        auto result = inet_aton(host->as_string()->c_str(), &a);
-        if (!result)
-            env->raise("SocketError", "getaddrinfo: Name or service not known");
-        struct sockaddr_in in {};
-#if defined(__OpenBSD__) or defined(__APPLE__)
-        in.sin_len = sizeof(in);
-#endif
-        in.sin_family = AF_INET;
-        in.sin_port = port_unsigned;
-        in.sin_addr = a;
-        return new StringObject { (const char*)&in, sizeof(in) };
-
-      }
-    END
+    __bind_method__ :pack_sockaddr_in, :Socket_pack_sockaddr_in
+    __bind_method__ :pack_sockaddr_un, :Socket_pack_sockaddr_un
+    __bind_method__ :unpack_sockaddr_in, :Socket_unpack_sockaddr_in
+    __bind_method__ :unpack_sockaddr_un, :Socket_unpack_sockaddr_un
 
     alias sockaddr_in pack_sockaddr_in
-
-    __define_method__ :pack_sockaddr_un, [:path], <<-END
-      path->assert_type(env, Object::Type::String, "String");
-      auto path_string = path->as_string();
-
-      struct sockaddr_un un {};
-      constexpr auto unix_path_max = sizeof(un.sun_path);
-
-      if (path_string->length() >= unix_path_max)
-          env->raise("ArgumentError", "too long unix socket path ({} bytes given but {} bytes max))", path_string->length(), unix_path_max);
-
-      un.sun_family = AF_UNIX;
-      memcpy(un.sun_path, path_string->c_str(), path_string->length());
-      return new StringObject { (const char*)&un, sizeof(un) };
-    END
-
     alias sockaddr_un pack_sockaddr_un
-
-    __define_method__ :unpack_sockaddr_in, [:sockaddr], <<-END
-      if (sockaddr->is_a(env, self->const_find(env, "Addrinfo"_s, Object::ConstLookupSearchMode::NotStrict)))
-          sockaddr = sockaddr->send(env, "sockaddr"_s);
-
-      sockaddr->assert_type(env, Object::Type::String, "String");
-
-      if (sockaddr->as_string()->length() == sizeof(struct sockaddr_in6)) {
-
-          // IPV6
-          const char *str = sockaddr->as_string()->c_str();
-          auto in = (struct sockaddr_in6 *)str;
-          auto ary = new ArrayObject;
-          ary->push(Value::integer(in->sin6_port));
-          ary->push(new StringObject((const char*)in->sin6_addr.s6_addr));
-          return ary;
-
-      } else if (sockaddr->as_string()->length() == sizeof(struct sockaddr_in)) {
-
-          // IPV4
-          const char *str = sockaddr->as_string()->c_str();
-          auto in = (struct sockaddr_in *)str;
-          auto addr = inet_ntoa(in->sin_addr);
-          auto ary = new ArrayObject;
-          ary->push(Value::integer(in->sin_port));
-          ary->push(new StringObject(addr));
-          return ary;
-
-      } else {
-          env->raise("ArgumentError", "not an AF_INET/AF_INET6 sockaddr");
-      }
-    END
-
-    __define_method__ :unpack_sockaddr_un, [:sockaddr], <<-END
-      if (sockaddr->is_a(env, self->const_find(env, "Addrinfo"_s, Object::ConstLookupSearchMode::NotStrict)))
-          sockaddr = sockaddr->send(env, "sockaddr"_s);
-
-      sockaddr->assert_type(env, Object::Type::String, "String");
-
-      if (sockaddr->as_string()->length() != sizeof(struct sockaddr_un))
-          env->raise("ArgumentError", "not an AF_UNIX sockaddr");
-
-      const char *str = sockaddr->as_string()->c_str();
-      auto un = (struct sockaddr_un *)str;
-      return new StringObject { un->sun_path };
-    END
   end
 end
 
 class Addrinfo
   attr_reader :sockaddr, :family, :protocol
 
-  def self.tcp(ip, port)
-    Addrinfo.new(Socket.pack_sockaddr_in(port, ip), nil, nil, Socket::IPPROTO_TCP)
-  end
+  class << self
+    def tcp(ip, port)
+      Addrinfo.new(Socket.pack_sockaddr_in(port, ip), nil, nil, Socket::IPPROTO_TCP)
+    end
 
-  def self.udp(_ip, _port)
-    Addrinfo.new # TODO
-  end
+    def udp(_ip, _port)
+      Addrinfo.new # TODO
+    end
 
-  def self.unix(path)
-    Addrinfo.new(Socket.pack_sockaddr_un(path))
+    def unix(path)
+      Addrinfo.new(Socket.pack_sockaddr_un(path))
+    end
   end
 
   __bind_method__ :initialize, :Addrinfo_initialize
