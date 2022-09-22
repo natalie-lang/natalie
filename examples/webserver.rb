4528baa5bfc8442caac61fdf22fb77a40da19c80
Tim Morgan <tim@timmorgan.org>
2022-09-21 19:53:56 -0500

    Update example web server to use forked workers

--- examples/webserver.rb
+++ examples/webserver.rb
@@ -2,30 +2,41 @@ require 'socket'
 
 server = TCPServer.new 3000
 
-loop do
-  client = server.accept
-  method, request_target, _http_version = client.gets.strip.split
-  headers = {}
-  until (line = client.gets) =~ /^\r?\n$/
-    name, value = line.strip.split(': ')
-    headers[name] = value
-  end
-  p([method, request_target])
-  if method.upcase == 'GET'
-    case request_target
-    when '/'
-      client.write "HTTP/1.1 200\r\n"
-      client.write "\r\n"
-      client.write "hello world\r\n"
-    else
-      client.write "HTTP/1.1 404\r\n"
-      client.write "\r\n"
-      client.write "resource not found\r\n"
+pids = (1..10).map do
+  fork do
+    loop do
+      client = server.accept
+      # sleep 1 # for more-easily testing concurrency :-)
+      method, request_target, _http_version = client.gets.strip.split
+      headers = {}
+      until (line = client.gets) =~ /^\r?\n$/
+        name, value = line.strip.split(': ')
+        headers[name] = value
+      end
+      p([method, request_target])
+      if method.upcase == 'GET'
+        case request_target
+        when '/'
+          client.write "HTTP/1.1 200\r\n"
+          client.write "\r\n"
+          client.write "hello world\r\n"
+        else
+          client.write "HTTP/1.1 404\r\n"
+          client.write "\r\n"
+          client.write "resource not found\r\n"
+        end
+      else
+        client.write "HTTP/1.1 405\r\n"
+        client.write "\r\n"
+        client.write "method not allowed\r\n"
+      end
+    ensure
+      client.close
     end
-  else
-    client.write "HTTP/1.1 405\r\n"
-    client.write "\r\n"
-    client.write "method not allowed\r\n"
+  rescue StandardError
+    puts 'client misbehaved or connection dropped or something'
+    :noop
   end
-  client.close
 end
+
+pids.map { |pid| Process.wait(pid) }
