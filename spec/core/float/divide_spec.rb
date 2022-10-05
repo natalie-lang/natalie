556e802ba73c8e3453e85f162fab11a8efd7c218
Stephen Ierodiaconou <stevegeek@gmail.com>
2022-10-05 16:39:55 +0200

    Add Float Infinity expectation & unskip related tests
    Also update divide_spec to match latest from ruby/spec

--- spec/core/float/divide_spec.rb
+++ spec/core/float/divide_spec.rb
@@ -8,19 +8,19 @@ describe "Float#/" do
   it "returns self divided by other" do
     (5.75 / -2).should be_close(-2.875,TOLERANCE)
     (451.0 / 9.3).should be_close(48.494623655914,TOLERANCE)
-    #(91.1 / -0xffffffff).should be_close(-2.12108716418061e-08, TOLERANCE)
+    (91.1 / -0xffffffff).should be_close(-2.12108716418061e-08, TOLERANCE)
   end
 
   it "properly coerces objects" do
     (5.0 / FloatSpecs::CanCoerce.new(5)).should be_close(0, TOLERANCE)
   end
 
-  xit "returns +Infinity when dividing non-zero by zero of the same sign" do
+  it "returns +Infinity when dividing non-zero by zero of the same sign" do
     (1.0 / 0.0).should be_positive_infinity
     (-1.0 / -0.0).should be_positive_infinity
   end
 
-  xit "returns -Infinity when dividing non-zero by zero of opposite sign" do
+  it "returns -Infinity when dividing non-zero by zero of opposite sign" do
     (-1.0 / 0.0).should be_negative_infinity
     (1.0 / -0.0).should be_negative_infinity
   end
@@ -35,5 +35,9 @@ describe "Float#/" do
   it "raises a TypeError when given a non-Numeric" do
     -> { 13.0 / "10"    }.should raise_error(TypeError)
     -> { 13.0 / :symbol }.should raise_error(TypeError)
+  end
+
+  it "divides correctly by Rational numbers" do
+    (1.2345678901234567 / Rational(1, 10000000000000000000)).should == 1.2345678901234567e+19
   end
 end
