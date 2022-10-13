0fe6f2e42b6f7d813bd09b16cccd518f46e3c0bf
Tim Morgan <tim@timmorgan.org>
2022-10-12 20:19:09 -0500

    Add Array#pack directive 's', 'S', and modifiers '<', '>'

--- include/natalie/array_packer/tokenizer.hpp
+++ include/natalie/array_packer/tokenizer.hpp
@@ -9,11 +9,18 @@ namespace Natalie {
 using namespace TM;
 
 namespace ArrayPacker {
+    enum class Endianness {
+        Native,
+        Little,
+        Big,
+    };
 
     struct Token {
         signed char directive { 0 };
         int count { -1 };
         bool star { false };
+        bool native_size { false };
+        Endianness endianness { Endianness::Native };
         Optional<String> error {};
     };
 
@@ -34,49 +41,83 @@ namespace ArrayPacker {
 
     private:
         Token next_token() {
-            auto d = next_char();
+            auto d = current_char();
             if (!d) return {};
 
             auto token = Token { d };
-            d = peek_char();
-            if (d == 0)
-                next_char();
-            if (isdigit(d)) {
-                d = next_char();
-                token.count = (int)d - 48;
-                while (isdigit(peek_char())) {
-                    d = next_char();
+
+            auto modifier = next_char();
+
+            if (modifier == '>') {
+                token.endianness = Endianness::Big;
+                modifier = next_char();
+            }
+
+            if (modifier == '<') {
+                token.endianness = Endianness::Little;
+                modifier = next_char();
+            }
+
+            if (modifier == '_' || modifier == '!') {
+                switch (token.directive) {
+                case 'i':
+                case 'I':
+                case 'j':
+                case 'J':
+                case 'l':
+                case 'L':
+                case 'q':
+                case 'Q':
+                case 's':
+                case 'S':
+                    token.native_size = true;
+                    break;
+                default:
+                    token.error = String::format("'{}' allowed only after types sSiIlLqQjJ", modifier);
+                }
+                modifier = next_char();
+            }
+
+            if (modifier == '>') {
+                token.endianness = Endianness::Big;
+                modifier = next_char();
+            }
+
+            if (modifier == '<') {
+                token.endianness = Endianness::Little;
+                modifier = next_char();
+            }
+
+            if (isdigit(modifier)) {
+                token.count = (int)modifier - '0';
+                modifier = next_char();
+                while (isdigit(modifier)) {
                     token.count *= 10;
-                    token.count += ((int)d - 48);
+                    token.count += ((int)modifier - '0');
+                    modifier = next_char();
                 }
             }
-            switch (d) {
-            case '*':
+
+            if (modifier == '*') {
                 next_char();
                 token.star = true;
-                return token;
-            case '_':
-            case '!':
-                next_char();
-                if (d != 's' && d != 'S' && d != 'i' && d != 'I' && d != 'l' && d != 'L' && d != 'q' && d != 'Q' && d != 'j' && d != 'J')
-                    token.error = String::format("'{}' allowed only after types sSiIlLqQjJ", d);
-                return token;
-            default:
-                return token;
             }
+
+            // printf("token { '%c', %d, %d, %d }\n", token.directive, token.count, (int)token.star, (int)token.native_size);
+
+            return token;
         }
 
         signed char next_char() {
-            char c = peek_char();
             m_index++;
-            return c;
+            return current_char();
         }
 
-        signed char peek_char() {
+        signed char current_char() {
             if (m_index >= m_directives.length())
                 return 0;
             signed char c = m_directives[m_index];
-            while (isspace(c)) {
+            while (isspace(c) || c == '\0') {
                 if (m_index + 1 >= m_directives.length())
                     return 0;
                 c = m_directives[++m_index];
