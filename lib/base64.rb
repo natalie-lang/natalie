47d49cf30c71aecaca272482939b8b0add2b2b55
Tim Morgan <tim@timmorgan.org>
2022-10-22 22:24:52 -0500

    Add Base64.decode64 et al

--- lib/base64.rb
+++ lib/base64.rb
@@ -3,8 +3,16 @@ module Base64
     [str].pack("m")
   end
 
+  def self.decode64(str)
+    str.unpack("m").first
+  end
+
   def self.strict_encode64(str)
     [str].pack("m0")
+  end
+
+  def self.strict_decode64(str)
+    str.unpack("m0").first
   end
 
   def self.urlsafe_encode64(bin, padding: true)
