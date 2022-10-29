fe9e4667b9620880b69f7831f4dfe3002462475a
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-10-29 19:38:53 +0300

    Add CSV#lineno test

---
+++ test/lib/csv/lineno_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../spec_helper'
+require 'csv'
+
+describe "CSV#lineno" do
+  it "is 0 initially" do
+    csv = CSV.new("a,b,c\n1,2,3")
+
+    csv.lineno.should == 0
+  end
+
+  it "is 1 after one row read" do
+    csv = CSV.new("a,b,c\n1,2,3")
+
+    csv.readline
+
+    csv.lineno.should == 1
+  end
+
+  # NATFIXME: enable when newlines in quotes are supported
+  xit "increases only once when line-end characters are in fields" do
+    csv = CSV.new(%Q[a,"multi\n\n\nline",c\n1,2,3])
+
+    csv.readline
+
+    csv.lineno.should == 1
+  end
+end
