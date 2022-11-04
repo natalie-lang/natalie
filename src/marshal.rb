d7fb5381d08b8c0713f6fac0ea0ff6d37d2c0aa8
Tim Craft <mail@timcraft.com>
2022-11-04 15:53:09 +0000

    Implement Marshal.dump

--- src/marshal.rb
+++ src/marshal.rb
@@ -1,4 +1,225 @@
 module Marshal
   MAJOR_VERSION = 4
   MINOR_VERSION = 8
+
+  class << self
+    def dump(object, io = nil, limit = -1)
+      if io.nil?
+        writer = StringWriter.new
+      else
+        writer = Writer.new(io)
+      end
+      writer.write_version
+      writer.write(object)
+    end
+  end
+
+  class Writer
+    def initialize(output)
+      @output = output
+    end
+
+    def write_byte(value)
+      @output.ungetbyte(value)
+    end
+
+    def write_bytes(value)
+      @output.write(value)
+    end
+
+    def write_version
+      write_byte(MAJOR_VERSION)
+      write_byte(MINOR_VERSION)
+    end
+
+    def write_integer_bytes(value)
+      if value.zero?
+        write_byte(0)
+      elsif value > 0 && value < 123
+        write_byte(value + 5)
+      elsif value > -124 && value < 0
+        write_byte((value - 5) & 255)
+      else
+        buffer = []
+        until value.zero? || value == -1
+          buffer << (value & 255)
+          value = value >> 8
+        end
+        if value.zero?
+          write_byte(buffer.length)
+        elsif value == -1
+          write_byte(256 - buffer.length)
+        end
+        buffer.each do |integer|
+          write_byte(integer)
+        end
+      end
+    end
+
+    def write_string_bytes(value)
+      string = value.to_s
+      write_integer_bytes(string.length)
+      write_bytes(string)
+    end
+
+    def write_encoding_bytes(value)
+      case value.encoding
+      when Encoding::US_ASCII
+        write_integer_bytes(1)
+        write_symbol(:E)
+        write_false
+      when Encoding::UTF_8
+        write_integer_bytes(1)
+        write_symbol(:E)
+        write_true
+      end
+    end
+
+    def write_char(string)
+      write_byte(string[0])
+    end
+
+    def write_nil
+      write_char('0')
+    end
+
+    def write_true
+      write_char('T')
+    end
+
+    def write_false
+      write_char('F')
+    end
+
+    def write_integer(value)
+      write_char('i')
+      write_integer_bytes(value)
+    end
+
+    def write_string(value)
+      write_char('I')
+      write_char('"')
+      write_string_bytes(value)
+      write_encoding_bytes(value)
+    end
+
+    def write_symbol(value)
+      write_char(':')
+      write_string_bytes(value)
+    end
+
+    def write_float(value)
+      write_char('f')
+      if value.nan?
+        write_string_bytes('nan')
+      elsif value.infinite?
+        write_string_bytes(value < 0 ? '-inf' : 'inf')
+      elsif value == 0.0
+        write_string_bytes(1 / value < 0 ? '-0' : '0')
+      else
+        write_string_bytes(value)
+      end
+    end
+
+    def write_array(values)
+      write_char('[')
+      write_integer_bytes(values.size)
+      values.each do |value|
+        write(value)
+      end
+    end
+
+    def write_hash(values)
+      write_char('{')
+      write_integer_bytes(values.size)
+      values.each do |key, value|
+        write(key)
+        write(value)
+      end
+    end
+
+    def write_class(value)
+      write_char('c')
+      write_string_bytes(value.name)
+    end
+
+    def write_module(value)
+      write_char('m')
+      write_string_bytes(value.name)
+    end
+
+    def write_regexp(value)
+      write_char('I')
+      write_string_bytes(value.source)
+      write_byte(value.options)
+      write_encoding_bytes(value)
+    end
+
+    def write_user_marshaled_object(value)
+      write_char('U')
+      write(value.class.name.to_sym)
+      write(value.send(:marshal_dump))
+    end
+
+    def write_object(value)
+      write_char('o')
+      write(value.class.name.to_sym)
+      ivar_names = value.instance_variables
+      write_integer_bytes(ivar_names.size)
+      ivar_names.each do |ivar_name|
+        write(ivar_name)
+        write(value.instance_variable_get(ivar_name))
+      end
+    end
+
+    def write(value)
+      if value.nil?
+        write_nil
+      elsif value.is_a?(TrueClass)
+        write_true
+      elsif value.is_a?(FalseClass)
+        write_false
+      elsif value.is_a?(Integer)
+        write_integer(value)
+      elsif value.is_a?(String)
+        write_string(value)
+      elsif value.is_a?(Symbol)
+        write_symbol(value)
+      elsif value.is_a?(Float)
+        write_float(value)
+      elsif value.is_a?(Array)
+        write_array(value)
+      elsif value.is_a?(Hash)
+        write_hash(value)
+      elsif value.is_a?(Class)
+        write_class(value)
+      elsif value.is_a?(Module)
+        write_module(value)
+      elsif value.is_a?(Regexp)
+        write_regexp(value)
+      elsif value.respond_to?(:marshal_dump, true)
+        write_user_marshaled_object(value)
+      elsif value.is_a?(Object)
+        write_object(value)
+      else
+        raise TypeError, "can't dump #{value.class}"
+      end
+
+      @output
+    end
+  end
+
+  class StringWriter < Writer
+    def initialize
+      @output = String.new.force_encoding(Encoding::ASCII_8BIT)
+    end
+
+    def write_byte(value)
+      @output << value
+    end
+
+    def write_bytes(value)
+      @output.concat(value)
+    end
+  end
 end
---
+++ test/natalie/marshal_test.rb
@@ -0,0 +1,76 @@
+require_relative '../spec_helper'
+
+describe 'Marshal' do
+  describe '.dump' do
+    it 'writes nil' do
+      Marshal.dump(nil).should == "\x04\b0".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes true' do
+      Marshal.dump(true).should == "\x04\bT".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes false' do
+      Marshal.dump(false).should == "\x04\bF".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes integers' do
+      Marshal.dump(0).should == "\x04\bi\x00".force_encoding(Encoding::BINARY)
+      Marshal.dump(4).should == "\x04\bi\t".force_encoding(Encoding::BINARY)
+      Marshal.dump(128).should == "\x04\bi\x01\x80".force_encoding(Encoding::BINARY)
+      Marshal.dump(123456).should == "\x04\bi\x03@\xE2\x01".force_encoding(Encoding::BINARY)
+      Marshal.dump(-1).should == "\x04\bi\xFA".force_encoding(Encoding::BINARY)
+      Marshal.dump(-4).should == "\x04\bi\xF7".force_encoding(Encoding::BINARY)
+      Marshal.dump(-128).should == "\x04\bi\xFF\x80".force_encoding(Encoding::BINARY)
+      Marshal.dump(-123456).should == "\x04\bi\xFD\xC0\x1D\xFE".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes strings' do
+      Marshal.dump('').should == "\x04\bI\"\x00\x06:\x06ET".force_encoding(Encoding::BINARY)
+      Marshal.dump('ruby').should == "\x04\bI\"\truby\x06:\x06ET".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes symbols' do
+      Marshal.dump(:ruby).should == "\x04\b:\truby".force_encoding(Encoding::BINARY)
+      Marshal.dump(:E).should == "\x04\b:\x06E".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes floats' do
+      Marshal.dump(1.23).should == "\x04\bf\t1.23".force_encoding(Encoding::BINARY)
+      Marshal.dump(-1.23).should == "\x04\bf\n-1.23".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes arrays' do
+      Marshal.dump([]).should == "\x04\b[\x00".force_encoding(Encoding::BINARY)
+      Marshal.dump([1, 2, 3]).should == "\x04\b[\bi\x06i\ai\b".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes hashes' do
+      Marshal.dump({}).should == "\x04\b{\x00".force_encoding(Encoding::BINARY)
+      Marshal.dump({a: 1}).should == "\x04\b{\x06:\x06ai\x06".force_encoding(Encoding::BINARY)
+      Marshal.dump({E: true}).should == "\x04\b{\x06:\x06ET".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes classes' do
+      Marshal.dump(Array).should == "\x04\bc\nArray".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes modules' do
+      Marshal.dump(Enumerable).should == "\x04\bm\x0FEnumerable".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes objects' do
+      object = Object.new
+      object.instance_variable_set(:@ivar, 1)
+      Marshal.dump(object).should == "\x04\bo:\vObject\x06:\n@ivari\x06"
+    end
+
+    it 'writes complex numbers' do
+      Marshal.dump(Complex(1, 2)).should == "\x04\bU:\fComplex[\ai\x06i\a".force_encoding(Encoding::BINARY)
+    end
+
+    it 'writes rational numbers' do
+      Marshal.dump(Rational(1, 2)).should == "\x04\bU:\rRational[\ai\x06i\a".force_encoding(Encoding::BINARY)
+    end
+  end
+end
