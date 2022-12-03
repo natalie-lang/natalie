09f5f44d9280678fc6c934192f8872db1a0e14e2
Andrew Konchin <andry.konchin@gmail.com>
2022-12-03 19:32:27 +0200

    Add UTF-32BE encoding

--- include/natalie/encoding_object.hpp
+++ include/natalie/encoding_object.hpp
@@ -45,6 +45,9 @@ public:
         case Encoding::UTF_32LE:
             // it's positive and takes 1-4 bytes
             return codepoint >= 0 && codepoint < 0x10000000000;
+        case Encoding::UTF_32BE:
+            // it's positive and takes 1-4 bytes
+            return codepoint >= 0 && codepoint < 0x10000000000;
         }
         NAT_UNREACHABLE();
     }
