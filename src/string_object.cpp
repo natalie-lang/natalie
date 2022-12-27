ed45736f157eeef519f36a870998f58e6ddd529b
Herwin Weststrate <herwin@snt.utwente.nl>
2022-12-27 19:30:58 +0100

    Support for BigInt in String#to_i
    It currently only works for base10

--- src/string_object.cpp
+++ src/string_object.cpp
@@ -1831,6 +1831,9 @@ Value StringObject::to_i(Env *env, Value base_obj) const {
         }
     }
     nat_int_t number = strtoll(c_str(), nullptr, base);
+    if (number == NAT_INT_MIN || number == NAT_INT_MAX) {
+        return IntegerObject::create(BigInt(string()));
+    }
     return Value::integer(number);
 }
 
