5c5b13686bcd5302ee2c723b0991959c84003925
Tim Morgan <tim@timmorgan.org>
2022-12-31 20:53:01 -0600

    Refactor case/when compilation
    We will start adding case/in soon...

--- lib/natalie/compiler/pass1.rb
+++ lib/natalie/compiler/pass1.rb
@@ -288,71 +288,116 @@ module Natalie
       end
 
       def transform_case(exp, used:)
-        _, var, *whens, else_block = exp
-        return transform_expression(else_block, used: used) if whens.empty?
+        _, value, *clauses, else_block = exp
+        return transform_expression(else_block, used: used) if clauses.empty?
+
         instructions = []
-        if var
-          instructions << transform_expression(var, used: true)
-          whens.each do |when_statement|
-            # case a
-            # when b, c, d
-            # =>
-            # if (b === a || c === a || d === a)
-            _, options_array, *body = when_statement
-            _, *options = options_array
 
-            options.each do |option|
-              # Splats are handled in the backend.
-              # For C++, it's done in the is_case_equal() function.
-              if option.sexp_type == :splat
-                _, option = option
-                is_splat = true
-              else
-                is_splat = false
-              end
+        unless value
+          return transform_case_when_without_value(exp, used: used)
+        end
 
-              option_instructions = transform_expression(option, used: true)
-              instructions << option_instructions
-              instructions << CaseEqualInstruction.new(is_splat: is_splat)
-              instructions << IfInstruction.new
-              instructions << PushTrueInstruction.new
-              instructions << ElseInstruction.new(:if)
-            end
-            instructions << PushFalseInstruction.new
-            instructions << [EndInstruction.new(:if)] * options.length
-            instructions << IfInstruction.new
-            instructions << transform_body(body, used: true)
-            instructions << ElseInstruction.new(:if)
-          end
-        else
-          # glorified if-else
-          whens.each do |when_statement|
-            # case
-            # when a == b, b == c, c == d
-            # =>
-            # if (a == b || b == c || c == d)
-            _, options, *body = when_statement
-
-            # s(:array, option1, option2, ...)
-            # =>
-            # s(:or, option1, s(:or, option2, ...))
-            options = options[2..].reduce(options[1]) { |prev, option| s(:or, prev, option) }
-            instructions << transform_expression(options, used: true)
-            instructions << IfInstruction.new
-            instructions << transform_body(body, used: true)
-            instructions << ElseInstruction.new(:if)
+        instructions << transform_expression(value, used: true)
+        clauses.each do |clause|
+          case clause.sexp_type
+          when :when
+            instructions += transform_case_when(clause)
+          when :in
+            instructions += transform_case_in(clause)
+          else
+            raise "Unknown case clause: #{clause.sexp_type}"
           end
         end
 
-        instructions << (else_block.nil? ? PushNilInstruction.new : transform_expression(else_block, used: true))
+        if else_block
+          instructions << transform_expression(else_block, used: true)
+        else
+          instructions << PushNilInstruction.new
+        end
 
-        instructions << [EndInstruction.new(:if)] * whens.length
+        instructions << [EndInstruction.new(:if)] * clauses.count { |c| c.sexp_type == :when }
 
         # The case value is never popped during comparison, so we have to pop it here.
-        instructions << [SwapInstruction.new, PopInstruction.new] if var
+        instructions << [SwapInstruction.new, PopInstruction.new]
 
         instructions << PopInstruction.new unless used
         instructions
+      end
+
+      def transform_case_when_without_value(exp, used:)
+        _, _, *clauses, else_block = exp
+        instructions = []
+
+        # glorified if-else
+        clauses.each do |clause|
+          # case
+          # when a == b, b == c, c == d
+          # =>
+          # if (a == b || b == c || c == d)
+          type, options, *body = clause
+
+          raise "unexpected case clause: #{type}" unless type == :when
+
+          # s(:array, option1, option2, ...)
+          # =>
+          # s(:or, option1, s(:or, option2, ...))
+          options = options[2..].reduce(options[1]) { |prev, option| s(:or, prev, option) }
+          instructions << transform_expression(options, used: true)
+          instructions << IfInstruction.new
+          instructions << transform_body(body, used: true)
+          instructions << ElseInstruction.new(:if)
+        end
+
+        if else_block
+          instructions << transform_expression(else_block, used: true)
+        else
+          instructions << PushNilInstruction.new
+        end
+
+        instructions << [EndInstruction.new(:if)] * clauses.size
+
+        instructions << PopInstruction.new unless used
+        instructions
+      end
+
+      def transform_case_when(exp)
+        instructions = []
+
+        # case a
+        # when b, c, d
+        # =>
+        # if (b === a || c === a || d === a)
+        _, options_array, *body = exp
+        _, *options = options_array
+
+        options.each do |option|
+          # Splats are handled in the backend.
+          # For C++, it's done in the is_case_equal() function.
+          if option.sexp_type == :splat
+            _, option = option
+            is_splat = true
+          else
+            is_splat = false
+          end
+
+          option_instructions = transform_expression(option, used: true)
+          instructions << option_instructions
+          instructions << CaseEqualInstruction.new(is_splat: is_splat)
+          instructions << IfInstruction.new
+          instructions << PushTrueInstruction.new
+          instructions << ElseInstruction.new(:if)
+        end
+        instructions << PushFalseInstruction.new
+        instructions << [EndInstruction.new(:if)] * options.length
+        instructions << IfInstruction.new
+        instructions << transform_body(body, used: true)
+        instructions << ElseInstruction.new(:if)
+
+        instructions
+      end
+
+      def transform_case_in(exp)
+        raise 'todo'
       end
 
       def transform_cdecl(exp, used:)
