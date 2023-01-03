c69ea949aaddff7263ece425d4d4831b7a5e7eee
Herwin Weststrate <herwin@snt.utwente.nl>
2023-01-03 21:58:53 +0100

    Import spec for String#unpack('w')
    Disable all the specs, since this one is not yet implemented

---
+++ spec/core/string/unpack/w_spec.rb
@@ -0,0 +1,39 @@
+# -*- encoding: binary -*-
+require_relative '../../../spec_helper'
+require_relative '../fixtures/classes'
+require_relative 'shared/basic'
+
+describe "String#unpack with directive 'w'" do
+  # it_behaves_like :string_unpack_basic, 'w'
+  # it_behaves_like :string_unpack_no_platform, 'w'
+
+  # NATFIXME: decodes a BER-compressed integer
+  xit "decodes a BER-compressed integer" do
+    [ ["\x00", [0]],
+      ["\x01", [1]],
+      ["\xce\x0f", [9999]],
+      ["\x84\x80\x80\x80\x80\x80\x80\x80\x80\x00", [2**65]]
+    ].should be_computed_by(:unpack, "w")
+  end
+
+  # NATFIXME: ignore NULL bytes between directive
+  xit "ignores NULL bytes between directives" do
+    "\x01\x02\x03".unpack("w\x00w").should == [1, 2]
+  end
+
+  # NATFIXME: ignore spaces between directives
+  xit "ignores spaces between directives" do
+    "\x01\x02\x03".unpack("w w").should == [1, 2]
+  end
+end
+
+# NATFIXME: String#unpack with directive 'w*
+xdescribe "String#unpack with directive 'w*'" do
+
+  it "decodes BER-compressed integers" do
+    "\x01\x02\x03\x04".unpack("w*").should == [1, 2, 3, 4]
+    "\x00\xCE\x0F\x84\x80\x80\x80\x80\x80\x80\x80\x80\x00\x01\x00".unpack("w*").should == [0, 9999, 2**65, 1, 0]
+    "\x81\x80\x80\x80\x80\x80\x80\x80\x80\x00\x90\x80\x80\x80\x80\x80\x80\x80\x03\x01\x02".unpack("w*").should == [2**63, (2**60 + 3), 1, 2]
+  end
+
+end
