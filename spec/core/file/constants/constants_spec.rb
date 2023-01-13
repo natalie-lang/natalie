5b3a9e17a7add8d64c1bbe118990f01c92029d98
Herwin Weststrate <herwin@snt.utwente.nl>
2023-01-13 14:13:14 +0100

    Import constants from fnmatch.h into File namespace
    Besides these entries from fnmatch.h, Ruby also adds three other FNM_
    constants. These appear to be unique to Ruby, and have not been added in
    this patch. These values probably have to be hardcoded.

--- spec/core/file/constants/constants_spec.rb
+++ spec/core/file/constants/constants_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../../../spec_helper'
 
-# NATFIXME: Implement FNM_* constants
+# NATFIXME: Implement missing FNM_* constants
 # ["APPEND", "CREAT", "EXCL", "FNM_CASEFOLD",
 #   "FNM_DOTMATCH", "FNM_EXTGLOB", "FNM_NOESCAPE", "FNM_PATHNAME",
 #   "FNM_SYSCASE", "LOCK_EX", "LOCK_NB", "LOCK_SH",
-["APPEND", "CREAT", "EXCL",
+["APPEND", "CREAT", "EXCL", "FNM_CASEFOLD",
+  "FNM_NOESCAPE", "FNM_PATHNAME",
   "LOCK_EX", "LOCK_NB", "LOCK_SH",
   "LOCK_UN", "NONBLOCK", "RDONLY",
   "RDWR", "TRUNC", "WRONLY"].each do |const|
