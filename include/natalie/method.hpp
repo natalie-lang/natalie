348f753fec1361f51b0d7425e0cfef446ca5e2d5
Herwin Weststrate <herwin@snt.utwente.nl>
2023-02-01 10:30:38 +0100

    Constify the Method::call method

--- include/natalie/method.hpp
+++ include/natalie/method.hpp
@@ -37,7 +37,7 @@ public:
     bool is_optimized() const { return m_optimized; }
     void set_optimized(bool optimized) { m_optimized = optimized; }
 
-    Value call(Env *env, Value self, Args args, Block *block);
+    Value call(Env *env, Value self, Args args, Block *block) const;
 
     String name() const { return m_name; }
     ModuleObject *owner() const { return m_owner; }
