710ac38c672cc4bf8f918786eecc30e90e7508ad
Herwin Weststrate <herwin@snt.utwente.nl>
2023-02-02 08:23:52 +0100

    Import bugfixes for bitwise Integer specs

--- spec/core/integer/bit_or_spec.rb
+++ spec/core/integer/bit_or_spec.rb
@@ -29,10 +29,11 @@ describe "Integer#|" do
       (-1 | 2**64).should == -1
     end
 
+    # NATFIXME: Upstream bug (https://github.com/ruby/spec/pull/1011)
     it "coerces the rhs and calls #coerce" do
-      obj = mock("fixnum bit and")
-      obj.should_receive(:coerce).with(6).and_return([3, 6])
-      (6 & obj).should == 2
+      obj = mock("fixnum bit or")
+      obj.should_receive(:coerce).with(6).and_return([6, 3])
+      (6 | obj).should == 7
     end
 
     it "raises a TypeError when passed a Float" do
