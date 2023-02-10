73f8962c43ed442e0caa9f0cd9bf4a6d7dada63b
Herwin Weststrate <herwin@snt.utwente.nl>
2023-02-10 20:27:25 +0100

    Convert NATFIXME comments in Float to blocks

--- spec/core/float/round_spec.rb
+++ spec/core/float/round_spec.rb
@@ -74,49 +74,54 @@ describe "Float#round" do
   end
 
   # redmine #5272
-  # NATFIXME: Support big number precision values
-  xit "returns rounded values for big values" do
-    +2.4e20.round(-20).should   eql( +2 * 10 ** 20  )
-    -2.4e20.round(-20).should   eql( -2 * 10 ** 20  )
-    +2.5e200.round(-200).should eql( +3 * 10 ** 200 )
-    +2.4e200.round(-200).should eql( +2 * 10 ** 200 )
-    -2.5e200.round(-200).should eql( -3 * 10 ** 200 )
-    -2.4e200.round(-200).should eql( -2 * 10 ** 200 )
+  it "returns rounded values for big values" do
+    NATFIXME 'Support big number precision values', exception: SpecFailedException do
+      +2.4e20.round(-20).should   eql( +2 * 10 ** 20  )
+      -2.4e20.round(-20).should   eql( -2 * 10 ** 20  )
+      +2.5e200.round(-200).should eql( +3 * 10 ** 200 )
+      +2.4e200.round(-200).should eql( +2 * 10 ** 200 )
+      -2.5e200.round(-200).should eql( -3 * 10 ** 200 )
+      -2.4e200.round(-200).should eql( -2 * 10 ** 200 )
+    end
   end
 
-  # TODO: keyword arguments to C++ functions
-  xit "returns different rounded values depending on the half option" do
-    2.5.round(half: nil).should      eql(3)
-    2.5.round(half: :up).should      eql(3)
-    2.5.round(half: :down).should    eql(2)
-    2.5.round(half: :even).should    eql(2)
-    3.5.round(half: nil).should      eql(4)
-    3.5.round(half: :up).should      eql(4)
-    3.5.round(half: :down).should    eql(3)
-    3.5.round(half: :even).should    eql(4)
-    (-2.5).round(half: nil).should   eql(-3)
-    (-2.5).round(half: :up).should   eql(-3)
-    (-2.5).round(half: :down).should eql(-2)
-    (-2.5).round(half: :even).should eql(-2)
+  it "returns different rounded values depending on the half option" do
+    NATFIXME 'Add keyword to Float#half', exception: TypeError, message: 'no implicit conversion of Hash into Integer' do
+      2.5.round(half: nil).should      eql(3)
+      2.5.round(half: :up).should      eql(3)
+      2.5.round(half: :down).should    eql(2)
+      2.5.round(half: :even).should    eql(2)
+      3.5.round(half: nil).should      eql(4)
+      3.5.round(half: :up).should      eql(4)
+      3.5.round(half: :down).should    eql(3)
+      3.5.round(half: :even).should    eql(4)
+      (-2.5).round(half: nil).should   eql(-3)
+      (-2.5).round(half: :up).should   eql(-3)
+      (-2.5).round(half: :down).should eql(-2)
+      (-2.5).round(half: :even).should eql(-2)
+    end
   end
 
-  # TODO: keyword arguments to C++ functions
-  xit "rounds self to an optionally given precision with a half option" do
-    5.55.round(1, half: nil).should eql(5.6)
-    5.55.round(1, half: :up).should eql(5.6)
-    5.55.round(1, half: :down).should eql(5.5)
-    5.55.round(1, half: :even).should eql(5.6)
+  it "rounds self to an optionally given precision with a half option" do
+    NATFIXME 'Add keyword to Float#half', exception: ArgumentError, message: 'wrong number of arguments' do
+      5.55.round(1, half: nil).should eql(5.6)
+      5.55.round(1, half: :up).should eql(5.6)
+      5.55.round(1, half: :down).should eql(5.5)
+      5.55.round(1, half: :even).should eql(5.6)
+    end
   end
 
-  # TODO: keyword arguments to C++ functions
-  xit "raises FloatDomainError for exceptional values with a half option" do
-    -> { (+infinity_value).round(half: :up) }.should raise_error(FloatDomainError)
-    -> { (-infinity_value).round(half: :down) }.should raise_error(FloatDomainError)
-    -> { nan_value.round(half: :even) }.should raise_error(FloatDomainError)
+  it "raises FloatDomainError for exceptional values with a half option" do
+    NATFIXME 'Add keyword to Float#half', exception: SpecFailedException do
+      -> { (+infinity_value).round(half: :up) }.should raise_error(FloatDomainError)
+      -> { (-infinity_value).round(half: :down) }.should raise_error(FloatDomainError)
+      -> { nan_value.round(half: :even) }.should raise_error(FloatDomainError)
+    end
   end
 
-  # TODO: keyword arguments to C++ functions
-  xit "raise for a non-existent round mode" do
-    -> { 14.2.round(half: :nonsense) }.should raise_error(ArgumentError, "invalid rounding mode: nonsense")
+  it "raise for a non-existent round mode" do
+    NATFIXME 'Add keyword to Float#half', exception: SpecFailedException do
+      -> { 14.2.round(half: :nonsense) }.should raise_error(ArgumentError, "invalid rounding mode: nonsense")
+    end
   end
 end
