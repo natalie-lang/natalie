25ef5470be72e7a41e3fe86d764a89f18915c4c7
Tim Morgan <tim@timmorgan.org>
2023-02-14 07:45:45 -0600

    Add a test for method visibility

--- test/natalie/method_visibility_test.rb
+++ test/natalie/method_visibility_test.rb
@@ -9,10 +9,20 @@ class Foo
     private_foo
   end
 
+  def public_foo_calling_protected_foo
+    protected_foo
+  end
+
   private
 
   def private_foo
     'private'
+  end
+
+  protected
+
+  def protected_foo
+    'protected'
   end
 
   public
@@ -30,6 +40,16 @@ end
 class Foo
   def another_public_foo
     'public'
+  end
+end
+
+class Bar < Foo
+  def public_bar_calling_protected_foo
+    protected_foo
+  end
+
+  def public_bar_calling_protected_foo_on_another_object
+    Foo.new.protected_foo
   end
 end
 
@@ -61,6 +81,29 @@ describe 'method visibility' do
 
     it 'is visible inside the class' do
       Foo.new.public_foo_calling_private_foo.should == 'private'
+    end
+  end
+
+  describe 'explicit protected' do
+    it 'is not visible outside the class' do
+      -> { Foo.new.protected_foo }.should raise_error(
+                                            NoMethodError,
+                                            /^protected method `protected_foo' called for #<Foo:0x\X+>/,
+                                          )
+    end
+
+    it 'is visible inside the class' do
+      Foo.new.public_foo_calling_protected_foo.should == 'protected'
+    end
+
+    it 'is visible from inside a subclass' do
+      Bar.new.public_bar_calling_protected_foo.should == 'protected'
+    end
+
+    it 'is visible from inside a subclass calling the method on another object (not self)' do
+      NATFIXME 'Need to rethink how method visiblity is determined', exception: NoMethodError do
+        Bar.new.public_bar_calling_protected_foo_on_another_object.should == 'protected'
+      end
     end
   end
 end
