d4ed80dc6837eaf7f3fe3a18a3efb9700830bbce
Ben Bowers <nanobowers@gmail.com>
2023-03-19 18:56:34 -0400

    Fix IO error spec

--- src/io.rb
+++ src/io.rb
@@ -8,8 +8,37 @@ class IO
   end
 
   def each
-    while line = gets
+    while (line = gets)
       yield line
     end
+  end
+
+  # The following are used in IO.select
+
+  module WaitReadable; end
+  module WaitWritable; end
+
+  class EAGAINWaitReadable < Errno::EAGAIN
+    include IO::WaitReadable
+  end
+
+  class EAGAINWaitWritable < Errno::EAGAIN
+    include IO::WaitWritable
+  end
+
+  class EWOULDBLOCKWaitReadable < Errno::EWOULDBLOCK
+    include IO::WaitReadable
+  end
+
+  class EWOULDBLOCKWaitWritable < Errno::EWOULDBLOCK
+    include IO::WaitWritable
+  end
+
+  class EINPROGRESSWaitReadable < Errno::EINPROGRESS
+    include IO::WaitReadable
+  end
+
+  class EINPROGRESSWaitWritable < Errno::EINPROGRESS
+    include IO::WaitWritable
   end
 end
