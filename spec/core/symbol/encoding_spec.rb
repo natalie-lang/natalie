20b5ba6355fb925cd16034de2bc43ab9d16b1dba
Herwin <herwinw@users.noreply.github.com>
2023-03-28 11:48:18 +0200

    Import spec for Symbol#encoding
    Disable all failing tests with NATFIXME

---
+++ spec/core/symbol/encoding_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+require_relative '../../spec_helper'
+
+describe "Symbol#encoding for ASCII symbols" do
+  it "is US-ASCII" do
+    NATFIXME 'Implement Symbol#encoding', exception: NoMethodError, message: "undefined method `encoding'" do
+      :foo.encoding.name.should == "US-ASCII"
+    end
+  end
+
+  it "is US-ASCII after converting to string" do
+    NATFIXME 'Automatically encode input as US-ASCII if input is ascii compatible', exception: SpecFailedException do
+      :foo.to_s.encoding.name.should == "US-ASCII"
+    end
+  end
+end
+
+describe "Symbol#encoding for UTF-8 symbols" do
+  it "is UTF-8" do
+    NATFIXME 'Implement Symbol#encoding', exception: NoMethodError, message: "undefined method `encoding'" do
+      :åäö.encoding.name.should == "UTF-8"
+    end
+  end
+
+  it "is UTF-8 after converting to string" do
+    :åäö.to_s.encoding.name.should == "UTF-8"
+  end
+end
