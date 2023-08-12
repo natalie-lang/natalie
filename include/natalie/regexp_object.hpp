1e8e2c9dd9819e0d1338eb9f97a78dc4247451ac
Herwin <herwinw@users.noreply.github.com>
2023-08-12 12:44:25 +0200

    Implement Regexp.try_convert

--- include/natalie/regexp_object.hpp
+++ include/natalie/regexp_object.hpp
@@ -58,6 +58,7 @@ public:
 
     static Value last_match(Env *, Value);
     static Value quote(Env *, Value);
+    static Value try_convert(Env *, Value);
 
     static Value literal(Env *env, const char *pattern, int options = 0) {
         auto regex = new RegexpObject(env, pattern, options);
