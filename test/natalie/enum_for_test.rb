756c2d38f44c5593058a3bdb0d64037f21101971
Herwin <herwinw@users.noreply.github.com>
2023-08-12 11:31:18 +0200

    Add test for arity of block in enum_for results

---
+++ test/natalie/enum_for_test.rb
@@ -0,0 +1,33 @@
+require_relative '../spec_helper'
+
+class Collection
+  include Enumerable
+
+  attr_reader :arity
+
+  def initialize(*arr)
+    @arr = arr
+  end
+
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    @arr.each(&block)
+  end
+
+  def mockingbird_each(&block)
+    return enum_for(:mockingbird_each) unless block_given?
+    @arity = block.arity
+    each { |x| block[x, x] }
+  end
+end
+
+describe 'enum_for arity validation' do
+  it 'returns the correct arity for the block' do
+    collection = Collection.new
+    collection.mockingbird_each { |x, y| x + y }
+    collection.arity.should == 2
+
+    collection.mockingbird_each.each { |x, y| x + y }
+    collection.arity.should == 2
+  end
+end
