8879abe340ba48479c6ae48b20b7634837f41db7
Herwin <herwinw@users.noreply.github.com>
2023-08-13 09:40:54 +0200

    Update the failing arity test
    Expect different behaviour for MRI and Natalie here. This way we can
    start to rewrite the enum_for method to C++ and validate that we don't
    have any changed behaviour.

--- test/natalie/enum_for_test.rb
+++ test/natalie/enum_for_test.rb
@@ -28,6 +28,12 @@ describe 'enum_for arity validation' do
     collection.arity.should == 2
 
     collection.mockingbird_each.each { |x, y| x + y }
-    collection.arity.should == 2
+    if RUBY_ENGINE == 'natalie'
+      # NATFIXME: No the desired behaviour, but we'll keep it during the rewrite to C++ to validate
+      #           that we made no behavioural changes.
+      collection.arity.should == -1
+    else
+      collection.arity.should == 2
+    end
   end
 end
