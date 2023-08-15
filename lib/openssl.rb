80dda1a9891100daca836f2aef760bdb19ea3253
Herwin <herwinw@users.noreply.github.com>
2023-08-15 16:35:13 +0200

    Scaffolding for OpenSSL::Digest
    Implement the base64 and hex conversions, and return a stub value in the
    digest

--- lib/openssl.rb
+++ lib/openssl.rb
@@ -7,4 +7,18 @@ module OpenSSL
   module Random
     __bind_static_method__ :random_bytes :OpenSSL_Random_random_bytes
   end
+
+  class Digest
+    def self.digest(cipher, data)
+      '' # TODO
+    end
+
+    def self.base64digest(cipher, data)
+      [digest(cipher, data)].pack('m0')
+    end
+
+    def self.hexdigest(cipher, data)
+      digest(cipher, data).unpack1('H*')
+    end
+  end
 end
