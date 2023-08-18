dbae59f5d157c773f5efe3a0980ffb81187f60b5
Herwin <herwinw@users.noreply.github.com>
2023-08-18 19:05:21 +0200

    Move digest method from subclasses to Digest
    Since we only depend on the `@name` instance variable, we can reduce all
    this repetition. This also means we no longer have to create a new
    subclass every time we call `digest` on the main class.

--- lib/openssl.rb
+++ lib/openssl.rb
@@ -10,11 +10,7 @@ module OpenSSL
 
   class Digest
     def self.digest(name, data)
-      klass = const_get(name.to_s.upcase.to_sym)
-      raise NameError unless klass.ancestors[1] == self
-      klass.new.digest(data)
-    rescue NameError
-      raise "Unsupported digest algorithm (#{name}).: unknown object name"
+      new(name).digest(data)
     end
 
     def self.base64digest(name, data)
@@ -28,47 +24,35 @@ module OpenSSL
     def initialize(name)
       klass = self.class.const_get(name.to_s.upcase.to_sym)
       raise NameError unless klass.ancestors[1] == self.class
-      @name = name.to_s.upcase.to_sym
+      @name = name.to_s.upcase
     rescue NameError
       raise "Unsupported digest algorithm (#{name}).: unknown object name"
     end
 
-    def digest(data)
-      self.class.const_get(@name).new.digest(data)
-    rescue NameError
-      raise "Unsupported digest algorithm (#{@name}).: unknown object name"
-    end
+    __bind_method__ :digest, :OpenSSL_Digest_digest
 
     class SHA1 < Digest
       def initialize()
         @name = 'SHA1'
       end
-
-      __bind_method__ :digest, :OpenSSL_Digest_digest
     end
 
     class SHA256 < Digest
       def initialize()
         @name = 'SHA256'
       end
-
-      __bind_method__ :digest, :OpenSSL_Digest_digest
     end
 
     class SHA384 < Digest
       def initialize()
         @name = 'SHA384'
       end
-
-      __bind_method__ :digest, :OpenSSL_Digest_digest
     end
 
     class SHA512 < Digest
       def initialize()
         @name = 'SHA512'
       end
-
-      __bind_method__ :digest, :OpenSSL_Digest_digest
     end
   end
 end
