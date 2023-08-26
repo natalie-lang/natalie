6c423efe1dc3a5574059fa670804ad1bee3e71dd
Herwin <herwinw@users.noreply.github.com>
2023-08-26 13:57:41 +0200

    Expose Fiber.current

--- spec/library/fiber/current_spec.rb
+++ spec/library/fiber/current_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../../spec_helper'
 
-require 'fiber'
+NATFIXME 'Add a stub fiber.rb', exception: LoadError do
+  require 'fiber'
+end
 
 describe "Fiber.current" do
   ruby_version_is "3.1" do
@@ -14,8 +16,12 @@ describe "Fiber.current" do
     root.should be_an_instance_of(Fiber)
     # We can always transfer to the root Fiber; it will never die
     5.times do
-      root.transfer.should be_nil
-      root.alive?.should be_true
+      NATFIXME 'Implement Fiber#transfer', exception: NoMethodError, message: "undefined method `transfer'" do
+        root.transfer.should be_nil
+      end
+      NATFIXME 'Implement Fiber#alive?', exception: NoMethodError, message: "undefined method `alive?'" do
+        root.alive?.should be_true
+      end
     end
   end
 
@@ -24,35 +30,39 @@ describe "Fiber.current" do
       this = Fiber.current
       this.should be_an_instance_of(Fiber)
       this.should == fiber
-      this.alive?.should be_true
+      NATFIXME 'Implement Fiber#alive?', exception: NoMethodError, message: "undefined method `alive?'" do
+        this.alive?.should be_true
+      end
     end
     fiber.resume
   end
 
   it "returns the current Fiber when called from a Fiber that transferred to another" do
-    states = []
-    fiber = Fiber.new do
-      states << :fiber
-      this = Fiber.current
-      this.should be_an_instance_of(Fiber)
-      this.should == fiber
-      this.alive?.should be_true
+    NATFIXME 'Implement Fiber#transfer', exception: NoMethodError, message: "undefined method `transfer'" do
+      states = []
+      fiber = Fiber.new do
+        states << :fiber
+        this = Fiber.current
+        this.should be_an_instance_of(Fiber)
+        this.should == fiber
+        this.alive?.should be_true
+      end
+
+      fiber2 = Fiber.new do
+        states << :fiber2
+        fiber.transfer
+        flunk
+      end
+
+      fiber3 = Fiber.new do
+        states << :fiber3
+        fiber2.transfer
+        states << :fiber3_terminated
+      end
+
+      fiber3.resume
+
+      states.should == [:fiber3, :fiber2, :fiber, :fiber3_terminated]
     end
-
-    fiber2 = Fiber.new do
-      states << :fiber2
-      fiber.transfer
-      flunk
-    end
-
-    fiber3 = Fiber.new do
-      states << :fiber3
-      fiber2.transfer
-      states << :fiber3_terminated
-    end
-
-    fiber3.resume
-
-    states.should == [:fiber3, :fiber2, :fiber, :fiber3_terminated]
   end
 end
