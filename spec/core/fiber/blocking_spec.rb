a65036a9968d96047000c3c9f2fe83ee4ddad66e
Herwin <herwinw@users.noreply.github.com>
2023-08-27 12:32:32 +0200

    Disable all faling tests for Fiber blocking

--- spec/core/fiber/blocking_spec.rb
+++ spec/core/fiber/blocking_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../../spec_helper'
 require_relative 'shared/blocking'
 
-require "fiber"
+NATFIXME 'Add a stub fiber.rb', exception: LoadError do
+  require "fiber"
+end
 
 describe "Fiber.blocking?" do
   it_behaves_like :non_blocking_fiber, -> { Fiber.blocking? }
@@ -9,23 +11,27 @@ describe "Fiber.blocking?" do
   context "when fiber is blocking" do
     context "root Fiber of the main thread" do
       it "returns 1 for blocking: true" do
-        fiber = Fiber.new(blocking: true) { Fiber.blocking? }
-        blocking = fiber.resume
-
-        blocking.should == 1
-      end
-    end
-
-    context "root Fiber of a new thread" do
-      it "returns 1 for blocking: true" do
-        thread = Thread.new do
+        NATFIXME 'Support keyword arguments in Fiber.new', exception: ArgumentError, message: 'wrong number of arguments (given 1, expected 0)' do
           fiber = Fiber.new(blocking: true) { Fiber.blocking? }
           blocking = fiber.resume
 
           blocking.should == 1
         end
+      end
+    end
 
-        thread.join
+    context "root Fiber of a new thread" do
+      it "returns 1 for blocking: true" do
+        NATFIXME 'Threads', exception: NameError, message: 'uninitialized constant Thread' do
+          thread = Thread.new do
+            fiber = Fiber.new(blocking: true) { Fiber.blocking? }
+            blocking = fiber.resume
+
+            blocking.should == 1
+          end
+
+          thread.join
+        end
       end
     end
   end
@@ -37,23 +43,27 @@ describe "Fiber#blocking?" do
   context "when fiber is blocking" do
     context "root Fiber of the main thread" do
       it "returns true for blocking: true" do
-        fiber = Fiber.new(blocking: true) { Fiber.current.blocking? }
-        blocking = fiber.resume
-
-        blocking.should == true
-      end
-    end
-
-    context "root Fiber of a new thread" do
-      it "returns true for blocking: true" do
-        thread = Thread.new do
+        NATFIXME 'Support keyword arguments in Fiber.new', exception: ArgumentError, message: 'wrong number of arguments (given 1, expected 0)' do
           fiber = Fiber.new(blocking: true) { Fiber.current.blocking? }
           blocking = fiber.resume
 
           blocking.should == true
         end
+      end
+    end
 
-        thread.join
+    context "root Fiber of a new thread" do
+      it "returns true for blocking: true" do
+        NATFIXME 'Threads', exception: NameError, message: 'uninitialized constant Thread' do
+          thread = Thread.new do
+            fiber = Fiber.new(blocking: true) { Fiber.current.blocking? }
+            blocking = fiber.resume
+
+            blocking.should == true
+          end
+
+          thread.join
+        end
       end
     end
   end
--- spec/core/fiber/shared/blocking.rb
+++ spec/core/fiber/shared/blocking.rb
@@ -1,41 +1,49 @@
 describe :non_blocking_fiber, shared: true do
   context "root Fiber of the main thread" do
     it "returns false" do
-      fiber = Fiber.new { @method.call }
-      blocking = fiber.resume
-
-      blocking.should == false
-    end
-
-    it "returns false for blocking: false" do
-      fiber = Fiber.new(blocking: false) { @method.call }
-      blocking = fiber.resume
-
-      blocking.should == false
-    end
-  end
-
-  context "root Fiber of a new thread" do
-    it "returns false" do
-      thread = Thread.new do
+      NATFIXME 'Implement Fiber.blocking? and Fiber#blocking?', exception: NoMethodError, message: "undefined method `blocking?'" do
         fiber = Fiber.new { @method.call }
         blocking = fiber.resume
 
         blocking.should == false
       end
-
-      thread.join
     end
 
     it "returns false for blocking: false" do
-      thread = Thread.new do
+      NATFIXME 'Support keyword arguments in Fiber.new', exception: ArgumentError, message: 'wrong number of arguments (given 1, expected 0)' do
         fiber = Fiber.new(blocking: false) { @method.call }
         blocking = fiber.resume
 
         blocking.should == false
       end
+    end
+  end
 
-      thread.join
+  context "root Fiber of a new thread" do
+    it "returns false" do
+      NATFIXME 'Threads', exception: NameError, message: 'uninitialized constant Thread' do
+        thread = Thread.new do
+          fiber = Fiber.new { @method.call }
+          blocking = fiber.resume
+
+          blocking.should == false
+        end
+
+        thread.join
+      end
+    end
+
+    it "returns false for blocking: false" do
+      NATFIXME 'Threads', exception: NameError, message: 'uninitialized constant Thread' do
+        thread = Thread.new do
+          fiber = Fiber.new(blocking: false) { @method.call }
+          blocking = fiber.resume
+
+          blocking.should == false
+        end
+
+        thread.join
+      end
     end
   end
 end
