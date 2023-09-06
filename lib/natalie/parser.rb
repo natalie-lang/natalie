8574221b2d1c1f3ea885393e730c3c62e37de1f9
Tim Morgan <tim@timmorgan.org>
2023-09-06 06:39:46 -0500

    Get examples/fib.rb parsing

--- lib/natalie/parser.rb
+++ lib/natalie/parser.rb
@@ -7,8 +7,12 @@ class SexpVisitor < ::YARP::BasicVisitor
     @path = path
   end
 
+  def visit_alias_node(node)
+    s(:alias, visit(node.new_name), visit(node.old_name), location: node.location)
+  end
+
   def visit_call_node(node)
-    arguments = node.child_nodes[1].child_nodes
+    arguments = node.arguments&.child_nodes || []
     s(:call,
       visit(node.child_nodes.first),
       node.name.to_sym,
@@ -16,23 +20,78 @@ class SexpVisitor < ::YARP::BasicVisitor
       location: node.location)
   end
 
+  def visit_constant_read_node(node)
+    name = node.slice.to_sym
+    s(:const, name, location: node.location)
+  end
+
+  def visit_constant_write_node(node)
+    s(:cdecl, node.name.to_sym, visit(node.value), location: node.location)
+  end
+
+  def visit_def_node(node)
+    _unknown, parameters_node, statements_node = node.child_nodes
+    s(:defn, node.name, visit(parameters_node), visit(statements_node), location: node.location)
+  end
+
+  def visit_else_node(node)
+    visit(node.child_nodes.first)
+  end
+
+  def visit_false_node(node)
+    s(:false, location: node.location)
+  end
+
+  def visit_if_node(node)
+    condition, true_body, false_body = node.child_nodes
+    s(:if, visit(condition), visit(true_body), visit(false_body), location: node.location)
+  end
+
   def visit_integer_node(node)
     s(:lit, node.value, location: node.location)
+  end
+
+  def visit_local_variable_read_node(node)
+    s(:lvar, node.name, location: node.location)
+  end
+
+  def visit_local_variable_write_node(node)
+    s(:lasgn, node.name, visit(node.value), location: node.location)
+  end
+
+  def visit_parameters_node(node)
+    return Sexp.new(:args) unless node
+
+    args = s(:args, location: node.location)
+    node.child_nodes.each do |n|
+      case n
+      when nil
+        :noop
+      when YARP::RequiredParameterNode
+        args << n.name
+      else
+        raise "don't yet know how to handle #{n.inspect}"
+      end
+    end
+    args
   end
 
   def visit_statements_node(node)
     s(:block, *node.child_nodes.map { |n| visit(n) }, location: node.location)
   end
-
   alias visit_program_node visit_statements_node
 
-  def visit_alias_node(node)
-    s(:alias, visit(node.new_name), visit(node.old_name), location: node.location)
+  def visit_string_node(node)
+    s(:str, node.content, location: node.location)
+  end
+
+  def visit_true_node(node)
+    s(:true, location: node.location)
   end
 
   private
 
-  def s(*items, location: nil)
+  def s(*items, location:)
     Sexp.new(*items, location:, file: @path)
   end
 end
@@ -43,8 +102,8 @@ class Sexp < Array
     parts.each_with_index do |part, index|
       self[index] = part
       self.file = file
-      self.line = location.start_line
-      self.column = location.start_column
+      self.line = location&.start_line
+      self.column = location&.start_column
     end
   end
 
