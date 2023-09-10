fbe6b9ace2b0b941291fc0b7f397afae2a98f092
Herwin <herwinw@users.noreply.github.com>
2023-09-10 10:02:56 +0200

    Fix binmode in TCPSocket

--- spec/library/socket/tcpsocket/initialize_spec.rb
+++ spec/library/socket/tcpsocket/initialize_spec.rb
@@ -41,9 +41,7 @@ describe 'TCPSocket#initialize' do
 
       it 'set the socket to binmode' do
         @client = TCPSocket.new(ip_address, @port)
-        NATFIXME 'set the socket to binmode', exception: SpecFailedException do
-          @client.binmode?.should be_true
-        end
+        @client.binmode?.should be_true
       end
 
       it 'connects to the right address' do
