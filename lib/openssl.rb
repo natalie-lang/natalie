0bf27af7090cc014f353158d7e3b14ca58e0bc9b
Tim Morgan <tim@timmorgan.org>
2023-09-14 06:17:04 -0500

    Fix missing comma in lib/openssl.rb

--- lib/openssl.rb
+++ lib/openssl.rb
@@ -5,7 +5,7 @@ __ld_flags__ '-lcrypto'
 
 module OpenSSL
   module Random
-    __bind_static_method__ :random_bytes :OpenSSL_Random_random_bytes
+    __bind_static_method__ :random_bytes, :OpenSSL_Random_random_bytes
   end
 
   class Digest
