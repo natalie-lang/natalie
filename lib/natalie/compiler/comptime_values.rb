1d447579f8ca3dd719033d58c097f3dfcc531e87
Tim Morgan <tim@timmorgan.org>
2023-09-29 14:41:12 -0500

    Raise a better error for an unexpected comptime value

--- lib/natalie/compiler/comptime_values.rb
+++ lib/natalie/compiler/comptime_values.rb
@@ -1,27 +1,31 @@
 module Natalie
   class Compiler
     module ComptimeValues
-      def comptime_array_of_strings(exp)
-        unless exp.sexp_type == :array
-          raise "Expected array at compile time, but got: #{exp.inspect}"
+      def comptime_array_of_strings(node)
+        unless node.sexp_type == :array
+          raise_comptime_value_error('array', node)
         end
-        exp[1..].map do |item|
+        node[1..].map do |item|
           comptime_string(item)
         end
       end
 
-      def comptime_string(exp)
-        unless exp.sexp_type == :str
-          raise "Expected string at compile time, but got: #{exp.inspect}"
+      def comptime_string(node)
+        unless node.sexp_type == :str
+          raise_comptime_value_error('string', node)
         end
-        exp.last
+        node.last
       end
 
-      def comptime_symbol(exp)
-        unless exp.sexp_type == :lit && exp.last.is_a?(Symbol)
-          raise "Expected symbol at compile time, but got: #{exp.inspect}"
+      def comptime_symbol(node)
+        unless node.sexp_type == :lit && node.last.is_a?(Symbol)
+          raise_comptime_value_error('symbol', node)
         end
-        exp.last
+        node.last
+      end
+
+      def raise_comptime_value_error(expected, node)
+          raise ArgumentError, "expected #{expected} at compile time, but got: #{node.inspect} (#{node.file}##{node.line})"
       end
     end
   end
