63efa5aed032cb107e37908f9e60b74d4e306d80
Tim Morgan <tim@timmorgan.org>
2023-10-04 21:30:41 -0500

    Fix function name for librubyparser in Fiddle test

--- test/natalie/fiddle_test.rb
+++ test/natalie/fiddle_test.rb
@@ -4,7 +4,7 @@ require 'fiddle'
 describe 'Fiddle' do
   it 'can open a library and call a function' do
     lib = Fiddle.dlopen("build/librubyparser.#{RbConfig::CONFIG['SOEXT']}")
-    fn = Fiddle::Function.new(lib['yp_version'], [], Fiddle::TYPE_VOIDP)
+    fn = Fiddle::Function.new(lib['pm_version'], [], Fiddle::TYPE_VOIDP)
     fn.call.to_s.should =~ /^\d+.\d+\.\d+$/
   end
 end
