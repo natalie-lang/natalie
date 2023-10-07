e1f49e9de4f0622668f27f313a8fca2e7ff71c33
Tim Morgan <tim@timmorgan.org>
2023-10-06 19:33:28 -0500

    Fix loading a file more than once
    This should continue to return `true` and execute the code again.

--- lib/natalie/compiler/instructions/with_filename_instruction.rb
+++ lib/natalie/compiler/instructions/with_filename_instruction.rb
@@ -29,10 +29,8 @@ module Natalie
           transform.compiled_files[@filename] = fn
           transform.with_new_scope(body) do |t|
             fn_code = []
-            fn_code << "Value #{fn}(Env *env, Value self) {"
-            if @require_once
-              fn_code << "if (#{transform.files_var_name}.get(#{filename_sym})) return FalseObject::the();"
-            end
+            fn_code << "Value #{fn}(Env *env, Value self, bool require_once) {"
+            fn_code << "if (require_once && #{transform.files_var_name}.get(#{filename_sym})) return FalseObject::the();"
             fn_code << "#{transform.files_var_name}.set(#{filename_sym});"
             fn_code << t.transform
             fn_code << 'return TrueObject::the();'
@@ -42,7 +40,7 @@ module Natalie
         end
         transform.exec_and_push(
           :result_of_with_filename,
-          "#{fn}(env, GlobalEnv::the()->main_obj())"
+          "#{fn}(env, GlobalEnv::the()->main_obj(), #{@require_once})"
         )
       end
 
--- test/support/require/simple_again.rb
+++ test/support/require/simple_again.rb
@@ -1,3 +1,6 @@
 def simple_again
   'simple_again'
 end
+
+$simple_again_loaded ||= 0
+$simple_again_loaded += 1
