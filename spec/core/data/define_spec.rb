964e7c6420be052ba0c17130b64b2094e615f9b1
Herwin <herwinw@users.noreply.github.com>
2023-10-10 18:29:20 +0200

    Wrap failing Data tests in NATFIXME blocks

--- spec/core/data/define_spec.rb
+++ spec/core/data/define_spec.rb
@@ -4,33 +4,43 @@ require_relative 'fixtures/classes'
 ruby_version_is "3.2" do
   describe "Data.define" do
     it "accepts no arguments" do
-      empty_data = Data.define
-      empty_data.members.should == []
+      NATFIXME 'Implement Data.define', exception: NoMethodError, message: "undefined method `define' for Data:Class" do
+        empty_data = Data.define
+        empty_data.members.should == []
+      end
     end
 
     it "accepts symbols" do
-      movie = Data.define(:title, :year)
-      movie.members.should == [:title, :year]
+      NATFIXME 'Implement Data.define', exception: NoMethodError, message: "undefined method `define' for Data:Class" do
+        movie = Data.define(:title, :year)
+        movie.members.should == [:title, :year]
+      end
     end
 
     it "accepts strings" do
-      movie = Data.define("title", "year")
-      movie.members.should == [:title, :year]
+      NATFIXME 'Implement Data.define', exception: NoMethodError, message: "undefined method `define' for Data:Class" do
+        movie = Data.define("title", "year")
+        movie.members.should == [:title, :year]
+      end
     end
 
     it "accepts a mix of strings and symbols" do
-      movie = Data.define("title", :year, "genre")
-      movie.members.should == [:title, :year, :genre]
+      NATFIXME 'Implement Data.define', exception: NoMethodError, message: "undefined method `define' for Data:Class" do
+        movie = Data.define("title", :year, "genre")
+        movie.members.should == [:title, :year, :genre]
+      end
     end
 
     it "accepts a block" do
-      movie = Data.define(:title, :year) do
-        def title_with_year
-          "#{title} (#{year})"
+      NATFIXME 'Implement Data.define', exception: NoMethodError, message: "undefined method `define' for Data:Class" do
+        movie = Data.define(:title, :year) do
+          def title_with_year
+            "#{title} (#{year})"
+          end
         end
+        movie.members.should == [:title, :year]
+        movie.new("Matrix", 1999).title_with_year.should == "Matrix (1999)"
       end
-      movie.members.should == [:title, :year]
-      movie.new("Matrix", 1999).title_with_year.should == "Matrix (1999)"
     end
   end
 end
--- spec/core/data/initialize_spec.rb
+++ spec/core/data/initialize_spec.rb
@@ -4,55 +4,69 @@ require_relative 'fixtures/classes'
 ruby_version_is "3.2" do
   describe "Data#initialize" do
     it "accepts positional arguments" do
-      data = DataSpecs::Measure.new(42, "km")
+      NATFIXME 'Implement Data.define', exception: NameError, message: 'uninitialized constant DataSpecs::Measure' do
+        data = DataSpecs::Measure.new(42, "km")
 
-      data.amount.should == 42
-      data.unit.should == "km"
+        data.amount.should == 42
+        data.unit.should == "km"
+      end
     end
 
     it "accepts alternative positional arguments" do
-      data = DataSpecs::Measure[42, "km"]
+      NATFIXME 'Implement Data.define', exception: NameError, message: 'uninitialized constant DataSpecs::Measure' do
+        data = DataSpecs::Measure[42, "km"]
 
-      data.amount.should == 42
-      data.unit.should == "km"
+        data.amount.should == 42
+        data.unit.should == "km"
+      end
     end
 
     it "accepts keyword arguments" do
-      data = DataSpecs::Measure.new(amount: 42, unit: "km")
+      NATFIXME 'Implement Data.define', exception: NameError, message: 'uninitialized constant DataSpecs::Measure' do
+        data = DataSpecs::Measure.new(amount: 42, unit: "km")
 
-      data.amount.should == 42
-      data.unit.should == "km"
+        data.amount.should == 42
+        data.unit.should == "km"
+      end
     end
 
     it "accepts alternative keyword arguments" do
-      data = DataSpecs::Measure[amount: 42, unit: "km"]
+      NATFIXME 'Implement Data.define', exception: NameError, message: 'uninitialized constant DataSpecs::Measure' do
+        data = DataSpecs::Measure[amount: 42, unit: "km"]
 
-      data.amount.should == 42
-      data.unit.should == "km"
+        data.amount.should == 42
+        data.unit.should == "km"
+      end
     end
 
     it "raises ArgumentError if no arguments are given" do
-      -> {
-        DataSpecs::Measure.new
-      }.should raise_error(ArgumentError) { |e|
-        e.message.should.include?("missing keywords: :amount, :unit")
-      }
+      NATFIXME 'Implement Data.define', exception: SpecFailedException do
+        -> {
+          DataSpecs::Measure.new
+        }.should raise_error(ArgumentError) { |e|
+          e.message.should.include?("missing keywords: :amount, :unit")
+        }
+      end
     end
 
     it "raises ArgumentError if at least one argument is missing" do
-      -> {
-        DataSpecs::Measure.new(unit: "km")
-      }.should raise_error(ArgumentError) { |e|
-        e.message.should.include?("missing keyword: :amount")
-      }
+      NATFIXME 'Implement Data.define', exception: SpecFailedException do
+        -> {
+          DataSpecs::Measure.new(unit: "km")
+        }.should raise_error(ArgumentError) { |e|
+          e.message.should.include?("missing keyword: :amount")
+        }
+      end
     end
 
     it "raises ArgumentError if unknown keyword is given" do
-      -> {
-        DataSpecs::Measure.new(amount: 42, unit: "km", system: "metric")
-      }.should raise_error(ArgumentError) { |e|
-        e.message.should.include?("unknown keyword: :system")
-      }
+      NATFIXME 'Implement Data.define', exception: SpecFailedException do
+        -> {
+          DataSpecs::Measure.new(amount: 42, unit: "km", system: "metric")
+        }.should raise_error(ArgumentError) { |e|
+          e.message.should.include?("unknown keyword: :system")
+        }
+      end
     end
   end
 end
