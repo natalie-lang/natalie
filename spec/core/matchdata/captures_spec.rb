63e492102ad7f2434583214d0e3528bd8a3aebc0
Herwin <herwinw@users.noreply.github.com>
2023-10-19 20:31:32 +0200

    Refresh specs for MatchData#captures

--- spec/core/matchdata/captures_spec.rb
+++ spec/core/matchdata/captures_spec.rb
@@ -1,13 +1,6 @@
 require_relative '../../spec_helper'
-require_relative 'fixtures/classes'
+require_relative 'shared/captures'
 
 describe "MatchData#captures" do
-  it "returns an array of the match captures" do
-    /(.)(.)(\d+)(\d)/.match("THX1138.").captures.should == ["H","X","113","8"]
-  end
-
-  it "returns instances of String when given a String subclass" do
-    str = MatchDataSpecs::MyString.new("THX1138: The Movie")
-    /(.)(.)(\d+)(\d)/.match(str).captures.each { |c| c.should be_an_instance_of(String) }
-  end
+  it_behaves_like :matchdata_captures, :captures
 end
