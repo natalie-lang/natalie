8b4835a8106fc760dd3759c1af1f4d6d7408afbb
Herwin <herwinw@users.noreply.github.com>
2023-11-01 17:51:52 +0100

    Add specs for OpenSSL::Cipher#key_len

--- test/natalie/openssl_cipher/ciphers_test.rb
+++ test/natalie/openssl_cipher/ciphers_test.rb
@@ -11,6 +11,7 @@ describe "OpenSSL::Cipher.ciphers" do
   end
 
   it "includes all the ciphers we use for testing" do
+    OpenSSL::Cipher.ciphers.should.include?("aes-128-cbc")
     OpenSSL::Cipher.ciphers.should.include?("aes-256-cbc")
     OpenSSL::Cipher.ciphers.should.include?("chacha20")
   end
