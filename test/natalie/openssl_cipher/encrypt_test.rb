6fac6376ea0e2802bc744fb8d135528492cb5663
Herwin <herwinw@users.noreply.github.com>
2023-11-01 12:10:50 +0100

    Add rudimentary specs for OpenSSL::Cipher#encrypt

---
+++ test/natalie/openssl_cipher/encrypt_test.rb
@@ -0,0 +1,15 @@
+require_relative '../../../spec/spec_helper'
+require 'openssl'
+
+describe "OpenSSL::Cipher#encrypt" do
+  it "can be called" do
+    cipher = OpenSSL::Cipher.new("aes-256-cbc")
+    cipher.encrypt.should == cipher
+  end
+
+  it "can be called multiple times" do
+    cipher = OpenSSL::Cipher.new("aes-256-cbc")
+    cipher.encrypt
+    cipher.encrypt.should == cipher
+  end
+end
