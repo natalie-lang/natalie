e761d7cd5967206430770046b6e7edcd4154678e
Herwin <herwinw@users.noreply.github.com>
2023-11-04 17:16:54 +0100

    Wrap failing tests of OpenSSL::X509::Name.parse in NATFIXME blocks
    Try to create the expected object via calls to #add_entry. This way we
    at least hit the #to_s tests, and we can see more clearly that we need
    to implement #to_a as well.

--- spec/library/openssl/x509/name/parse_spec.rb
+++ spec/library/openssl/x509/name/parse_spec.rb
@@ -4,45 +4,65 @@ require 'openssl'
 describe "OpenSSL::X509::Name.parse" do
   it "parses a /-delimited string of key-value pairs into a Name" do
     dn = "/DC=org/DC=ruby-lang/CN=www.ruby-lang.org"
-    name = OpenSSL::X509::Name.parse(dn)
+    NATFIXME 'Implement OpenSSL::X509::Name.parse', exception: NoMethodError, message: "undefined method `parse' for OpenSSL::X509::Name:Class" do
+      name = OpenSSL::X509::Name.parse(dn)
+    end
+    name = OpenSSL::X509::Name.new
+    name.add_entry('DC', 'org')
+    name.add_entry('DC', 'ruby-lang')
+    name.add_entry('CN', 'www.ruby-lang.org')
 
     name.to_s.should == dn
 
-    ary = name.to_a
+    NATFIXME 'Implement OpenSSL::X509::Name#to_a', exception: NoMethodError, message: "undefined method `to_a'" do
+      ary = name.to_a
 
-    ary[0][0].should == "DC"
-    ary[1][0].should == "DC"
-    ary[2][0].should == "CN"
-    ary[0][1].should == "org"
-    ary[1][1].should == "ruby-lang"
-    ary[2][1].should == "www.ruby-lang.org"
-    ary[0][2].should == OpenSSL::ASN1::IA5STRING
-    ary[1][2].should == OpenSSL::ASN1::IA5STRING
-    ary[2][2].should == OpenSSL::ASN1::UTF8STRING
+      ary[0][0].should == "DC"
+      ary[1][0].should == "DC"
+      ary[2][0].should == "CN"
+      ary[0][1].should == "org"
+      ary[1][1].should == "ruby-lang"
+      ary[2][1].should == "www.ruby-lang.org"
+      ary[0][2].should == OpenSSL::ASN1::IA5STRING
+      ary[1][2].should == OpenSSL::ASN1::IA5STRING
+      ary[2][2].should == OpenSSL::ASN1::UTF8STRING
+    end
   end
 
   it "parses a comma-delimited string of key-value pairs into a name" do
     dn = "DC=org, DC=ruby-lang, CN=www.ruby-lang.org"
-    name = OpenSSL::X509::Name.parse(dn)
+    NATFIXME 'Implement OpenSSL::X509::Name.parse', exception: NoMethodError, message: "undefined method `parse' for OpenSSL::X509::Name:Class" do
+      name = OpenSSL::X509::Name.parse(dn)
+    end
+    name = OpenSSL::X509::Name.new
+    name.add_entry('DC', 'org')
+    name.add_entry('DC', 'ruby-lang')
+    name.add_entry('CN', 'www.ruby-lang.org')
 
     name.to_s.should == "/DC=org/DC=ruby-lang/CN=www.ruby-lang.org"
 
-    ary = name.to_a
+    NATFIXME 'Implement OpenSSL::X509::Name#to_a', exception: NoMethodError, message: "undefined method `to_a'" do
+      ary = name.to_a
 
-    ary[0][1].should == "org"
-    ary[1][1].should == "ruby-lang"
-    ary[2][1].should == "www.ruby-lang.org"
+      ary[0][1].should == "org"
+      ary[1][1].should == "ruby-lang"
+      ary[2][1].should == "www.ruby-lang.org"
+    end
   end
 
   it "raises TypeError if the given string contains no key/value pairs" do
-    -> do
-      OpenSSL::X509::Name.parse("hello")
-    end.should raise_error(TypeError)
+    NATFIXME 'Implement OpenSSL::X509::Name.parse', exception: SpecFailedException do
+      -> do
+        OpenSSL::X509::Name.parse("hello")
+      end.should raise_error(TypeError)
+    end
   end
 
   it "raises OpenSSL::X509::NameError if the given string contains invalid keys" do
-    -> do
-      OpenSSL::X509::Name.parse("hello=goodbye")
-    end.should raise_error(OpenSSL::X509::NameError)
+    NATFIXME 'Implement OpenSSL::X509::Name.parse', exception: SpecFailedException do
+      -> do
+        OpenSSL::X509::Name.parse("hello=goodbye")
+      end.should raise_error(OpenSSL::X509::NameError)
+    end
   end
 end
