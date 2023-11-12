579d03414e13ff6c899338a6afea6c7843a070c5
Herwin <herwinw@users.noreply.github.com>
2023-11-12 16:36:37 +0100

    Implement Date.parse for numeric values

--- spec/library/date/parse_spec.rb
+++ spec/library/date/parse_spec.rb
@@ -7,18 +7,24 @@ require 'date'
 describe "Date#parse" do
   # The space separator is also different, doesn't work for only numbers
   it "parses a day name into a Date object" do
-    d = Date.parse("friday")
-    d.should == Date.commercial(d.cwyear, d.cweek, 5)
+    NATFIXME 'Support string format', exception: Date::Error do
+      d = Date.parse("friday")
+      d.should == Date.commercial(d.cwyear, d.cweek, 5)
+    end
   end
 
   it "parses a month name into a Date object" do
-    d = Date.parse("october")
-    d.should == Date.civil(Date.today.year, 10)
+    NATFIXME 'Support string format', exception: Date::Error do
+      d = Date.parse("october")
+      d.should == Date.civil(Date.today.year, 10)
+    end
   end
 
   it "parses a month day into a Date object" do
-    d = Date.parse("5th")
-    d.should == Date.civil(Date.today.year, Date.today.month, 5)
+    NATFIXME 'Support string format', exception: Date::Error do
+      d = Date.parse("5th")
+      d.should == Date.civil(Date.today.year, Date.today.month, 5)
+    end
   end
 
   # Specs using numbers
@@ -32,11 +38,13 @@ describe "Date#parse" do
   end
 
   it "parses DDD as year day number" do
-    d = Date.parse("100")
-    if Date.gregorian_leap?(Date.today.year)
-      d.should == Date.civil(Date.today.year, 4, 9)
-    else
-      d.should == Date.civil(Date.today.year, 4, 10)
+    NATFIXME 'Support DDD', exception: Date::Error do
+      d = Date.parse("100")
+      if Date.gregorian_leap?(Date.today.year)
+        d.should == Date.civil(Date.today.year, 4, 9)
+      else
+        d.should == Date.civil(Date.today.year, 4, 10)
+      end
     end
   end
 
@@ -46,8 +54,10 @@ describe "Date#parse" do
   end
 
   it "parses YYDDD as year and day number in 1969--2068" do
-    d = Date.parse("10100")
-    d.should == Date.civil(2010, 4, 10)
+    NATFIXME 'Support YYDDD', exception: Date::Error do
+      d = Date.parse("10100")
+      d.should == Date.civil(2010, 4, 10)
+    end
   end
 
   it "parses YYMMDD as year, month and day in 1969--2068" do
@@ -56,8 +66,10 @@ describe "Date#parse" do
   end
 
   it "parses YYYYDDD as year and day number" do
-    d = Date.parse("1910100")
-    d.should == Date.civil(1910, 4, 10)
+    NATFIXME 'Support YYYYDDD', exception: Date::Error do
+      d = Date.parse("1910100")
+      d.should == Date.civil(1910, 4, 10)
+    end
   end
 
   it "parses YYYYMMDD as year, month and day number" do
