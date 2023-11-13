fe49262a07c379c8664cdc7aaf869f41fcb2fe4f
Tim Morgan <tim@timmorgan.org>
2023-11-13 08:57:16 -0600

    Execute bytecode

--- lib/vm.rb
+++ lib/vm.rb
@@ -3,8 +3,10 @@ require_relative '../lib/natalie/compiler/instructions/meta'
 class VM
   INSTRUCTIONS = Natalie::Compiler::INSTRUCTIONS_META
 
-  def initialize(io)
+  def initialize(io:, main:)
     @io = io
+    @stack = []
+    @self = main
   end
 
   class Instruction
@@ -23,12 +25,42 @@ class VM
   end
 
   def execute
-    instructions = load_instructions
-    puts instructions
-    # TODO: something
+    load_instructions.each do |instruction|
+      send "execute_#{instruction.type}", instruction
+    end
   end
 
   private
+
+  def execute_pop(_)
+    @stack.pop
+  end
+
+  def execute_push_argc(instruction)
+    @stack << instruction.args.first
+  end
+
+  def execute_push_self(_)
+    @stack << @self
+  end
+
+  def execute_push_string(instruction)
+    @stack << instruction.args.first
+  end
+
+  def execute_send(instruction)
+    message = instruction.args[0].to_sym
+    receiver_is_self, with_block, args_array_on_stack, has_keyword_hash = instruction.args[1]
+    argc = @stack.pop
+    args = 1.upto(argc).map { @stack.pop }
+    receiver = @stack.pop
+    result = if receiver_is_self
+      receiver.send(message, *args)
+    else
+      receiver.public_send(message, *args)
+    end
+    @stack << result
+  end
 
   def load_instructions
     instructions = []
@@ -82,5 +114,8 @@ class VM
 end
 
 if __FILE__ == $0
-  VM.new(File.open(ARGV.first, 'rb')).execute
+  VM.new(
+    io: File.open(ARGV.first, 'rb'),
+    main: self,
+  ).execute
 end
