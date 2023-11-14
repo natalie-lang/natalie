cd6e4e7fe05147875097f58e759dc8598ffddb27
Tim Morgan <tim@timmorgan.org>
2023-11-14 06:26:56 -0800

    Fix PP.pp with generic object
    We don't support instance_eval with a string, but instance_variable_get
    works fine here.

---
+++ test/natalie/pp_test.rb
@@ -0,0 +1,19 @@
+require_relative '../spec_helper'
+require 'stringio'
+require 'pp'
+
+class Foo
+  def initialize
+    @foo = 'foo'
+  end
+end
+
+describe 'PP' do
+  it 'it handles generic objects' do
+    out = ''
+    io = StringIO.new(out)
+    obj = Foo.new
+    PP.pp obj, io
+    out.should =~ /\A#<Foo:0x[0-9a-f]+ @foo="foo">\n\z/
+  end
+end
