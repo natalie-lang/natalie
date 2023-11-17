50eff94925de21fe856c8f1936f71de8b52357cc
Herwin <herwinw@users.noreply.github.com>
2023-11-17 17:42:36 +0100

    Implement first version of Digest::Instance
    Good enough to pass the specs, but completely useless

--- lib/digest.rb
+++ lib/digest.rb
@@ -3,6 +3,14 @@ require 'digest/sha1'
 require 'digest/sha2'
 
 module Digest
+  module Instance
+    def update(_)
+      raise "#{self} does not implement update()"
+    end
+
+    alias << update
+  end
+
   def self.hexencode(str)
     str = str.to_str if !str.is_a?(String) && str.respond_to?(:to_str)
     raise TypeError, "no implicit conversion of #{str.nil? ? 'nil' : str.class} into String" unless str.is_a?(String)
