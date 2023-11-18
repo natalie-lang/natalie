359b0e64434ddb44de2a47180c762d5b551ec287
Herwin <herwinw@users.noreply.github.com>
2023-11-18 17:49:49 +0100

    Add Psych::VERSION to our YAML lib
    This is checked in the specs.

--- lib/yaml.rb
+++ lib/yaml.rb
@@ -7,6 +7,11 @@ module YAML
   __bind_static_method__ :dump, :YAML_dump
 end
 
+# Compatibility version, required to run the specs
+module Psych
+  VERSION = '4.0.0'.freeze
+end
+
 class Object
   def to_yaml
     YAML.dump(self)
