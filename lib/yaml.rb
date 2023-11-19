d15c42a048bf2583ec82d81a4e6238a9869ea702
Herwin <herwinw@users.noreply.github.com>
2023-11-19 19:30:15 +0100

    Implement YAML.load_file

--- lib/yaml.rb
+++ lib/yaml.rb
@@ -6,6 +6,10 @@ __ld_flags__ '-lyaml'
 module YAML
   __bind_static_method__ :dump, :YAML_dump
   __bind_static_method__ :load, :YAML_load
+
+  def self.load_file(file)
+    File.open(file) { |fh| self.load(fh) }
+  end
 end
 
 # Compatibility version, required to run the specs
