cc13fdce2d45e9ca119b64bfa340e45d18427c69
Tim Morgan <tim@timmorgan.org>
2023-11-26 07:13:28 -0600

    Remove hack to run certain specs serially
    This didn't really fix any problem, as the bugs were all related to threads
    within a process -- not multiple processes conflicting with each other.

--- test/ruby/ruby_specs_test.rb
+++ test/ruby/ruby_specs_test.rb
@@ -7,7 +7,9 @@ require_relative '../support/nat_binary'
 describe 'ruby/spec' do
   parallelize_me!
 
-  SPEC_TIMEOUT = (ENV['SPEC_TIMEOUT'] || 120).to_i
+  def spec_timeout
+    (ENV['SPEC_TIMEOUT'] || 120).to_i
+  end
 
   Dir.chdir File.expand_path('../..', __dir__)
   glob = if ENV['DEBUG_COMPILER']
@@ -20,36 +22,13 @@ describe 'ruby/spec' do
   glob.each do |path|
     describe path do
       it 'passes all specs' do
-        obtain_lock if serialize_test?(path)
-        out_nat = Timeout.timeout(SPEC_TIMEOUT, nil, "execution expired running: #{path}") { `#{NAT_BINARY} #{path} 2>&1` }
+        out_nat = Timeout.timeout(spec_timeout, nil, "execution expired running: #{path}") do
+          `#{NAT_BINARY} #{path} 2>&1`
+        end
         puts out_nat unless $?.success?
         expect($?).must_be :success?
         expect(out_nat).wont_match(/traceback|error/i)
-      ensure
-        release_lock
       end
     end
-  end
-
-  # NATFIXME: Some of our specs don't like to run simultaneously. :-(
-  def serialize_test?(path)
-    %w[
-      spec/core/io
-      spec/library/socket
-    ].any? { |dir| path.start_with?(dir) }
-  end
-
-  def obtain_lock
-    path = File.expand_path('../tmp/serialized_tests_lock.txt', __dir__)
-    @file_lock = File.open(path, File::CREAT)
-    @file_lock.flock(File::LOCK_EX)
-  end
-
-  def release_lock
-    return unless @file_lock
-
-    @file_lock.flock(File::LOCK_UN)
-    @file_lock.close
-    @file_lock = nil
   end
 end
