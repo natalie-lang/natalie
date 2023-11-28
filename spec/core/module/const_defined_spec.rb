f53a27ebcc4d0ead31cc3f1bc1e641b46bffba05
Herwin <herwinw@users.noreply.github.com>
2023-11-28 08:07:53 +0100

    Handle faling specs for Module#const_defined?
    One of them results in a panic, disable that test. Wrap the other ones
    in NATFIXME blocks.

--- spec/core/module/const_defined_spec.rb
+++ spec/core/module/const_defined_spec.rb
@@ -38,16 +38,22 @@ describe "Module#const_defined?" do
   end
 
   it "returns false if the constant is defined in the receiver's superclass and the inherit flag is false" do
-    ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, false).should be_false
+    NATFIXME 'Support inherited argument', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, false).should be_false
+    end
   end
 
   it "returns true if the constant is defined in the receiver's superclass and the inherit flag is true" do
-    ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, true).should be_true
+    NATFIXME 'Support inherited argument', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, true).should be_true
+    end
   end
 
   it "coerces the inherit flag to a boolean" do
-    ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, nil).should be_false
-    ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, :true).should be_true
+    NATFIXME 'Support inherited argument', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, nil).should be_false
+      ConstantSpecs::ContainerA::ChildA.const_defined?(:CS_CONST4, :true).should be_true
+    end
   end
 
   it "returns true if the given String names a constant defined in the receiver" do
@@ -61,7 +67,8 @@ describe "Module#const_defined?" do
     ConstantUnicodeSpecs.const_defined?("CS_CONSTλ").should be_true
   end
 
-  it "returns true when passed a constant name with EUC-JP characters" do
+  # NATFIXME: Multibyte in EUC_JP
+  xit "returns true when passed a constant name with EUC-JP characters" do
     str = "CS_CONSTλ".encode("euc-jp")
     ConstantSpecs.const_set str, 1
     ConstantSpecs.const_defined?(str).should be_true
@@ -100,27 +107,39 @@ describe "Module#const_defined?" do
   end
 
   it "special cases Object and checks it's included Modules" do
-    Object.const_defined?(:CS_CONST10).should be_true
+    NATFIXME "special cases Object and checks it's included Modules", exception: SpecFailedException do
+      Object.const_defined?(:CS_CONST10).should be_true
+    end
   end
 
   it "returns true for toplevel constant when the name begins with '::'" do
-    ConstantSpecs.const_defined?("::Array").should be_true
+    NATFIXME 'Scoping', exception: SpecFailedException do
+      ConstantSpecs.const_defined?("::Array").should be_true
+    end
   end
 
   it "returns true when passed a scoped constant name" do
-    ConstantSpecs.const_defined?("ClassC::CS_CONST1").should be_true
+    NATFIXME 'Scoping', exception: SpecFailedException do
+      ConstantSpecs.const_defined?("ClassC::CS_CONST1").should be_true
+    end
   end
 
   it "returns true when passed a scoped constant name for a constant in the inheritance hierarchy and the inherited flag is default" do
-    ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2").should be_true
+    NATFIXME 'Scoping', exception: SpecFailedException do
+      ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2").should be_true
+    end
   end
 
   it "returns true when passed a scoped constant name for a constant in the inheritance hierarchy and the inherited flag is true" do
-    ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2", true).should be_true
+    NATFIXME 'Support inherited argument', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2", true).should be_true
+    end
   end
 
   it "returns false when passed a scoped constant name for a constant in the inheritance hierarchy and the inherited flag is false" do
-    ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2", false).should be_false
+    NATFIXME 'Support inherited argument', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ClassD.const_defined?("ClassE::CS_CONST2", false).should be_false
+    end
   end
 
   it "returns false when the name begins with '::' and the toplevel constant does not exist" do
@@ -128,33 +147,45 @@ describe "Module#const_defined?" do
   end
 
   it "raises a NameError if the name does not start with a capital letter" do
-    -> { ConstantSpecs.const_defined? "name" }.should raise_error(NameError)
+    NATFIXME 'Validate name', exception: SpecFailedException do
+      -> { ConstantSpecs.const_defined? "name" }.should raise_error(NameError)
+    end
   end
 
   it "raises a NameError if the name starts with '_'" do
-    -> { ConstantSpecs.const_defined? "__CONSTX__" }.should raise_error(NameError)
+    NATFIXME 'Validate name', exception: SpecFailedException do
+      -> { ConstantSpecs.const_defined? "__CONSTX__" }.should raise_error(NameError)
+    end
   end
 
   it "raises a NameError if the name starts with '@'" do
-    -> { ConstantSpecs.const_defined? "@Name" }.should raise_error(NameError)
+    NATFIXME 'Validate name', exception: SpecFailedException do
+      -> { ConstantSpecs.const_defined? "@Name" }.should raise_error(NameError)
+    end
   end
 
   it "raises a NameError if the name starts with '!'" do
-    -> { ConstantSpecs.const_defined? "!Name" }.should raise_error(NameError)
+    NATFIXME 'Validate name', exception: SpecFailedException do
+      -> { ConstantSpecs.const_defined? "!Name" }.should raise_error(NameError)
+    end
   end
 
   it "returns true or false for the nested name" do
     ConstantSpecs.const_defined?("NotExist::Name").should == false
     ConstantSpecs.const_defined?("::Name").should == false
-    ConstantSpecs.const_defined?("::Object").should == true
-    ConstantSpecs.const_defined?("ClassA::CS_CONST10").should == true
+    NATFIXME 'Scoping', exception: SpecFailedException do
+      ConstantSpecs.const_defined?("::Object").should == true
+      ConstantSpecs.const_defined?("ClassA::CS_CONST10").should == true
+    end
     ConstantSpecs.const_defined?("ClassA::CS_CONST10_").should == false
   end
 
   it "raises a NameError if the name contains non-alphabetic characters except '_'" do
     ConstantSpecs.const_defined?("CS_CONSTX").should == false
-    -> { ConstantSpecs.const_defined? "Name=" }.should raise_error(NameError)
-    -> { ConstantSpecs.const_defined? "Name?" }.should raise_error(NameError)
+    NATFIXME 'Validate name', exception: SpecFailedException do
+      -> { ConstantSpecs.const_defined? "Name=" }.should raise_error(NameError)
+      -> { ConstantSpecs.const_defined? "Name?" }.should raise_error(NameError)
+    end
   end
 
   it "raises a TypeError if conversion to a String by calling #to_str fails" do
