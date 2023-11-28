a10fbf631a2ef5f1671024f88fa65489c09e41a0
Herwin <herwinw@users.noreply.github.com>
2023-11-28 10:32:31 +0100

    Make specs for Module#const_get pass
    Disable one test that results in an assertion failure. Wrap all other
    failing tests in NATFIXME blocks.

--- spec/core/module/const_get_spec.rb
+++ spec/core/module/const_get_spec.rb
@@ -13,8 +13,10 @@ describe "Module#const_get" do
   end
 
   it "raises a NameError with the not found constant symbol" do
-    error_inspection = -> e { e.name.should == :CS_CONSTX }
-    -> { ConstantSpecs.const_get :CS_CONSTX }.should raise_error(NameError, &error_inspection)
+    NATFIXME 'raises a NameError with the not found constant symbol', exception: SpecFailedException do
+      error_inspection = -> e { e.name.should == :CS_CONSTX }
+      -> { ConstantSpecs.const_get :CS_CONSTX }.should raise_error(NameError, &error_inspection)
+    end
   end
 
   it "raises a NameError if the name does not start with a capital letter" do
@@ -60,58 +62,78 @@ describe "Module#const_get" do
   end
 
   it "does not search the containing scope" do
-    ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST20).should == :const20_2
-    -> do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST5)
-    end.should raise_error(NameError)
+    NATFIXME 'does not search the containing scope', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST20' do
+      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST20).should == :const20_2
+      -> do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST5)
+      end.should raise_error(NameError)
+    end
   end
 
   it "raises a NameError if the constant is defined in the receiver's superclass and the inherit flag is false" do
-    -> do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, false)
-    end.should raise_error(NameError)
+    NATFIXME "raises a NameError if the constant is defined in the receiver's superclass and the inherit flag is false", exception: SpecFailedException do
+      -> do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, false)
+      end.should raise_error(NameError)
+    end
   end
 
   it "searches into the receiver superclasses if the inherit flag is true" do
-    ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, true).should == :const4
+    NATFIXME 'searches into the receiver superclasses if the inherit flag is true', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, true).should == :const4
+    end
   end
 
   it "raises a NameError when the receiver is a Module, the constant is defined at toplevel and the inherit flag is false" do
-    -> do
-      ConstantSpecs::ModuleA.const_get(:CS_CONST1, false)
-    end.should raise_error(NameError)
+    NATFIXME 'raises a NameError when the receiver is a Module, the constant is defined at toplevel and the inherit flag is false', exception: SpecFailedException do
+      -> do
+        ConstantSpecs::ModuleA.const_get(:CS_CONST1, false)
+      end.should raise_error(NameError)
+    end
   end
 
   it "raises a NameError when the receiver is a Class, the constant is defined at toplevel and the inherit flag is false" do
-    -> do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1, false)
-    end.should raise_error(NameError)
+    NATFIXME 'raises a NameError when the receiver is a Class, the constant is defined at toplevel and the inherit flag is false', exception: SpecFailedException do
+      -> do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1, false)
+      end.should raise_error(NameError)
+    end
   end
 
   it "coerces the inherit flag to a boolean" do
-    ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, :true).should == :const4
+    NATFIXME 'coerces the inherit flag to a boolean', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST4, :true).should == :const4
 
-    -> do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1, nil)
-    end.should raise_error(NameError)
+      -> do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1, nil)
+      end.should raise_error(NameError)
+    end
   end
 
   it "accepts a toplevel scope qualifier" do
-    ConstantSpecs.const_get("::CS_CONST1").should == :const1
+    NATFIXME 'accepts a toplevel scope qualifier', exception: NameError, message: 'uninitialized constant ConstantSpecs::::CS_CONST1' do
+      ConstantSpecs.const_get("::CS_CONST1").should == :const1
+    end
   end
 
   it "accepts a toplevel scope qualifier when inherit is false" do
-    ConstantSpecs.const_get("::CS_CONST1", false).should == :const1
-    -> { ConstantSpecs.const_get("CS_CONST1", false) }.should raise_error(NameError)
+    NATFIXME 'accepts a toplevel scope qualifier when inherit is false', exception: ArgumentError, message: 'wrong number of arguments (given 2, expected 1)' do
+      ConstantSpecs.const_get("::CS_CONST1", false).should == :const1
+      -> { ConstantSpecs.const_get("CS_CONST1", false) }.should raise_error(NameError)
+    end
   end
 
   it "returns a constant whose module is defined the toplevel" do
-    ConstantSpecs.const_get("ConstantSpecsTwo::Foo").should == :cs_two_foo
-    ConstantSpecsThree.const_get("ConstantSpecsTwo::Foo").should == :cs_three_foo
+    NATFIXME 'returns a constant whose module is defined the toplevel', exception: NameError, message: 'uninitialized constant ConstantSpecs::ConstantSpecsTwo::Foo' do
+      ConstantSpecs.const_get("ConstantSpecsTwo::Foo").should == :cs_two_foo
+      ConstantSpecsThree.const_get("ConstantSpecsTwo::Foo").should == :cs_three_foo
+    end
   end
 
   it "accepts a scoped constant name" do
-    ConstantSpecs.const_get("ClassA::CS_CONST10").should == :const10_10
+    NATFIXME 'accepts a scoped constant name', exception: NameError, message: 'uninitialized constant ConstantSpecs::ClassA::CS_CONST10' do
+      ConstantSpecs.const_get("ClassA::CS_CONST10").should == :const10_10
+    end
   end
 
   it "raises a NameError if the name includes two successive scope separators" do
@@ -134,20 +156,27 @@ describe "Module#const_get" do
      ConstantSpecs.const_get(:CS_PRIVATE).should == :cs_private
   end
 
-  it 'does autoload a constant' do
+  # NATFIXME: Failed assertion in include/natalie/constant.hpp:20
+  xit 'does autoload a constant' do
     Object.const_get('CSAutoloadA').name.should == 'CSAutoloadA'
   end
 
   it 'does autoload a constant with a toplevel scope qualifier' do
-    Object.const_get('::CSAutoloadB').name.should == 'CSAutoloadB'
+    NATFIXME 'does autoload a constant with a toplevel scope qualifier', exception: NameError, message: 'uninitialized constant ::CSAutoloadB' do
+      Object.const_get('::CSAutoloadB').name.should == 'CSAutoloadB'
+    end
   end
 
   it 'does autoload a module and resolve a constant within' do
-    Object.const_get('CSAutoloadC::CONST').should == 7
+    NATFIXME 'does autoload a module and resolve a constant within', exception: NameError, message: 'uninitialized constant CSAutoloadC::CONST' do
+      Object.const_get('CSAutoloadC::CONST').should == 7
+    end
   end
 
   it 'does autoload a non-toplevel module' do
-    Object.const_get('CSAutoloadD::InnerModule').name.should == 'CSAutoloadD::InnerModule'
+    NATFIXME 'does autoload a non-toplevel module', exception: NameError, message: 'uninitialized constant CSAutoloadD::InnerModule' do
+      Object.const_get('CSAutoloadD::InnerModule').name.should == 'CSAutoloadD::InnerModule'
+    end
   end
 
   it "raises a NameError when the nested constant does not exist on the module but exists in Object" do
@@ -164,28 +193,40 @@ describe "Module#const_get" do
     end
 
     it "searches a module included in the immediate class before the superclass" do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST15).should == :const15_1
+      NATFIXME 'searches a module included in the immediate class before the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST15' do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST15).should == :const15_1
+      end
     end
 
     it "searches the superclass before a module included in the superclass" do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST11).should == :const11_1
+      NATFIXME 'searches the superclass before a module included in the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST11' do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST11).should == :const11_1
+      end
     end
 
     it "searches a module included in the superclass" do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST12).should == :const12_1
+      NATFIXME 'searches a module included in the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST12' do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST12).should == :const12_1
+      end
     end
 
     it "searches the superclass chain" do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST13).should == :const13
+      NATFIXME 'searches the superclass chain', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST13' do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST13).should == :const13
+      end
     end
 
     it "returns a toplevel constant when the receiver is a Class" do
-      ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1).should == :const1
+      NATFIXME 'returns a toplevel constant when the receiver is a Class', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerA::ChildA::CS_CONST1' do
+        ConstantSpecs::ContainerA::ChildA.const_get(:CS_CONST1).should == :const1
+      end
     end
 
     it "returns a toplevel constant when the receiver is a Module" do
-      ConstantSpecs.const_get(:CS_CONST1).should == :const1
-      ConstantSpecs::ModuleA.const_get(:CS_CONST1).should == :const1
+      NATFIXME 'returns a toplevel constant when the receiver is a Module', exception: NameError, message: 'uninitialized constant ConstantSpecs::CS_CONST1' do
+        ConstantSpecs.const_get(:CS_CONST1).should == :const1
+        ConstantSpecs::ModuleA.const_get(:CS_CONST1).should == :const1
+      end
     end
   end
 
@@ -207,44 +248,58 @@ describe "Module#const_get" do
     it "searches a module included in the immediate class before the superclass" do
       ConstantSpecs::ParentB::CS_CONST302 = :const302_1
       ConstantSpecs::ModuleF::CS_CONST302 = :const302_2
-      ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST302).should == :const302_2
+      NATFIXME 'searches a module included in the immediate class before the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerB::ChildB::CS_CONST302' do
+        ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST302).should == :const302_2
+      end
     end
 
     it "searches the superclass before a module included in the superclass" do
       ConstantSpecs::ModuleE::CS_CONST303 = :const303_1
       ConstantSpecs::ParentB::CS_CONST303 = :const303_2
-      ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST303).should == :const303_2
+      NATFIXME 'searches the superclass before a module included in the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerB::ChildB::CS_CONST303' do
+        ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST303).should == :const303_2
+      end
     end
 
     it "searches a module included in the superclass" do
       ConstantSpecs::ModuleA::CS_CONST304 = :const304_1
       ConstantSpecs::ModuleE::CS_CONST304 = :const304_2
-      ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST304).should == :const304_2
+      NATFIXME 'searches a module included in the superclass', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerB::ChildB::CS_CONST304' do
+        ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST304).should == :const304_2
+      end
     end
 
     it "searches the superclass chain" do
       ConstantSpecs::ModuleA::CS_CONST305 = :const305
-      ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST305).should == :const305
+      NATFIXME 'searches the superclass chain', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerB::ChildB::CS_CONST305' do
+        ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST305).should == :const305
+      end
     end
 
     it "returns a toplevel constant when the receiver is a Class" do
       Object::CS_CONST306 = :const306
-      ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST306).should == :const306
+      NATFIXME 'returns a toplevel constant when the receiver is a Class', exception: NameError, message: 'uninitialized constant ConstantSpecs::ContainerB::ChildB::CS_CONST306' do
+        ConstantSpecs::ContainerB::ChildB.const_get(:CS_CONST306).should == :const306
+      end
     end
 
     it "returns a toplevel constant when the receiver is a Module" do
       Object::CS_CONST308 = :const308
-      ConstantSpecs.const_get(:CS_CONST308).should == :const308
-      ConstantSpecs::ModuleA.const_get(:CS_CONST308).should == :const308
+      NATFIXME 'returns a toplevel constant when the receiver is a Module', exception: NameError, message: 'uninitialized constant ConstantSpecs::CS_CONST308' do
+        ConstantSpecs.const_get(:CS_CONST308).should == :const308
+        ConstantSpecs::ModuleA.const_get(:CS_CONST308).should == :const308
+      end
     end
 
     it "returns the updated value of a constant" do
       ConstantSpecs::ClassB::CS_CONST309 = :const309_1
       ConstantSpecs::ClassB.const_get(:CS_CONST309).should == :const309_1
 
-      -> {
-        ConstantSpecs::ClassB::CS_CONST309 = :const309_2
-      }.should complain(/already initialized constant/)
+      NATFIXME 'Complain about already initialized constant', exception: SpecFailedException do
+        -> {
+          ConstantSpecs::ClassB::CS_CONST309 = :const309_2
+        }.should complain(/already initialized constant/)
+      end
       ConstantSpecs::ClassB.const_get(:CS_CONST309).should == :const309_2
     end
   end
