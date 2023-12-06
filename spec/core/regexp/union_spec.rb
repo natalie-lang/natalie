9b314374d93e21a067369bf27edf7517a14510bd
Herwin <herwinw@users.noreply.github.com>
2023-12-06 07:40:28 +0100

    Enable specs for ISO-8859-1 for higher ASCII

--- spec/core/regexp/union_spec.rb
+++ spec/core/regexp/union_spec.rb
@@ -25,9 +25,10 @@ describe "Regexp.union" do
     end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "returns a Regexp with the encoding of a String containing non-ASCII-compatible characters" do
-    Regexp.union("\u00A9".encode("ISO-8859-1")).encoding.should == Encoding::ISO_8859_1
+  it "returns a Regexp with the encoding of a String containing non-ASCII-compatible characters" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      Regexp.union("\u00A9".encode("ISO-8859-1")).encoding.should == Encoding::ISO_8859_1
+    end
   end
 
   it "returns a Regexp with US-ASCII encoding if all arguments are ASCII-only" do
@@ -42,14 +43,16 @@ describe "Regexp.union" do
     end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "returns a Regexp with the encoding of multiple non-conflicting Strings containing non-ASCII-compatible characters" do
-    Regexp.union("\u00A9".encode("ISO-8859-1"), "\u00B0".encode("ISO-8859-1")).encoding.should == Encoding::ISO_8859_1
+  it "returns a Regexp with the encoding of multiple non-conflicting Strings containing non-ASCII-compatible characters" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      Regexp.union("\u00A9".encode("ISO-8859-1"), "\u00B0".encode("ISO-8859-1")).encoding.should == Encoding::ISO_8859_1
+    end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "returns a Regexp with the encoding of a String containing non-ASCII-compatible characters and another ASCII-only String" do
-    Regexp.union("\u00A9".encode("ISO-8859-1"), "a".encode("UTF-8")).encoding.should == Encoding::ISO_8859_1
+  it "returns a Regexp with the encoding of a String containing non-ASCII-compatible characters and another ASCII-only String" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      Regexp.union("\u00A9".encode("ISO-8859-1"), "a".encode("UTF-8")).encoding.should == Encoding::ISO_8859_1
+    end
   end
 
   it "returns ASCII-8BIT if the regexp encodings are ASCII-8BIT and at least one has non-ASCII characters" do
@@ -111,20 +114,22 @@ describe "Regexp.union" do
     end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include a fixed encoding Regexp and a String containing non-ASCII-compatible characters in a different encoding" do
-    -> {
-      Regexp.union(Regexp.new("a".encode("UTF-8"), Regexp::FIXEDENCODING),
-                   "\u00A9".encode("ISO-8859-1"))
-    }.should raise_error(ArgumentError, 'incompatible encodings: UTF-8 and ISO-8859-1')
+  it "raises ArgumentError if the arguments include a fixed encoding Regexp and a String containing non-ASCII-compatible characters in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union(Regexp.new("a".encode("UTF-8"), Regexp::FIXEDENCODING),
+                     "\u00A9".encode("ISO-8859-1"))
+      }.should raise_error(ArgumentError, 'incompatible encodings: UTF-8 and ISO-8859-1')
+    end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include a String containing non-ASCII-compatible characters and a fixed encoding Regexp in a different encoding" do
-    -> {
-      Regexp.union("\u00A9".encode("ISO-8859-1"),
-                   Regexp.new("a".encode("UTF-8"), Regexp::FIXEDENCODING))
-    }.should raise_error(ArgumentError, 'incompatible encodings: ISO-8859-1 and UTF-8')
+  it "raises ArgumentError if the arguments include a String containing non-ASCII-compatible characters and a fixed encoding Regexp in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union("\u00A9".encode("ISO-8859-1"),
+                     Regexp.new("a".encode("UTF-8"), Regexp::FIXEDENCODING))
+      }.should raise_error(ArgumentError, 'incompatible encodings: ISO-8859-1 and UTF-8')
+    end
   end
 
   it "raises ArgumentError if the arguments include an ASCII-incompatible String and an ASCII-only String" do
@@ -159,32 +164,36 @@ describe "Regexp.union" do
     end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include an ASCII-incompatible String and a String containing non-ASCII-compatible characters in a different encoding" do
-    -> {
-      Regexp.union("a".encode("UTF-16LE"), "\u00A9".encode("ISO-8859-1"))
-    }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+  it "raises ArgumentError if the arguments include an ASCII-incompatible String and a String containing non-ASCII-compatible characters in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union("a".encode("UTF-16LE"), "\u00A9".encode("ISO-8859-1"))
+      }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+    end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include an ASCII-incompatible Regexp and a String containing non-ASCII-compatible characters in a different encoding" do
-    -> {
-      Regexp.union(Regexp.new("a".encode("UTF-16LE")), "\u00A9".encode("ISO-8859-1"))
-    }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+  it "raises ArgumentError if the arguments include an ASCII-incompatible Regexp and a String containing non-ASCII-compatible characters in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union(Regexp.new("a".encode("UTF-16LE")), "\u00A9".encode("ISO-8859-1"))
+      }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+    end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include an ASCII-incompatible String and a Regexp containing non-ASCII-compatible characters in a different encoding" do
-    -> {
-      Regexp.union("a".encode("UTF-16LE"), Regexp.new("\u00A9".encode("ISO-8859-1")))
-    }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+  it "raises ArgumentError if the arguments include an ASCII-incompatible String and a Regexp containing non-ASCII-compatible characters in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union("a".encode("UTF-16LE"), Regexp.new("\u00A9".encode("ISO-8859-1")))
+      }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+    end
   end
 
-  # NATFIXME NOT YET IMPLEMENTED: Conversion above Unicode Basic Latin (0x00..0x7F) not implemented
-  xit "raises ArgumentError if the arguments include an ASCII-incompatible Regexp and a Regexp containing non-ASCII-compatible characters in a different encoding" do
-    -> {
-      Regexp.union(Regexp.new("a".encode("UTF-16LE")), Regexp.new("\u00A9".encode("ISO-8859-1")))
-    }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+  it "raises ArgumentError if the arguments include an ASCII-incompatible Regexp and a Regexp containing non-ASCII-compatible characters in a different encoding" do
+    NATFIXME 'Encodings', exception: SpecFailedException do
+      -> {
+        Regexp.union(Regexp.new("a".encode("UTF-16LE")), Regexp.new("\u00A9".encode("ISO-8859-1")))
+      }.should raise_error(ArgumentError, 'incompatible encodings: UTF-16LE and ISO-8859-1')
+    end
   end
 
   it "uses to_str to convert arguments (if not Regexp)" do
