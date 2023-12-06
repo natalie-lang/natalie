ebc2f18f74fbe9cb86ad8eefaa85a2287bc67a21
Herwin <herwinw@users.noreply.github.com>
2023-12-06 18:01:17 +0100

    Implement ISO-8859-8 transcoding

--- test/natalie/encoding_test.rb
+++ test/natalie/encoding_test.rb
@@ -99,6 +99,54 @@ describe 'encodings' do
     end
   end
 
+  describe 'ISO-8859-8' do
+    it 'can convert codepoints' do
+      [
+        0x61,
+        0x8E,
+        0xFF,
+      ].each do |codepoint|
+        codepoint.chr(Encoding::ISO_8859_8).ord.should == codepoint
+      end
+    end
+
+    it 'can convert to UTF-8' do
+      {
+        0x61 => 0x61,
+        0x8E => 0x8E,
+        0xA8 => 0xA8,
+        0xE0 => 0x5D0,
+      }.each do |codepoint, expected|
+        codepoint.chr(Encoding::ISO_8859_8).encode(Encoding::UTF_8).ord.to_s(16).should == expected.to_s(16)
+      end
+    end
+
+    it 'can convert from UTF-8' do
+      {
+        0x61 => 0x61,
+        0x8E => 0x8E,
+        0xA8 => 0xA8,
+        0x5D0 => 0xE0,
+      }.each do |codepoint, expected|
+        codepoint.chr(Encoding::UTF_8).encode(Encoding::ISO_8859_8).ord.to_s(16).should == expected.to_s(16)
+      end
+    end
+
+    it 'can chop a character (this uses EncodingObject::prev_char)' do
+      [
+        0x61,
+        0x8E,
+        0xFF,
+      ].each do |codepoint|
+        string = 'a'.encode(Encoding::ISO_8859_8) + codepoint.chr(Encoding::ISO_8859_8)
+        string.encoding.should == Encoding::ISO_8859_8
+        string.chop!
+        string.encoding.should == Encoding::ISO_8859_8
+        string.bytes.should == 'a'.encode(Encoding::ISO_8859_8).bytes
+      end
+    end
+  end
+
   describe 'ISO-8859-9' do
     before :each do
       @exceptions = {
