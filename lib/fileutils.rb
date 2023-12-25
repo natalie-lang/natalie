f1ddb3c8d6a1f89850df53c4b9b36f1aac51b81a
Tim Morgan <tim@timmorgan.org>
2023-12-25 09:52:54 -0600

    Add FileUtils.mkdir_p

--- lib/fileutils.rb
+++ lib/fileutils.rb
@@ -14,4 +14,22 @@ module FileUtils
       File.unlink(path)
     end
   end
+
+  def self.mkdir_p(path, mode: 0775)
+    parts = File.expand_path(path).split(File::SEPARATOR)
+
+    dir = parts.shift
+    parts.each do |part|
+      dir = File.join(dir, part)
+
+      unless File.directory?(dir)
+        Dir.mkdir(dir, mode)
+
+        # mkdir(2) (and thus Ruby's Dir.mkdir) does some masking of the mode,
+        # and thus the passed mode above in many cases doesn't have the desired
+        # affect. So we call chmod here to set the mode as desired.
+        File.chmod(mode, dir)
+      end
+    end
+  end
 end
