a27845c13e9b415ea825ffd8e68c3480f4f14317
Herwin <herwinw@users.noreply.github.com>
2024-01-02 18:04:47 +0100

    Return Backtrace::Location objects in caller_locations
    Add a `to_s` method, so most specs pass.

--- src/backtrace.cpp
+++ src/backtrace.cpp
@@ -12,7 +12,7 @@ ArrayObject *Backtrace::to_ruby_array() {
 ArrayObject *Backtrace::to_ruby_backtrace_locations_array() {
     ArrayObject *generated = new ArrayObject { m_items.size() };
     for (auto item : m_items) {
-        generated->push(StringObject::format("{}:{}:in `{}'", item.file, item.line, item.source_location));
+        generated->push(new Thread::Backtrace::LocationObject { item.source_location, item.file, item.line });
     }
     return generated;
 }
