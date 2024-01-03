4249221d53dce3ee0aaa23d0101b8b0bbfc57288
Herwin <herwinw@users.noreply.github.com>
2024-01-03 08:30:45 +0100

    Add Thread::Backtrace::Location#lineno

--- lib/natalie/compiler/binding_gen.rb
+++ lib/natalie/compiler/binding_gen.rb
@@ -1344,6 +1344,7 @@ gen.binding('Thread', 'wakeup', 'ThreadObject', 'wakeup', argc: 0, pass_env: tru
 
 gen.undefine_singleton_method('ThreadBacktraceLocation', 'new')
 gen.binding('Thread::Backtrace::Location', 'inspect', 'Thread::Backtrace::LocationObject', 'inspect', argc: 0, pass_env: true, pass_block: false, return_type: :Object)
+gen.binding('Thread::Backtrace::Location', 'lineno', 'Thread::Backtrace::LocationObject', 'lineno', argc: 0, pass_env: false, pass_block: false, return_type: :Object)
 gen.binding('Thread::Backtrace::Location', 'to_s', 'Thread::Backtrace::LocationObject', 'to_s', argc: 0, pass_env: false, pass_block: false, return_type: :Object)
 
 gen.binding('Thread::Mutex', 'lock', 'Thread::MutexObject', 'lock', argc: 0, pass_env: true, pass_block: false, return_type: :Object)
