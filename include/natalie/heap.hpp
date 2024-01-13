b93a2bf03daaaa143729b4932ede1ded575e9482
Tim Morgan <tim@timmorgan.org>
2024-01-13 15:10:09 -0600

    Hide warning when re-enabling GC

--- include/natalie/heap.hpp
+++ include/natalie/heap.hpp
@@ -16,6 +16,8 @@ namespace Natalie {
 
 using namespace TM;
 
+static void ignore_warning(char *msg, GC_word arg) { }
+
 class Heap {
 public:
     NAT_MAKE_NONCOPYABLE(Heap);
@@ -27,19 +29,18 @@ public:
         return *s_instance;
     }
 
-    void collect() { GC_gcollect(); }
+    void collect() const { GC_gcollect(); }
 
     bool gc_enabled() const { return !GC_is_disabled(); }
 
-    void gc_enable() {
-        assert(GC_dont_gc != 0);
-        GC_dont_gc--;
+    void gc_enable() const {
+        auto warn_proc = GC_get_warn_proc();
+        GC_set_warn_proc(ignore_warning);
+        GC_enable();
+        GC_set_warn_proc(warn_proc);
     }
 
-    void gc_disable() {
-        GC_dont_gc++;
-        assert(GC_dont_gc != 0);
-    }
+    void gc_disable() const { GC_disable(); }
 
     bool collect_all_at_exit() const { return m_collect_all_at_exit; }
     void set_collect_all_at_exit(bool collect) { m_collect_all_at_exit = collect; }
