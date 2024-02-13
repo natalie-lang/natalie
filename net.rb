c51adb00acc0906de7481c9d962c3e189530c888
Herwin <herwinw@users.noreply.github.com>
2024-02-13 20:58:32 +0100

    Extend example script to include HTTP without net/http
    This includes both a HTTP and a HTTPS example. The HTTPS example should
    be updated to use code similar to the internals of net/http, so we have
    a working example with the new OpenSSL code.

--- net.rb
+++ net.rb
@@ -1,7 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'socket'
 require 'net/http'
+require 'openssl'
 
+# Examples without net/http
+TCPSocket.open('natalie-lang.org', 80) do |sock|
+  sock.write("GET / HTTP/1.1\nHost: natalie-lang.org\nConnection: close\n\n")
+  puts sock.read
+end
+
+# NATFIXME: Update this example with the changes made to support net/http
+TCPSocket.open('natalie-lang.org', 443) do |sock|
+  ssl_sock = OpenSSL::SSL::SSLSocket.new(sock)
+  ssl_sock.connect
+  ssl_sock.write("GET / HTTP/1.1\nHost: natalie-lang.org\nConnection: close\n\n")
+  puts ssl_sock.read
+end
+
+# Examples with net/http
 puts Net::HTTP.get(URI('http://natalie-lang.org/'))
 puts Net::HTTP.get(URI('https://natalie-lang.org/specs'))
