069be11389a61c12f585d3693615d290cf0c12c0
Herwin <herwinw@users.noreply.github.com>
2024-02-18 17:52:38 +0100

    Refresh specs for StringIO#set_encoding
    The commented code works fine nowadays.

--- spec/library/stringio/set_encoding_spec.rb
+++ spec/library/stringio/set_encoding_spec.rb
@@ -3,8 +3,7 @@ require_relative '../../spec_helper'
 
 describe "StringIO#set_encoding" do
   it "sets the encoding of the underlying String if the String is not frozen" do
-    # str = "".encode(Encoding::US_ASCII)
-    str = "".encode(Encoding::BINARY)
+    str = "".encode(Encoding::US_ASCII)
 
     io = StringIO.new(str)
     io.set_encoding Encoding::UTF_8
@@ -12,12 +11,10 @@ describe "StringIO#set_encoding" do
   end
 
   it "does not set the encoding of the underlying String if the String is frozen" do
-    # str = "".encode(Encoding::US_ASCII).freeze
-    str = "".encode(Encoding::BINARY).freeze
+    str = "".encode(Encoding::US_ASCII).freeze
 
     io = StringIO.new(str)
     io.set_encoding Encoding::UTF_8
-    # io.string.encoding.should == Encoding::US_ASCII
-    io.string.encoding.should == Encoding::BINARY
+    io.string.encoding.should == Encoding::US_ASCII
   end
 end
