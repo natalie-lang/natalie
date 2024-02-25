c76fdad34d256a2104cfbae54514cbd48d1e1293
Herwin <herwinw@users.noreply.github.com>
2024-02-25 11:57:57 +0100

    Refresh constants in specs of digest classes
    They had some slight alterations to mark the input strings as binary,
    but that issue has been resolved.

--- spec/library/digest/md5/shared/constants.rb
+++ spec/library/digest/md5/shared/constants.rb
@@ -8,8 +8,8 @@ module MD5Constants
   Klass          = ::Digest::MD5
   BlockLength    = 64
   DigestLength   = 16
-  BlankDigest    = "\324\035\214\331\217\000\262\004\351\200\t\230\354\370B~".b
-  Digest         = "\2473\267qw\276\364\343\345\320\304\350\313\314\217n".b
+  BlankDigest    = "\324\035\214\331\217\000\262\004\351\200\t\230\354\370B~"
+  Digest         = "\2473\267qw\276\364\343\345\320\304\350\313\314\217n"
   BlankHexdigest = "d41d8cd98f00b204e9800998ecf8427e"
   Hexdigest      = "a733b77177bef4e3e5d0c4e8cbcc8f6e"
   Base64digest   = "pzO3cXe+9OPl0MToy8yPbg=="
