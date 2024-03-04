74908235281d82b9a31d359a7de7246c50c08223
Herwin <herwinw@users.noreply.github.com>
2024-03-04 18:21:07 +0100

    Add example script to dynamically evaluate code

---
+++ examples/evaluate.rb
@@ -0,0 +1,25 @@
+#!bin/natalie
+
+# Poor man's emulation of Ruby's `eval`.
+# * No binding support, so unable to access local variables
+# * Limited to support of bytecode methods (which is pretty much limited to printing strings)
+# * Terribly slow (although most of that is to compile Natalie's compiler and parser), creating an
+#   executable will make it run at a more acceptable speed).
+
+require 'stringio'
+require 'natalie/compiler'
+require 'natalie/parser'
+
+def evaluate(code)
+  parser = Natalie::Parser.new(code, __FILE__)
+  compiler = Natalie::Compiler.new(ast: parser.ast, encoding: parser.encoding, path: __FILE__)
+  bytecode = compiler.instructions.each.with_object(''.b) do |instruction, stream|
+    stream << instruction.serialize
+  end
+  loader = Natalie::Compiler::BytecodeLoader.new(StringIO.new(bytecode, binmode: true))
+  im = Natalie::Compiler::InstructionManager.new(loader.instructions)
+  Natalie::VM.new(im, path: __FILE__).run
+end
+
+name = ARGV.shift || 'world'
+puts evaluate("puts 'hello, #{name}!'; #{name.dump}.upcase").inspect
