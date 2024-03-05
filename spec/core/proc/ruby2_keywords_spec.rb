8872bd80ab07be5e99c37aae891e3bab1a86930f
Herwin <herwinw@users.noreply.github.com>
2024-03-05 08:05:58 +0100

    Implement Proc#ruby2_keywords

--- spec/core/proc/ruby2_keywords_spec.rb
+++ spec/core/proc/ruby2_keywords_spec.rb
@@ -22,7 +22,9 @@ describe "Proc#ruby2_keywords" do
     f3.ruby2_keywords
 
     Hash.ruby2_keywords_hash?(f3.call(1, 2, a: "a")).should == true
-    Hash.ruby2_keywords_hash?(f4.call(1, 2, a: "a")).should == true
+    NATFIXME 'applies to the underlying method and applies across duplication', exception: SpecFailedException do
+      Hash.ruby2_keywords_hash?(f4.call(1, 2, a: "a")).should == true
+    end
   end
 
   it "returns self" do
@@ -33,24 +35,30 @@ describe "Proc#ruby2_keywords" do
   it "prints warning when a proc does not accept argument splat" do
     f = -> a, b, c { }
 
-    -> {
-      f.ruby2_keywords
-    }.should complain(/Skipping set of ruby2_keywords flag for/)
+    NATFIXME 'prints warning when a proc does not accept argument splat', exception: SpecFailedException do
+      -> {
+        f.ruby2_keywords
+      }.should complain(/Skipping set of ruby2_keywords flag for/)
+    end
   end
 
   it "prints warning when a proc accepts keywords" do
     f = -> a:, b: { }
 
-    -> {
-      f.ruby2_keywords
-    }.should complain(/Skipping set of ruby2_keywords flag for/)
+    NATFIXME 'prints warning when a proc accepts keywords', exception: SpecFailedException do
+      -> {
+        f.ruby2_keywords
+      }.should complain(/Skipping set of ruby2_keywords flag for/)
+    end
   end
 
   it "prints warning when a proc accepts keyword splat" do
     f = -> **a { }
 
-    -> {
-      f.ruby2_keywords
-    }.should complain(/Skipping set of ruby2_keywords flag for/)
+    NATFIXME 'prints warning when a proc accepts keyword splat', exception: SpecFailedException do
+      -> {
+        f.ruby2_keywords
+      }.should complain(/Skipping set of ruby2_keywords flag for/)
+    end
   end
 end
