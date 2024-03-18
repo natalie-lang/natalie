791d222a102a20451d1153c578e81a9453c892cd
Herwin <herwinw@users.noreply.github.com>
2024-03-18 17:13:30 +0100

    Bytecode VM: Add specs for method definition with required positional arguments

---
+++ test/bytecode/define_method_test.rb
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+
+require_relative '../spec_helper'
+
+describe 'define a method' do
+  before :each do
+    @bytecode_file = tmp('bytecode')
+  end
+
+  after :each do
+    rm_r @bytecode_file
+  end
+
+  it 'can define a method without arguments' do
+    code = <<~RUBY
+      def foo = 'foo'
+      puts foo
+    RUBY
+    ruby_exe(code, options: "--compile-bytecode #{@bytecode_file}")
+
+    ruby_exe(@bytecode_file, options: "--bytecode").should == "foo\n"
+  end
+
+  it 'can define a method with required positional arguments' do
+    code = <<~RUBY
+      def foo(a, b) = a + b
+      puts foo('bar', 'baz')
+    RUBY
+    ruby_exe(code, options: "--compile-bytecode #{@bytecode_file}")
+
+    ruby_exe(@bytecode_file, options: "--bytecode").should == "barbaz\n"
+  end
+end
