ed940fc8ee5164895edbf01cdba2461043ac12e2
Herwin <herwinw@users.noreply.github.com>
2024-03-30 17:51:43 +0100

    Import passing specs for Socket

--- spec/library/socket/shared/partially_closable_sockets.rb
+++ spec/library/socket/shared/partially_closable_sockets.rb
@@ -1,7 +1,7 @@
 describe :partially_closable_sockets, shared: true do
   it "if the write end is closed then the other side can read past EOF without blocking" do
     @s1.write("foo")
-    NATFIXME 'Implement IO#close_write', exception: NoMethodError, message: "undefined method `close_write' for an instance of UNIXSocket" do
+    NATFIXME 'Implement IO#close_write', exception: NoMethodError, message: "undefined method `close_write' for an instance of #{@s1.class}" do
       @s1.close_write
       @s2.read("foo".size + 1).should == "foo"
     end
@@ -9,7 +9,7 @@ describe :partially_closable_sockets, shared: true do
 
   it "closing the write end ensures that the other side can read until EOF" do
     @s1.write("hello world")
-    NATFIXME 'Implement IO#close_write', exception: NoMethodError, message: "undefined method `close_write' for an instance of UNIXSocket" do
+    NATFIXME 'Implement IO#close_write', exception: NoMethodError, message: "undefined method `close_write' for an instance of #{@s1.class}" do
       @s1.close_write
       @s2.read.should == "hello world"
     end
