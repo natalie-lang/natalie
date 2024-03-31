02029ae6b8dff1742c3ea4a8cb65ed107b88f5b0
Herwin <herwinw@users.noreply.github.com>
2024-03-31 12:36:54 +0200

    Bytecode VM: Start with unit tests for BytecodeRoData

---
+++ test/bytecode/rodata_test.rb
@@ -0,0 +1,23 @@
+# -*- encoding: binary -*-
+
+require_relative '../spec_helper'
+require 'natalie/compiler/bytecode_ro_data'
+
+describe 'BytecodeRoData' do
+  before :each do
+    @rodata = Natalie::Compiler::BytecodeRoData.new
+  end
+
+  it 'can store multiple strings' do
+    @rodata.add('foo')
+    @rodata.add('quux')
+
+    @rodata.bytesize.should == 9
+    @rodata.to_s.should == "\x03foo\x04quux"
+  end
+
+  it 'returns the index of the stored item' do
+    @rodata.add('foo').should == 0
+    @rodata.add('quux').should == 4
+  end
+end
