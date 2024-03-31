dd37c12f759b45d88b7bf55df800f4129555c66e
Herwin <herwinw@users.noreply.github.com>
2024-03-31 10:29:16 +0200

    Add support for UDP_CORK and TCP_CORK

--- spec/library/socket/spec_helper.rb
+++ spec/library/socket/spec_helper.rb
@@ -4,8 +4,8 @@ require 'socket'
 # NATFIXME: enable as needed
 MSpec.enable_feature :sock_packet if Socket.const_defined?(:SOCK_PACKET)
 MSpec.enable_feature :unix_socket unless PlatformGuard.windows?
-#MSpec.enable_feature :udp_cork if Socket.const_defined?(:UDP_CORK)
-#MSpec.enable_feature :tcp_cork if Socket.const_defined?(:TCP_CORK)
+MSpec.enable_feature :udp_cork if Socket.const_defined?(:UDP_CORK)
+MSpec.enable_feature :tcp_cork if Socket.const_defined?(:TCP_CORK)
 #MSpec.enable_feature :pktinfo if Socket.const_defined?(:IP_PKTINFO)
 #MSpec.enable_feature :ipv6_pktinfo if Socket.const_defined?(:IPV6_PKTINFO)
 #MSpec.enable_feature :ip_mtu if Socket.const_defined?(:IP_MTU)
