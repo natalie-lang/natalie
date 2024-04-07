e5e4b551c34a70df02204880ce8bbf7c25c01995
Herwin <herwinw@users.noreply.github.com>
2024-04-07 07:35:06 +0200

    Convert Proc#<< to C++

--- include/natalie/proc_object.hpp
+++ include/natalie/proc_object.hpp
@@ -50,6 +50,7 @@ public:
 
     Value call(Env *, Args = {}, Block * = nullptr);
     bool equal_value(Value) const;
+    Value ltlt(Env *, Value);
     Value ruby2_keywords(Env *);
     Value source_location();
     StringObject *to_s(Env *);
