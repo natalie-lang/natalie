80c0746902c1ec46139b40d1e69bfacb35bf4b5e
Herwin <herwinw@users.noreply.github.com>
2024-05-18 19:00:56 +0200

    Add SSLContext#session_cache_mode to example script

--- examples/sslsocket.rb
+++ examples/sslsocket.rb
@@ -8,6 +8,7 @@ require 'openssl'
 TCPSocket.open('natalie-lang.org', 443) do |sock|
   ssl_context = OpenSSL::SSL::SSLContext.new
   ssl_context.set_params({ min_version: :TLS1_3, max_version: :TLS1_3, security_level: 2 })
+  ssl_context.session_cache_mode = OpenSSL::SSL::SSLContext::SESSION_CACHE_CLIENT | OpenSSL::SSL::SSLContext::SESSION_CACHE_NO_INTERNAL_STORE
 
   ssl_sock = OpenSSL::SSL::SSLSocket.new(sock, ssl_context)
   ssl_sock.hostname = 'natalie-lang.org'
