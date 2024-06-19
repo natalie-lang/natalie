05df171d7abbaece867b2d22d04851dfdcac24e4
Herwin <herwinw@users.noreply.github.com>
2024-06-19 07:09:58 +0200

    Extract transform_match_required_node into its own class
    This will make it easier to rewrite this code, especially since we might
    be rewriting this code into something that works on a different level.

--- lib/natalie/compiler/pass1.rb
+++ lib/natalie/compiler/pass1.rb
@@ -3,6 +3,7 @@ require_relative './arity'
 require_relative './base_pass'
 require_relative './const_prepper'
 require_relative './multiple_assignment'
+require_relative './transformers/match_required_node'
 
 module Natalie
   class Compiler
@@ -2000,72 +2001,10 @@ module Natalie
       end
 
       def transform_match_required_node(node, used:)
-        case node.pattern.type
-        when :local_variable_target_node
-          instructions = [
-            VariableDeclareInstruction.new(node.pattern.name),
-            transform_expression(node.value, used: true),
-            VariableSetInstruction.new(node.pattern.name),
-          ]
-          instructions << PushNilInstruction.new if used
-          instructions
-        when :constant_path_node, :constant_read_node
-          instructions = [
-            transform_expression(node.value, used: true),
-            DupInstruction.new, # Required for the error message
-            transform_constant_path_node(node.pattern, used: true),
-            SwapInstruction.new,
-            PushArgcInstruction.new(1),
-            SendInstruction.new(
-              :===,
-              args_array_on_stack: false,
-              receiver_is_self: false,
-              with_block: false,
-              has_keyword_hash: false,
-              file: @file.path,
-              line: node.location.start_line,
-            ),
-            IfInstruction.new,
-            PopInstruction.new,
-            ElseInstruction.new(:if),
-            # Comments are for stack of `1 => String`
-            DupInstruction.new, # [1, 1]
-            PushStringInstruction.new(''), # [1, 1, '']
-            SwapInstruction.new, # [1, '', 1']
-            StringAppendInstruction.new, # [1, '1'],
-            PushStringInstruction.new(': '), # [1, '1', ': ']
-            StringAppendInstruction.new, # [1, '1: ']
-            transform_constant_path_node(node.pattern, used: true), # [1, '1: ', String]
-            StringAppendInstruction.new, # [1, '1: String']
-            PushStringInstruction.new(' === '), # [1, '1: String', ' === ']
-            StringAppendInstruction.new, # [1, '1: String === ']
-            SwapInstruction.new, # ['1: String === ', 1]
-            StringAppendInstruction.new, # ['1: String === 1']
-            PushStringInstruction.new(' does not return true'),
-            StringAppendInstruction.new,
-            PushSelfInstruction.new, # [msg, self],
-            SwapInstruction.new, # [self, msg]
-            PushSelfInstruction.new, # [self, msg, self]
-            ConstFindInstruction.new(:NoMatchingPatternError, strict: false), # [self, msg, NoMatchingPatternError]
-            SwapInstruction.new, # [self, NoMatchingPatternError, msg]
-            PushArgcInstruction.new(2),
-            SendInstruction.new(
-              :raise,
-              args_array_on_stack: false,
-              receiver_is_self: true,
-              with_block: false,
-              has_keyword_hash: false,
-              file: @file.path,
-              line: node.location.start_line,
-            ),
-            EndInstruction.new(:if),
-            PopInstruction.new,
-          ]
-          instructions << PushNilInstruction.new if used
-          instructions
-        else
-          raise SyntaxError, "Pattern not yet supported: #{node.pattern.inspect}"
-        end
+        match_required_node_compiler = Transformers::MatchRequiredNode.new(self)
+        instructions = node.accept(match_required_node_compiler)
+        instructions << PushNilInstruction.new if used
+        instructions
       end
 
       def transform_match_write_node(node, used:)
