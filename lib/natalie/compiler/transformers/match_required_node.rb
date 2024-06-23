8b27634f7ddadd834e59b3a182c1b094efd05d74
Herwin <herwinw@users.noreply.github.com>
2024-06-23 12:22:07 +0200

    Implement first version of pattern match with array target
    Only support fixed size targets consisting of only
    LocalVariableTargetNodes.

--- lib/natalie/compiler/transformers/match_required_node.rb
+++ lib/natalie/compiler/transformers/match_required_node.rb
@@ -23,8 +23,26 @@ module Natalie
 
         private
 
-        def transform_array_pattern_node(node, _value)
-          raise SyntaxError, "Pattern not yet supported: #{node.inspect}"
+        def transform_array_pattern_node(node, value)
+          raise SyntaxError, 'Rest argument in array pattern not yet supported' unless node.rest.nil?
+          raise SyntaxError, 'Post arguments in array pattern not yet supported' unless node.posts.empty?
+          raise SyntaxError, 'Targets other then local variables not yet supported' unless node.requireds.all? { |n| n.type == :local_variable_target_node }
+
+          # Transform `expr => [a, b] into `a, b = ->(expr) { expr.deconstruct }.call(expr)`
+          target = node.requireds.map(&:name).join(', ')
+          code_str = <<~RUBY
+            #{target} = lambda do |result|
+              values = result.deconstruct
+              if values.size != #{node.requireds.size}
+                raise ::NoMatchingPatternError, "\#{result}: \#{result} length mismatch (given \#{values.size}, expected #{node.requireds.size})"
+              end
+              values
+            rescue NoMethodError
+              raise ::NoMatchingPatternError, "\#{result}: \#{result} does not respond to #deconstruct"
+            end.call(#{value.location.slice})
+          RUBY
+          parser = Natalie::Parser.new(code_str, compiler.file.path, locals: compiler.current_locals)
+          compiler.transform_expression(parser.ast.statements, used: false)
         end
 
         def transform_eqeqeq_check(node, value)
