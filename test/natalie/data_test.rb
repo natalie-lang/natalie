cd62f8af7670356b66f621077fb83f13827325ad
Herwin <herwinw@users.noreply.github.com>
2024-10-01 07:30:09 +0200

    Remove Natalie tests for Data#to_h
    They have been added upstream.

--- test/natalie/data_test.rb
+++ test/natalie/data_test.rb
@@ -3,10 +3,6 @@ require_relative '../../spec/core/data/fixtures/classes'
 
 ruby_version_is ''...'3.2' do
   describe 'we need the same number of tests here to make the ruby comparison pass' do
-    it 'transforms the data object into a hash' do
-      1.should == 1
-    end
-
     it 'results in a readable representation' do
       1.should == 1
     end
@@ -18,13 +14,6 @@ ruby_version_is ''...'3.2' do
 end
 
 ruby_version_is '3.2' do
-  describe 'Data#to_h' do
-    it 'transforms the data object into a hash' do
-      data = DataSpecs::Measure.new(amount: 42, unit: 'km')
-      data.to_h.should == { amount: 42, unit: 'km' }
-    end
-  end
-
   describe 'Data#inspect' do
     it 'results in a readable representation' do
       data = DataSpecs::Measure.new(amount: 42, unit: 'km')
