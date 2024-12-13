73daef5e0eb785e8d3afcca23cc49a83460f12e5
Herwin <herwinw@users.noreply.github.com>
2024-12-13 19:30:21 +0100

    Write local specs for Data#==

--- test/natalie/data_test.rb
+++ test/natalie/data_test.rb
@@ -14,6 +14,22 @@ ruby_version_is ''...'3.2' do
     it 'should be a subclass of Data' do
       1.should == 1
     end
+
+    it "returns true if the other is the same object" do
+      1.should == 1
+    end
+
+    it "returns true if the other has all the same fields" do
+      1.should == 1
+    end
+
+    it "returns false if the other is a different object or has different fields" do
+      1.should == 1
+    end
+
+    it "returns false if other is of a different class" do
+      1.should == 1
+    end
   end
 end
 
@@ -36,6 +52,32 @@ ruby_version_is '3.2' do
     it 'should be a subclass of Data' do
       data = DataSpecs::Measure.new(amount: 42, unit: 'km')
       data.should be_kind_of(Data)
+    end
+  end
+
+  describe 'Data#==' do
+    it "returns true if the other is the same object" do
+      measure = same_measure = DataSpecs::Measure.new(amount: 42, unit: 'km')
+      measure.should == same_measure
+    end
+
+    it "returns true if the other has all the same fields" do
+      measure = DataSpecs::Measure.new(amount: 42, unit: 'km')
+      similar_measure = DataSpecs::Measure.new(amount: 42, unit: 'km')
+      measure.should == similar_measure
+    end
+
+    it "returns false if the other is a different object or has different fields" do
+      measure = DataSpecs::Measure.new(amount: 42, unit: 'km')
+      different_measure = DataSpecs::Measure.new(amount: 26, unit: 'miles')
+      measure.should != different_measure
+    end
+
+    it "returns false if other is of a different class" do
+      measure = DataSpecs::Measure.new(amount: 42, unit: 'km')
+      klass = Data.define(:amount, :unit)
+      clone = klass.new(amount: 42, unit: 'km')
+      measure.should != clone
     end
   end
 end
