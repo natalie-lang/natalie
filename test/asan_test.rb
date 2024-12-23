f512e15b37e798dce58d1817e9a406fceb8a6754
Tim Morgan <tim@timmorgan.org>
2024-12-23 16:56:18 -0600

    Run all tests with ASan nightly*
    *with some exceptions that are failing

--- test/asan_test.rb
+++ test/asan_test.rb
@@ -8,14 +8,51 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require_relative 'support/compare_rubies'
 
-TESTS = Dir[
-  'test/natalie/**/*_test.rb',
-  'spec/language/*_spec.rb',
-].to_a
+TESTS = if ENV['SOME_TESTS']
+          # runs on every PR -- some tests
+          Dir[
+            'spec/language/*_spec.rb',
+            'test/natalie/**/*_test.rb',
+          ].to_a
+        else
+          # runs nightly -- all tests
+          Dir[
+            'spec/**/*_spec.rb',
+            'test/natalie/**/*_test.rb',
+          ].to_a
+        end
 
 TESTS_TO_SKIP = [
   'test/natalie/libnat_test.rb', # too slow, times out frequently
   'test/natalie/thread_test.rb', # calls GC.start, but we're not ready for that
+  'spec/library/socket/basicsocket/do_not_reverse_lookup_spec.rb', # getaddrinfo leak
+  'spec/library/socket/ipsocket/getaddress_spec.rb', # getaddrinfo leak
+  'spec/library/socket/socket/getaddrinfo_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpsocket/initialize_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpserver/new_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpserver/sysaccept_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpsocket/setsockopt_spec.rb', # getaddrinfo leak
+  'spec/library/socket/ipsocket/peeraddr_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpsocket/recv_nonblock_spec.rb', # getaddrinfo leak
+  'spec/library/socket/udpsocket/bind_spec.rb', # getaddrinfo leak
+  'spec/library/socket/tcpsocket/open_spec.rb', # getaddrinfo leak
+  'spec/library/socket/udpsocket/write_spec.rb', # getaddrinfo leak
+  'spec/library/socket/ipsocket/recvfrom_spec.rb', # getaddrinfo leak
+  'spec/library/socket/ipsocket/addr_spec.rb', # getaddrinfo leak
+  'spec/library/yaml/unsafe_load_spec.rb', # heap buffer overflow in Natalie::StringObject::convert_float()
+  'spec/library/yaml/load_spec.rb', # heap buffer overflow in Natalie::StringObject::convert_float()
+  'spec/library/yaml/dump_spec.rb', # heap buffer overflow in Natalie::StringObject::convert_float()
+  'spec/library/yaml/to_yaml_spec.rb', # heap buffer overflow in Natalie::StringObject::convert_float()
+  'spec/core/process/spawn_spec.rb', # leak in KernelModule::spawn
+  'spec/core/process/fork_spec.rb', # spec timeout
+  'spec/core/kernel/fork_spec.rb', # spec timeout
+  'spec/core/kernel/Float_spec.rb', # heap buffer overflow in Natalie::StringObject::convert_float()
+  'spec/core/kernel/srand_spec.rb', # leak in Natalie::RandomObject::srand
+  'spec/core/random/new_seed_spec.rb', # leak in Natalie::RandomObject::srand
+  'spec/core/random/srand_spec.rb', # leak in Natalie::RandomObject::srand
+  'spec/core/process/uid_spec.rb', # not sure why this breaks
+  'spec/core/process/euid_spec.rb', # not sure why this breaks
+  'spec/core/process/egid_spec.rb', # not sure why this breaks
 ].freeze
 
 describe 'ASAN tests' do
