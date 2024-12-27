97da339f9eb533854e4d76f21028b40e654c9022
Tim Morgan <tim@timmorgan.org>
2024-12-26 19:53:21 -0600

    Use builtin overflow detection

--- src/integer.cpp
+++ src/integer.cpp
@@ -142,13 +142,8 @@ Integer Integer::operator+(const Integer &other) const {
         return to_bigint() + other.to_bigint();
     }
 
-    auto result = to_nat_int_t() + other.to_nat_int_t();
-    auto overflowed = false;
-    if (to_nat_int_t() < 0 && other.to_nat_int_t() < 0 && result > 0)
-        overflowed = true;
-    if (to_nat_int_t() > 0 && other.to_nat_int_t() > 0 && result < 0)
-        overflowed = true;
-
+    nat_int_t result;
+    auto overflowed = __builtin_add_overflow(to_nat_int_t(), other.to_nat_int_t(), &result);
     if (overflowed)
         return to_bigint() + other.to_bigint();
 
@@ -159,14 +154,8 @@ Integer Integer::operator-(const Integer &other) const {
     if (is_bignum() || other.is_bignum())
         return to_bigint() - other.to_bigint();
 
-    bool overflowed = false;
-    nat_int_t result = to_nat_int_t() - other.to_nat_int_t();
-
-    if (to_nat_int_t() > 0 && other.to_nat_int_t() < 0 && result < 0)
-        overflowed = true;
-    if (to_nat_int_t() < 0 && other.to_nat_int_t() > 0 && result > 0)
-        overflowed = true;
-
+    nat_int_t result;
+    auto overflowed = __builtin_sub_overflow(to_nat_int_t(), other.to_nat_int_t(), &result);
     if (overflowed)
         return to_bigint() - other.to_bigint();
 
