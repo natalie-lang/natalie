34dc38d9136481a46c1dfd7405237ae16196e8d9
Tim Morgan <tim@timmorgan.org>
2025-01-01 16:25:49 -0600

    Don't heap allocate short lived Env
    This fixes the GC trying to visit the `outer` which may have disappeared
    by the time we collect this unused Env.
    
    This really points to a larger problem with how inconsistently we
    allocate and chain Envs together; I'll see what I can do about that
    soon...

--- src/kernel_module.cpp
+++ src/kernel_module.cpp
@@ -117,9 +117,9 @@ Value KernelModule::catch_method(Env *env, Value name, Block *block) {
     if (!name) name = new Object {};
 
     try {
-        auto block_env = new Env { env };
-        block_env->set_catch(name);
-        return NAT_RUN_BLOCK_AND_POSSIBLY_BREAK(block_env, block, { name }, nullptr);
+        Env block_env { env };
+        block_env.set_catch(name);
+        return NAT_RUN_BLOCK_AND_POSSIBLY_BREAK(&block_env, block, { name }, nullptr);
     } catch (ThrowCatchException *e) {
         if (e->get_name()->equal(name))
             return e->get_value();
