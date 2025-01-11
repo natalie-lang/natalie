46e211e75aa4029e9036ec122e003424f5c4f240
Tim Morgan <tim@timmorgan.org>
2025-01-10 21:27:40 -0600

    Do not capture `self` when using ruby2_keywords on a block

---
+++ test/natalie/ruby2_keywords_test.rb
@@ -0,0 +1,15 @@
+require_relative '../spec_helper'
+
+describe '#ruby2_keywords' do
+  it 'does not capture self when used as a method' do
+    x = 9
+    klass = Class.new do
+      define_method(:plain, ->(*) { [self, x] })
+      define_method(:wrapped, ->(*) { [self, x] }.ruby2_keywords)
+    end
+    obj = klass.new
+    obj.wrapped.first.should == obj
+    obj.wrapped.last.should == 9
+    obj.plain.should == obj.wrapped
+  end
+end
