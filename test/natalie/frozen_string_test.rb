11b10f24c21303a4838c364bcbd4a3f28b7e4b5f
Tim Morgan <tim@timmorgan.org>
2025-01-11 16:13:57 -0600

    Fix compilation error for certain frozen strings
    If the string contained /* it would confuse gcc.

---
+++ test/natalie/frozen_string_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'frozen string' do
+  it 'does not produce a compilation error if it contains C++ comment delimiters' do
+    [
+      'foo // bar',
+      'foo /* bar */',
+    ].each do |s|
+      # ensure each string is "used" by testing *something*
+      s.should =~ /foo/
+    end
+  end
+end
