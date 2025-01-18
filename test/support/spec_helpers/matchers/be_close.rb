3d6c3ecd91e177a9e9ab8e0849dcd190d5d4f7c2
Herwin <herwinw@users.noreply.github.com>
2025-01-18 15:02:58 +0100

    Import BeCloseMatcher from mspec

---
+++ test/support/spec_helpers/matchers/be_close.rb
@@ -0,0 +1,54 @@
+=begin
+Copyright (c) 2008 Engine Yard, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+=end
+
+TOLERANCE = 0.00003 unless Object.const_defined?(:TOLERANCE)
+# To account for GC, context switches, other processes, load, etc.
+TIME_TOLERANCE = 20.0 unless Object.const_defined?(:TIME_TOLERANCE)
+
+class BeCloseMatcher
+  def initialize(expected, tolerance)
+    @expected = expected
+    @tolerance = tolerance
+  end
+
+  def matches?(actual)
+    @actual = actual
+    (@actual - @expected).abs <= @tolerance
+  end
+
+  def failure_message
+    ["Expected #{@actual}", "to be within #{@expected} +/- #{@tolerance}"]
+  end
+
+  def negative_failure_message
+    ["Expected #{@actual}", "not to be within #{@expected} +/- #{@tolerance}"]
+  end
+end
+
+module MSpecMatchers
+  private def be_close(expected, tolerance)
+    BeCloseMatcher.new(expected, tolerance)
+  end
+end
