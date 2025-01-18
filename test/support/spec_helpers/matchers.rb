6d2b03bb4519f295cac08b8a86fe346a0a662f83
Herwin <herwinw@users.noreply.github.com>
2025-01-18 15:04:42 +0100

    Import BeComputedByMatcher from mspec

--- test/support/spec_helpers/matchers.rb
+++ test/support/spec_helpers/matchers.rb
@@ -1,6 +1,7 @@
 require_relative 'matchers/be_an_instance_of'
 require_relative 'matchers/be_ancestor_of'
 require_relative 'matchers/be_close'
+require_relative 'matchers/be_computed_by'
 require_relative 'matchers/be_empty'
 require_relative 'matchers/be_false'
 require_relative 'matchers/be_kind_of'
