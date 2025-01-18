968d5148737e7aa59bad12b2f6c4a2a14cf4d181
Herwin <herwinw@users.noreply.github.com>
2025-01-18 10:03:49 +0100

    Remove BeInstanceOfExpectation
    Make an Expectation class instead, that can run any matcher. The
    remaining code is generic enough to be completely disjointed from the
    actual matcher.
    
    The generic class is in a weird code spot, but putting it on the top of
    the list actually caused git to complete screw up the diff. This way it
    stays readable, and we should probably just fix the other two the same
    way.

--- test/support/spec.rb
+++ test/support/spec.rb
@@ -561,9 +561,9 @@ class BeKindOfExpectation
   end
 end
 
-class BeInstanceOfExpectation
-  def initialize(klass)
-    @matcher = BeAnInstanceOfMatcher.new(klass)
+class Expectation
+  def initialize(matcher)
+    @matcher = matcher
   end
 
   def match(subject)
@@ -1488,7 +1488,7 @@ class Object
   end
 
   def be_an_instance_of(klass)
-    BeInstanceOfExpectation.new(klass)
+    Expectation.new(BeAnInstanceOfMatcher.new(klass))
   end
 
   def be_ancestor_of(klass)
