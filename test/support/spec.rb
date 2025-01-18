1c12b605ab5317e4dc92a0d77d73a22b96a3530e
Herwin <herwinw@users.noreply.github.com>
2025-01-18 12:51:56 +0100

    Import BeAncestorOfMatcher from mspec

--- test/support/spec.rb
+++ test/support/spec.rb
@@ -547,24 +547,6 @@ class Expectation
   end
 end
 
-class BeAncestorOfExpectation
-  def initialize(klass)
-    @klass = klass
-  end
-
-  def match(subject)
-    unless @klass.ancestors.include?(subject)
-      raise SpecFailedException, subject.inspect + ' should be an ancestor of ' + @klass.inspect
-    end
-  end
-
-  def inverted_match(subject)
-    if @klass.ancestors.include?(subject)
-      raise SpecFailedException, subject.inspect + ' should not to be an ancestor of ' + @klass.inspect
-    end
-  end
-end
-
 class BlockCallerExpectation
   def match(subject)
     raise SpecFailedException, subject.inspect + ' should have blocked, but it did not' unless check(subject)
@@ -1464,7 +1446,7 @@ class Object
   end
 
   def be_ancestor_of(klass)
-    BeAncestorOfExpectation.new(klass)
+    Expectation.new(BeAncestorOfMatcher.new(klass))
   end
 
   def block_caller
