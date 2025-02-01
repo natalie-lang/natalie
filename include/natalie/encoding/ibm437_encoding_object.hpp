3d2b0168439f96993bcdd284a19273a06c735e91
Herwin <herwinw@users.noreply.github.com>
2025-02-01 17:09:57 +0100

    Convert IBM437 encoding object to SingleByteEncodingObject
    This removes a lot of boilerplate code, an inefficient hashmap, and
    fixes the unicode support for the higher bytes.

--- include/natalie/encoding/ibm437_encoding_object.hpp
+++ include/natalie/encoding/ibm437_encoding_object.hpp
@@ -1,45 +1,14 @@
 #pragma once
 
-#include <assert.h>
-#include <initializer_list>
-
-#include "natalie/encoding_object.hpp"
-#include "natalie/string_object.hpp"
+#include "natalie/encoding/single_byte_encoding_object.hpp"
 
 namespace Natalie {
 
 using namespace TM;
 
-class Ibm437EncodingObject : public EncodingObject {
+class Ibm437EncodingObject : public SingleByteEncodingObject {
 public:
-    Ibm437EncodingObject()
-        : EncodingObject { Encoding::IBM437, { "IBM437", "CP437" } } { }
-
-    virtual bool valid_codepoint(nat_int_t codepoint) const override {
-        return (codepoint >= 0 && codepoint <= 0xFF);
-    }
-    virtual bool in_encoding_codepoint_range(nat_int_t codepoint) const override {
-        return (codepoint >= 0 && codepoint <= 0xFF);
-    }
-    virtual bool is_ascii_compatible() const override { return true; };
-
-    virtual std::pair<bool, StringView> prev_char(const String &string, size_t *index) const override;
-    virtual std::pair<bool, StringView> next_char(const String &string, size_t *index) const override;
-
-    virtual void append_escaped_char(String &str, nat_int_t c) const override;
-
-    virtual nat_int_t to_unicode_codepoint(nat_int_t codepoint) const override;
-    virtual nat_int_t from_unicode_codepoint(nat_int_t codepoint) const override;
-
-    virtual String encode_codepoint(nat_int_t codepoint) const override;
-    virtual nat_int_t decode_codepoint(StringView &str) const override;
-
-    virtual bool is_single_byte_encoding() const override final { return true; }
-
-private:
-    static const TM::Hashmap<nat_int_t, nat_int_t> &to_unicode_map();
-
-    static inline TM::Hashmap<nat_int_t, nat_int_t> s_to_unicode_map {};
+    Ibm437EncodingObject();
 };
 
 }
