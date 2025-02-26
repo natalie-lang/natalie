d18ea918c00f25023f956c55f084fd40d6e04c29
Tim Morgan <tim@timmorgan.org>
2025-02-25 22:16:46 -0600

    Use less nullptr in ExceptionObject::create_for_raise()

--- src/exception_object.cpp
+++ src/exception_object.cpp
@@ -6,9 +6,9 @@ ExceptionObject *ExceptionObject::create_for_raise(Env *env, Args &&args, Except
     auto kwargs = args.pop_keyword_hash();
     auto cause = accept_cause && kwargs ? kwargs->remove(env, "cause"_s) : Optional<Value>();
     args.ensure_argc_between(env, 0, 3);
-    auto klass = args.at(0, nullptr);
-    auto message = args.at(1, nullptr);
-    auto backtrace = args.at(2, nullptr);
+    auto klass = args.maybe_at(0);
+    auto message = args.maybe_at(1);
+    auto backtrace = args.maybe_at(2);
 
     if (!message && kwargs && !kwargs->is_empty())
         message = kwargs;
@@ -18,13 +18,13 @@ ExceptionObject *ExceptionObject::create_for_raise(Env *env, Args &&args, Except
     if (!klass && !message && cause)
         env->raise("ArgumentError", "only cause is given with no arguments");
 
-    if (klass && klass.is_class() && !message)
-        return _new(env, klass.as_class(), {}, nullptr).as_exception_or_raise(env);
+    if (klass && klass.value().is_class() && !message)
+        return _new(env, klass.value().as_class(), {}, nullptr).as_exception_or_raise(env);
 
-    if (klass && !klass.is_class() && klass.respond_to(env, "exception"_s)) {
+    if (klass && !klass.value().is_class() && klass.value().respond_to(env, "exception"_s)) {
         Vector<Value> args;
-        if (message) args.push(message);
-        klass = klass.send(env, "exception"_s, std::move(args));
+        if (message) args.push(message.value());
+        klass = klass.value().send(env, "exception"_s, std::move(args));
     }
 
     if (!klass && current_exception)
@@ -36,7 +36,7 @@ ExceptionObject *ExceptionObject::create_for_raise(Env *env, Args &&args, Except
     }
 
     if (!message) {
-        Value arg = klass;
+        Value arg = klass.value();
         if (arg.is_string()) {
             klass = find_top_level_const(env, "RuntimeError"_s).as_class();
             message = arg;
@@ -49,13 +49,13 @@ ExceptionObject *ExceptionObject::create_for_raise(Env *env, Args &&args, Except
 
     Vector<Value> exception_args;
     if (message)
-        exception_args.push(message);
+        exception_args.push(message.value());
 
     ExceptionObject *exception;
-    if (klass.is_class())
-        exception = _new(env, klass.as_class(), { std::move(exception_args), false }, nullptr).as_exception();
-    else if (klass.is_exception())
-        exception = klass.as_exception();
+    if (klass.value().is_class())
+        exception = _new(env, klass.value().as_class(), { std::move(exception_args), false }, nullptr).as_exception();
+    else if (klass.value().is_exception())
+        exception = klass.value().as_exception();
     else
         env->raise("TypeError", "exception klass/object expected");
 
@@ -63,7 +63,7 @@ ExceptionObject *ExceptionObject::create_for_raise(Env *env, Args &&args, Except
         exception->set_cause(cause.value().as_exception());
 
     if (backtrace)
-        exception->set_backtrace(env, backtrace);
+        exception->set_backtrace(env, backtrace.value());
 
     return exception;
 }
