c4f31e44ea019c20af13c488670564d12398e565
Tim Morgan <tim@timmorgan.org>
2025-02-26 19:12:30 -0600

    Use less nullptr in Tempfile#initialize

--- lib/tempfile.cpp
+++ lib/tempfile.cpp
@@ -8,12 +8,14 @@ Value init_tempfile(Env *env, Value self) {
 
 Value Tempfile_initialize(Env *env, Value self, Args &&args, Block *) {
     auto kwargs = args.pop_keyword_hash();
+    if (!kwargs)
+        kwargs = new HashObject {};
     args.ensure_argc_between(env, 0, 2);
-    auto basename = args.at(0, nullptr);
-    auto tmpdir = args.at(1, nullptr);
+    auto basename = args.at(0, Value::nil());
+    auto tmpdir = args.at(1, Value::nil());
     auto suffix = new StringObject { "" };
 
-    if (!basename) {
+    if (basename.is_nil()) {
         basename = new StringObject { "" };
     } else if (!basename.is_string() && basename.respond_to(env, "to_str"_s)) {
         basename = basename.to_str(env);
@@ -26,7 +28,7 @@ Value Tempfile_initialize(Env *env, Value self, Args &&args, Block *) {
     if (!basename.is_string()) {
         env->raise("ArgumentError", "unexpected prefix: {}", basename.inspect_str(env));
     }
-    if (tmpdir && !tmpdir.is_nil()) {
+    if (!tmpdir.is_nil()) {
         tmpdir.assert_type(env, Object::Type::String, "String");
     } else {
         tmpdir = GlobalEnv::the()->Object()->const_fetch("Dir"_s).send(env, "tmpdir"_s);
