eec4343c056865f666093fc5541bea43618042ec
Herwin <herwinw@users.noreply.github.com>
2025-04-09 14:51:28 +0200

    Make Data#inspect and Data#to_s spec compliant
    This borrows some code from Set to prevent recursive structures.

--- src/data.rb
+++ src/data.rb
@@ -29,7 +29,24 @@ class Data
       end
 
       define_method(:inspect) do
-        "#<data #{self.class}#{members.map { |member| " #{member}=#{public_send(member).inspect}" }.join(',')}>"
+        name = if self.class.to_s.start_with?('#')
+                 ''
+               else
+                 " #{self.class}"
+               end
+        if !Fiber[:__data_inspect_current]
+          Fiber.new(storage: { __data_inspect_current: [] }, &->() { inspect }).resume
+        elsif Fiber[:__data_inspect_current].include?(object_id)
+          "#<data#{name}:...>"
+        else
+          Fiber[:__data_inspect_current] << object_id
+          name = if self.class.to_s.start_with?('#')
+                   ''
+                 else
+                   " #{self.class}"
+                 end
+          "#<data#{name}#{members.map { |member| " #{member}=#{public_send(member).inspect}" }.join(',')}>"
+        end
       end
       alias_method :to_s, :inspect
 
