bf9371d3eb51457f549ce663244477ccf6eef82c
Herwin <herwinw@users.noreply.github.com>
2025-05-19 14:37:07 +0200

    Update matched error messages for macos

--- test/natalie/exception_test.rb
+++ test/natalie/exception_test.rb
@@ -10,15 +10,15 @@ describe 'Errno' do
   it 'should use a default message for unknown error numbers' do
     errno = Errno.constants.map { Errno.const_get(_1)::Errno }.max + 1
     exc = SystemCallError.new(nil, errno)
-    exc.message.should == "Unknown error #{errno}"
+    exc.message.should =~ /Unknown error:? #{errno}/
 
     exc = SystemCallError.new('blah', errno)
-    exc.message.should == "Unknown error #{errno} - blah"
+    exc.message.should =~ /Unknown error:? #{errno} - blah/
   end
 
   it 'accepts negative errnos' do
     exc = SystemCallError.new(nil, -1)
-    exc.message.should == 'Unknown error -1'
+    exc.message.should =~ /Unknown error:? -1/
   end
 
   it 'converts objects with #to_int' do
