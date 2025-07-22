41a405652783c89e5410e81df676c17f06d1c3d3
Herwin <herwinw@users.noreply.github.com>
2025-07-22 15:09:25 +0200

    Add tests for String coercion of buffer argument to StringIO#read

--- test/natalie/stringio_test.rb
+++ test/natalie/stringio_test.rb
@@ -11,4 +11,33 @@ describe 'StringIO' do
       io.each_byte.to_a.should == str.bytes
     end
   end
+
+  describe '#read' do
+    context 'with a buffer' do
+      it 'tries to convert a buffer argument with #to_str' do
+        buffer = mock('to_str')
+        str = +''
+        buffer.should_receive(:to_str).and_return(str)
+        io = StringIO.new('foobar')
+        io.read(nil, buffer)
+        buffer.to_str.should == 'foobar'
+      end
+
+      it 'raises a TypeError if the buffer is not a String and does not respond to to_str' do
+        buffer = mock('')
+        io = StringIO.new('foobar')
+        -> { io.read(nil, buffer) }.should raise_error(TypeError, 'no implicit conversion of MockObject into String')
+      end
+
+      it 'raises a TypeError if the buffer is not a String and #to_str does not return a String' do
+        buffer = mock('to_str')
+        buffer.should_receive(:to_str).and_return(:symbol)
+        io = StringIO.new('foobar')
+        -> { io.read(nil, buffer) }.should raise_error(
+                     TypeError,
+                     "can't convert MockObject to String (MockObject#to_str gives Symbol)",
+                   )
+      end
+    end
+  end
 end
