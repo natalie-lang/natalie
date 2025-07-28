f5d9f53dea00f65c3dd85505f60d111dd750a722
Herwin <herwinw@users.noreply.github.com>
2025-07-28 13:00:56 +0200

    Add IPv6 support to TCPSocket.gethostbyname
    By removing the call to gethostbyname(3) and forwarding it to
    Addrinfo.getaddrinfo instead. This is what MRI does too, shown by the
    exception you get when calling this method with an unknown hostname.
    
    The function gethostbyname(3) has been removed from POSIX.1-2008, the
    fact that this still worked is very much by accident.

--- lib/socket.cpp
+++ lib/socket.cpp
@@ -1743,37 +1743,6 @@ Value TCPSocket_initialize(Env *env, Value self, Args &&args, Block *block) {
     return self;
 }
 
-Value TCPSocket_gethostbyname(Env *env, Value self, Args &&args, Block *) {
-    args.ensure_argc_is(env, 1);
-    env->warn("TCPSocket.gethostbyname is deprecated; use Addrinfo.getaddrinfo instead.");
-    auto name = args[0].to_str(env);
-    if (strlen(name->c_str()) < name->bytesize())
-        env->raise("ArgumentError", "string contains null byte");
-    auto hostent = gethostbyname(name->c_str());
-    if (!hostent) {
-        auto Socket_ResolutionError = fetch_nested_const({ "Socket"_s, "ResolutionError"_s }).as_class_or_raise(env);
-        env->raise(Socket_ResolutionError, "getaddrinfo: Name or service not known");
-    }
-    auto aliases = ArrayObject::create();
-    for (auto ptr = hostent->h_aliases; *ptr != nullptr; ptr++) {
-        aliases->push(StringObject::create(*ptr, Encoding::ASCII_8BIT));
-    }
-    auto result = ArrayObject::create({
-        StringObject::create(hostent->h_name, Encoding::ASCII_8BIT),
-        aliases,
-        Value::integer(hostent->h_addrtype),
-    });
-    for (auto ptr = hostent->h_addr_list; *ptr != nullptr; ptr++) {
-        char ipaddr[INET6_ADDRSTRLEN] = { 0 };
-        if (hostent->h_length == 4)
-            inet_ntop(AF_INET, *ptr, ipaddr, INET6_ADDRSTRLEN);
-        else
-            inet_ntop(AF_INET6, *ptr, ipaddr, INET6_ADDRSTRLEN);
-        result->push(StringObject::create(ipaddr, Encoding::ASCII_8BIT));
-    }
-    return result;
-}
-
 Value TCPServer_initialize(Env *env, Value self, Args &&args, Block *block) {
     args.ensure_argc_between(env, 1, 2);
     auto hostname = args.at(0);
