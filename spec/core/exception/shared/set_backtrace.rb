dc5dcf1a4cffa0779cbc2c515d811165b3f7e0b4
Herwin <herwinw@users.noreply.github.com>
2026-01-22 15:16:44 +0100

    Refresh specs for Exception#set_backtrace
    This removes some duplication and uses the shared specs.

--- spec/core/exception/shared/set_backtrace.rb
+++ spec/core/exception/shared/set_backtrace.rb
@@ -15,19 +15,21 @@ describe :exception_set_backtrace, shared: true do
   ruby_version_is "3.4" do
     it "allows the user to set backtrace locations from a rescued exception" do
       bt_locations = ExceptionSpecs::Backtrace.backtrace_locations
-      err = @method.call(bt_locations)
-      err.backtrace_locations.size.should == bt_locations.size
-      err.backtrace_locations.each_with_index do |loc, index|
-        other_loc = bt_locations[index]
+      NATFIXME 'it allows the user to set backtrace locations from a rescued exception', exception: TypeError, message: 'backtrace must be Array of String' do
+        err = @method.call(bt_locations)
+        err.backtrace_locations.size.should == bt_locations.size
+        err.backtrace_locations.each_with_index do |loc, index|
+          other_loc = bt_locations[index]
 
-        loc.path.should == other_loc.path
-        loc.label.should == other_loc.label
-        loc.base_label.should == other_loc.base_label
-        loc.lineno.should == other_loc.lineno
-        loc.absolute_path.should == other_loc.absolute_path
-        loc.to_s.should == other_loc.to_s
+          loc.path.should == other_loc.path
+          loc.label.should == other_loc.label
+          loc.base_label.should == other_loc.base_label
+          loc.lineno.should == other_loc.lineno
+          loc.absolute_path.should == other_loc.absolute_path
+          loc.to_s.should == other_loc.to_s
+        end
+        err.backtrace.size.should == err.backtrace_locations.size
       end
-      err.backtrace.size.should == err.backtrace_locations.size
     end
   end
 
